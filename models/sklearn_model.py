4850511ddedcb8055bb9f78d12432e31c7ebce55
Olga Gureenkova <ol.gure@gmail.com>
2017-11-22 18:55:40 +0300

    feat: add SklearnModel class

---
+++ models/sklearn_model.py
@@ -0,0 +1,50 @@
+"""
+Inherit from this model to implement a `scikit-learn <http://scikit-learn.org/stable/>`_ model.
+"""
+import pickle
+
+from models.model import Model
+
+
+class SklearnModel(Model):
+    def __init__(self, config_path, models, estimator):
+        # TODO where the estimator parameters should initialize?
+        self._estimator = estimator
+        super(SklearnModel).__init__(config_path, models)
+
+    def predict(self, data):
+        """
+        Predict data.
+        :param data: any type of input data
+        :return: any type of predicted values
+        """
+        raise NotImplementedError
+
+    def _save(self):
+        """
+        Save model to file.
+        """
+        # TODO create ser_dir, not 'any_dir'.
+        with open('any_dir') as f:
+            pickle.dump(self._estimator, f)
+
+    def _load(self):
+        """
+        Load model from file.
+        """
+        # TODO decide what dir to check for loading.
+        with open('any_dir') as f:
+            pickle.dump(self._estimator, f)
+
+    def _gen_features(self, dataset):
+        """
+        Generate necessary for training features. Use :attr:`models`to generate input feature
+        vector. The function should return an input feature vector.
+        :param dataset: a dataset used for training
+        """
+        raise NotImplementedError
+
+    def train(self, dataset):
+        X = self._gen_features(dataset)
+        self._estimator.fit(X)
+        self._save()
