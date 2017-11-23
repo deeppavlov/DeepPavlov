8f901541e11a38310ab5a3f8ca98e5bbcfccd0f8
Olga Gureenkova <ol.gure@gmail.com>
2017-11-23 19:37:39 +0300

    feat: implemnent from_params() in abstract class

--- deeppavlov/data/dataset_reader.py
+++ deeppavlov/data/dataset_reader.py
@@ -1,4 +1,5 @@
-from typing import Dict, Type
+from typing import Dict, List
+from inspect import getfullargspec
 
 
 class DatasetReader:
@@ -6,27 +7,27 @@ class DatasetReader:
     A ``DatasetReader`` reads data from some location and constructs a dataset.
     """
 
-    def read(self, file_path: str):
+    def read(self, file_path: str) -> List:
         """
-        Reads a file from a path and returns data as list.
+        Reads a file from a path and returns data as list with training instances.
         """
         raise NotImplementedError
 
     @classmethod
-    def from_params(cls, params: Dict) -> Type:
+    def from_params(cls, params: Dict) -> 'DatasetReader':
         """
-        Static method that constructs the dataset reader described by ``params`` in a config file.
-        ``params`` arg is what comes from the json config.
-
-        Example:
-            signature_params = ['param1', 'param2']
-            param_dict = {}
-            for sp in signature_params:
-                try:
-                    param_dict[sp] = params[sp]
-                except KeyError:
-                    pass
-
-            return TestReader(**param_dict)
+        Take config params and build a class instance. If params are not set in json config, default
+        params are used.
+        :param params: class params from json config
+        :return: a class instance
         """
-        raise NotImplementedError
+
+        signature_params = getfullargspec(cls.__init__).args[1:]
+        param_dict = {}
+        for sp in signature_params:
+            try:
+                param_dict[sp] = params[sp]
+            except KeyError:
+                pass
+
+        return cls(**param_dict)
