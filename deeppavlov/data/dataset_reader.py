2cd353dc64a84aa7570f550b28695f5c349691ff
Olga Gureenkova <ol.gure@gmail.com>
2017-11-24 13:44:59 +0300

    refactor: remove from_params()

--- deeppavlov/data/dataset_reader.py
+++ deeppavlov/data/dataset_reader.py
@@ -1,6 +1,8 @@
 from typing import Dict, List
 from inspect import getfullargspec
 
+from deeppavlov.common.registry import _REGISTRY
+
 
 class DatasetReader:
     """
@@ -12,22 +14,3 @@ class DatasetReader:
         Reads a file from a path and returns data as list with training instances.
         """
         raise NotImplementedError
-
-    @classmethod
-    def from_params(cls, params: Dict) -> 'DatasetReader':
-        """
-        Take config params and build a class instance. If params are not set in json config, default
-        params are used.
-        :param params: class params from json config
-        :return: a class instance
-        """
-
-        signature_params = getfullargspec(cls.__init__).args[1:]
-        param_dict = {}
-        for sp in signature_params:
-            try:
-                param_dict[sp] = params[sp]
-            except KeyError:
-                pass
-
-        return cls(**param_dict)
