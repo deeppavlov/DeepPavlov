42e9c2f3add43d8fa35e67f9dae94034f4aa088f
Olga Gureenkova <ol.gure@gmail.com>
2017-11-30 15:38:18 +0300

    feat: add train_now attribute

--- hcn/models/embedder.py
+++ hcn/models/embedder.py
@@ -9,16 +9,24 @@ from deeppavlov.models.model import Model
 
 @register_model('w2v')
 class UtteranceEmbed(Model):
-    def __init__(self, corpus_path, model_dir_path='emb', model_fpath='text8.model', dim=300):
+    def __init__(self, corpus_path, model_dir_path='emb', model_fpath='text8.model', dim=300,
+                 train_now=False):
         self._corpus_path = corpus_path
         self._model_path = Path(paths.USR_PATH).joinpath(model_dir_path, model_fpath).as_posix()
         self.dim = dim
-        try:
-            self.model = word2vec.Word2Vec.load(self._model_path)
-        except:
-            print(':: creating new word2vec model')
+        self._train_now = train_now
+
+        if self._train_now:
             self._create_model()
             self.model = word2vec.Word2Vec.load(self._model_path)
+
+        else:
+            try:
+                self.model = word2vec.Word2Vec.load(self._model_path)
+            except:
+                print("There is no pretrained model, training a new one anyway.")
+                self._create_model()
+                self.model = word2vec.Word2Vec.load(self._model_path)
 
     def _encode(self, utterance):
         embs = [self.model[word] for word in utterance.split(' ') if word and word in self.model]
@@ -30,6 +38,8 @@ class UtteranceEmbed(Model):
 
     def _create_model(self):
         sentences = word2vec.Text8Corpus(self._corpus_path)
+
+        print(':: creating new word2vec model')
         model = word2vec.Word2Vec(sentences, size=self.dim)
 
         Path.mkdir(paths.USR_PATH.joinpath(self._model_dir_path))
