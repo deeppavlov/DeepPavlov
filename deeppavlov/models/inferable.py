64a4fa2e86e4fda9bc29007abbc0cd741721d40f
Olga Gureenkova <ol.gure@gmail.com>
2017-11-30 18:48:33 +0300

    feat: add Inferable() interface

---
+++ deeppavlov/models/inferable.py
@@ -0,0 +1,26 @@
+"""
+:class:`deeppavlov.models.model.Inferable` is an abstract base class that expresses the interface
+for all models that can work for inferring. The scope of all inferring models is larger than the scope
+of trainable models. For example, encoders can be inferred, but can't be trained.
+All inferring models should inherit from this class.
+"""
+
+from abc import ABCMeta, abstractmethod
+
+
+class Inferable(metaclass=ABCMeta):
+    """
+    :attr:`train_now` expresses a developer intent for whether a model as part of a pipeline
+    should be trained in the current experiment run or not.
+    """
+
+    @abstractmethod
+    def infer(self, instance, *args):
+        """
+        Infer a model. Any model can infer other model and ask it to do something (predict, encode,
+        etc. via this method)
+        :param instance: pass data instance to an inferring model
+        :param inputs: all needed params for inferring
+        :return a result of inferring
+        """
+        pass
