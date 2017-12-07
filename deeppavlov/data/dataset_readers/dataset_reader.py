a7b21f309d11f1c2888e1113504e6120ac25dc61
Aleksey Lymar <yoptar@gmail.com>
2017-12-07 16:07:00 +0300

    feat: remove an unneeded method

--- deeppavlov/data/dataset_readers/dataset_reader.py
+++ deeppavlov/data/dataset_readers/dataset_reader.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Dict
 
 
 class DatasetReader:
@@ -6,17 +6,8 @@ class DatasetReader:
     A ``DatasetReader`` reads data from some location and constructs a dataset.
     """
 
-    def read(self, file_path: str) -> List:
+    def read(self, file_path: str) -> Dict:
         """
         Read a file from a path and returns data as list with training instances.
         """
         raise NotImplementedError
-
-    @staticmethod
-    def save_vocab(data, ser_dir):
-        """
-        Extract single words from data and save them to a serialization dir.
-        :param data: dataset
-        :param ser_dir specified by user serialization dir
-        """
-        pass
