813721c0a80f9473eea6536b454a2340d018bc8d
Mikhail Arkhipov <akrhipov@yahoo.com>
2017-12-08 18:51:51 +0300

    feat: model loading is moved to model_loader

--- ner/model_loader.py
+++ ner/model_loader.py
@@ -1,10 +1,10 @@
-from deeppavlov.data.utils import is_done, mark_done, download
+from deeppavlov.data.utils import is_done, mark_done, download_untar
+import deeppavlov
 import os
 
-def load_nerpa(dpath):
-    if not is_done(dpath):
+
+def load_ner_dstc_model(data_path='model'):
+    if not is_done(data_path):
         url = 'http://lnsigo.mipt.ru/export/ner_dstc_model.tar.gz'
-        os.makedirs(dpath, exist_ok=True)
-        download(url, dpath, 'dstc_ner_model.tar.gz')
-        build_data.untar(dpath, 'dstc_ner_model.tar.gz')
-        build_data.mark_done(dpath, version_string=version)
+        download_untar(url, data_path)
+        mark_done(data_path)
--- ner/run.py
+++ ner/run.py
@@ -1,12 +1,11 @@
 from ner.slotfill import DstcSlotFillingNetwork
+from ner.src.ner_network import NerNetwork
 from deeppavlov.common.registry import _REGISTRY
 from deeppavlov.infering.infer import interact
-from deeppavlov.data.utils import download_untar
-
+from ner.model_loader import load_ner_dstc_model
 
 CONFIG_PATH = 'config.json'
 
 # Download pretrained model
-download_untar('http://lnsigo.mipt.ru/export/ner_dstc_model.tar.gz', 'model/')
+load_ner_dstc_model('model/')
 interact(CONFIG_PATH)
-
