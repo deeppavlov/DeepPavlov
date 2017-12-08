f40d4fd123d22e6ba8b0eec9d518c26021a3f13d
Aleksey Lymar <yoptar@gmail.com>
2017-12-08 11:58:10 +0300

    feat: add seed to the Dataset class for shuffling

--- deeppavlov/data/dataset.py
+++ deeppavlov/data/dataset.py
@@ -6,14 +6,21 @@ class Dataset:
     def split(self, *args, **kwargs):
         pass
 
-    def __init__(self, data: Dict[str, List[Tuple[Any, Any]]], *args, **kwargs) -> None:
+    def __init__(self, data: Dict[str, List[Tuple[Any, Any]]], seed: int = None, *args, **kwargs) -> None:
         r""" Dataset takes a dict with fields 'train', 'test', 'valid'. A list of samples (pairs x, y) is stored
         in each field.
 
         Args:
             data: list of (x, y) pairs. Each pair is a sample from the dataset. x as well as y can be a tuple
                 of different input features.
+            seed (int): random seed for data shuffling. Defaults to None
         """
+
+        rs = random.getstate()
+        random.seed(seed)
+        self.random_state = random.getstate()
+        random.setstate(rs)
+
         self.train = data.get('train', [])
         self.valid = data.get('valid', [])
         self.test = data.get('test', [])
@@ -30,25 +37,33 @@ class Dataset:
          batches
 
         Args:
-            batch_size: number of samples in batch
-            data_type: can be either 'train', 'test', or 'valid'
+            batch_size (int): number of samples in batch
+            data_type (str): can be either 'train', 'test', or 'valid'
 
         Returns:
-            batch_gen: a generator, that iterates through the part (defined by data_type) of dataset
+            batch_gen (Generator): a generator, that iterates through the part (defined by data_type) of the dataset
         """
         data = self.data[data_type]
         data_len = len(data)
         order = list(range(data_len))
+
+        rs = random.getstate()
+        random.setstate(self.random_state)
         random.shuffle(order)
+        self.random_state = random.getstate()
+        random.setstate(rs)
+
         for i in range((data_len - 1) // batch_size + 1):
             yield list(zip(*[data[o] for o in order[i*batch_size:(i+1)*batch_size]]))
 
     def iter_all(self, data_type: str = 'train') -> Generator:
         r"""Iterate through all data. It can be used for building dictionary or
 
-        Returns:
-            samples_gen: a generator, that iterates through the all samples in the 'train' part of dataset
+        Args:
+            data_type (str): can be either 'train', 'test', or 'valid'
 
+        Returns:
+            samples_gen: a generator, that iterates through the all samples in the selected data type of the dataset
         """
         data = self.data[data_type]
         for x, y in data:
