781fcb48612d4c0b7c9dccd14d9c21a2d3658d49
Olga Gureenkova <ol.gure@gmail.com>
2017-12-08 06:03:46 +0300

    feat: add random commutator

--- commutators/random_commutator.py
+++ commutators/random_commutator.py
@@ -2,12 +2,17 @@ from typing import List
 from numpy.random import choice
 
 from deeppavlov.models.inferable import Inferable
+from deeppavlov.common.registry import register_model
 
+@register_model("random")
 class RandomCommutator(Inferable):
     def __init__(self):
         pass
 
-    def commutate(self, inputs: List):
+    def _commutate(self, predictions, history):
         # commutating
-        winner = choice(inputs)
+        winner = choice(predictions)
         return winner
+
+    def infer(self, predictions, history):
+        return self._commutate(predictions,  history)
