15128e13873ab2274c483a53f8a4f06fc1014182
Olga Gureenkova <ol.gure@gmail.com>
2017-12-11 18:20:55 +0300

    chore: minor update

--- deeppavlov/agent/agent.py
+++ deeppavlov/agent/agent.py
@@ -2,12 +2,13 @@ from typing import List, Dict
 
 from deeppavlov.models.inferable import Inferable
 
-# Create this class dynamically
+# TODO Create this class dynamically?
 class Agent(Inferable):
-    def __init__(self, skill_configs: List[Dict], commutator_config: Inferable):
+    def __init__(self, skill_configs: List[Dict], commutator_config: Dict):
         self.skill_configs = skill_configs
         self.commutator_config = commutator_config
         self.history = []
 
     def infer(self):
         pass
+
