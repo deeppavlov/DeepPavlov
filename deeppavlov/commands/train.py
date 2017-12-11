d3f684e402af2c58e29b216650e3c26110d3dfce
Olga Gureenkova <ol.gure@gmail.com>
2017-12-11 17:44:42 +0300

    refactor: structurize training

--- deeppavlov/commands/train.py
+++ deeppavlov/commands/train.py
@@ -1,37 +1,25 @@
-from pathlib import Path
-
-from deeppavlov.common import paths
 from deeppavlov.common.file import read_json
 from deeppavlov.common.registry import _REGISTRY
 from deeppavlov.common.params import from_params
-from deeppavlov.agent.agent import Agent
 from deeppavlov.models.trainable import Trainable
+
+from deeppavlov.commands.utils import set_vocab_path, build_agent_from_config, set_usr_dir, USR_DIR
 
 
 # TODO pass paths to local model configs to agent config.
 
-def set_usr_dir(config_path: str, usr_dir_name='USR_DIR'):
-    # make a serialization user dir
-    root_ = Path(config_path).resolve().parent
-    usr_dir_path = root_.joinpath(usr_dir_name)
-    if not usr_dir_path.exists():
-        usr_dir_path.mkdir()
-    paths.USR_PATH = usr_dir_path
+def get_data(skill_config, dataset_config, vocab_path):
+    dataset_name = dataset_config['name']
+    data_path = skill_config['data_path']
 
-
-def set_vocab_path():
-    return paths.USR_PATH.joinpath('vocab.txt')
-
-
-def build_agent_from_config(config_path: str):
-    config = read_json(config_path)
-    skill_configs = config['skills']
-    commutator_config = config['commutator']
-    return Agent(skill_configs, commutator_config)
+    data_reader = from_params(_REGISTRY[dataset_name], dataset_config)
+    data = data_reader.read(data_path)
+    data_reader.save_vocab(data, vocab_path)
+    return data
 
 
 def train_agent_models(config_path: str):
-    set_usr_dir(config_path)
+    set_usr_dir(config_path, USR_DIR)
     a = build_agent_from_config(config_path)
 
     for skill_config in a.skill_configs:
@@ -40,13 +28,7 @@ def train_agent_models(config_path: str):
         model_name = model_config['name']
 
         if issubclass(_REGISTRY[model_name], Trainable):
-            dataset_config = skill_config['dataset_reader']
-            dataset_name = dataset_config['name']
-            data_path = skill_config['data_path']
-
-            data_reader = from_params(_REGISTRY[dataset_name], dataset_config)
-            data = data_reader.read(data_path)
-            data_reader.save_vocab(data, vocab_path)
+            data = get_data(skill_config, skill_config['dataset_reader'], vocab_path)
 
             model = from_params(_REGISTRY[model_name], model_config, vocab_path=vocab_path)
 
@@ -56,31 +38,20 @@ def train_agent_models(config_path: str):
             num_tr_data = skill_config['num_train_instances']
             model.train(data, num_epochs, num_tr_data)
         else:
+            print('Model {} is not an instance of Trainable, skip training.'.format(model_name))
             pass
             # raise NotImplementedError("This model is not an instance of TFModel class."
             #                           "Only TFModel instances can train for now.")
 
 
-def train_model_from_config(config_path: str, usr_dir_name='USR_DIR'):
+def train_model_from_config(config_path: str, usr_dir_name=USR_DIR):
     # make a serialization user dir
-    root_ = Path(config_path).resolve().parent
-    usr_dir_path = root_.joinpath(usr_dir_name)
-    if not usr_dir_path.exists():
-        usr_dir_path.mkdir()
-
-    paths.USR_PATH = usr_dir_path
+    usr_dir_path = set_usr_dir(config_path, usr_dir_name)
 
     config = read_json(config_path)
-
-    dataset_config = config['dataset_reader']
-    dataset_name = dataset_config['name']
-    data_path = config['data_path']
-
-    data_reader = from_params(_REGISTRY[dataset_name], dataset_config)
-    data = data_reader.read(data_path)
-
     vocab_path = usr_dir_path.joinpath('vocab.txt')
-    data_reader.save_vocab(data, vocab_path)
+
+    data = get_data(config, config['dataset_reader'], vocab_path)
 
     model_config = config['model']
     model_name = model_config['name']
