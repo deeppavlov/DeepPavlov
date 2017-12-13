5ee3929ba2b721f73f4d6e9088b6c4dcba0141b7
Olga Gureenkova <ol.gure@gmail.com>
2017-12-13 15:38:16 +0300

    feat: now dict parameters are allowed in json config

--- deeppavlov/core/common/params.py
+++ deeppavlov/core/common/params.py
@@ -1,31 +1,41 @@
-from inspect import getfullargspec
-
-from typing import Dict, Type
+from typing import Dict, Type, TypeVar
 
 from deeppavlov.core.common.registry import _REGISTRY
 from deeppavlov.core.common.errors import ConfigError
 
+T = TypeVar('T')
 
-def from_params(cls: Type, params: Dict, **kwargs) -> Type:
-    signature_params = getfullargspec(cls.__init__).args[1:]
+
+def from_params(cls: Type, params: Dict, **kwargs) -> Type['T']:
     config_params = {}
-    for sp in signature_params:
+    for sp in params.keys():
         try:
-            config_params[sp] = params[sp]
+            if sp != 'name':
+                config_params[sp] = params[sp]
         except KeyError:
+            print("Using default value for parameter `{}`.".format(sp))
             # Occurs when params[sp] throws KeyError. It means that the needed configuration is
             # absent in the json file and a default configuration from class constructor should
-            # be taken instead.
+            #  be taken instead.
             pass
 
-    for reg_name, subcl_params in config_params.items():
+    for param_name, subcl_params in config_params.items():
         if isinstance(subcl_params, dict):
             try:
-                subcl = _REGISTRY[subcl_params['name']]
+                subcl_name = subcl_params['name']
+            except KeyError:
+                "This parameter is passed as dict to the class constructor."
+                " The user didn't intent it to be a model."
+                continue
+
+            try:
+                subcl = _REGISTRY[subcl_name]
                 subcl_params.pop('name')
-                config_params[reg_name] = from_params(subcl, subcl_params)
+                config_params[param_name] = from_params(subcl, subcl_params)
             except KeyError:
                 raise ConfigError(
-                    "Your registered classes should have 'name' keyword in the config.")
-    return cls(**dict(config_params, **kwargs))
+                    "The class {} is not registered. Either register this class,"
+                    " or rename the parameter.".format(
+                        subcl_params['name']))
 
+    return cls(**dict(config_params, **kwargs))
