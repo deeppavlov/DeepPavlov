0b6ba1981db8921c9a28b14d446484e79d7a1717
dilyararimovna <dilyara.rimovna@gmail.com>
2017-12-14 15:59:44 +0300

    feat: add keras_model class, add infering model for intents

--- intent_recognition/infer.py
+++ intent_recognition/infer.py
@@ -30,7 +30,7 @@ def log_metrics(names, values, updates=None, mode='train'):
     print(" ")  # , end='\r')
 
 
-def main(config_name='config.json'):
+def main(config_name='config_infer.json'):
     with open(config_name) as f:
         config = json.load(f)
 
@@ -61,76 +61,7 @@ def main(config_name='config.json'):
 
     print("Network parameters: ", model.network_params)
     print("Learning parameters:", model.learning_params)
-    print("Considered:", model.metrics_names)
 
-    if 'valid' in data.keys():
-        print('___Validation set is given___')
-    elif 'val_split' in model.learning_params.keys():
-        print('___Validation split is given___')
-    else:
-        print('___Validation set and validation split are not given.____\n____Validation split = 0.1____')
-        model.learning_params['val_split'] = 0.1
-
-    updates = 0
-    val_loss = 1e100
-    val_increase = 0
-    epochs_done = 0
-
-    print('\n____Training____\n\n')
-
-    while epochs_done < model.learning_params['epochs']:
-        batch_gen = dataset.batch_generator(batch_size=model.learning_params['batch_size'],
-                                            data_type='train')
-        for step, batch in enumerate(batch_gen):
-            model.train_on_batch(batch)
-            updates += 1
-
-            # log on training batch
-            if model.learning_params['verbose'] and step % 50 == 0:
-                batch_preds = model.infer(batch[0])
-                # TODO: как сделать вывод в одну строчку во время данной эпохи?
-                log_metrics(names=model.metrics_names,
-                            values=model.metrics_values, updates=updates, mode='train')
-
-        epochs_done += 1
-        if epochs_done % model.learning_params['val_every_n_epochs'] == 0:
-            if 'valid' in data.keys():
-                valid_batch_gen = dataset.batch_generator(batch_size=model.learning_params['batch_size'],
-                                                          data_type='valid')
-                valid_preds = []
-                valid_true = []
-                for valid_id, valid_batch in enumerate(valid_batch_gen):
-                    valid_preds.extend(model.infer(valid_batch[0]))
-                    valid_true.extend(model.labels2onehot(valid_batch[1]))
-                    # if model_config['show_examples'] and valid_id == 0:
-                    #     for j in range(model.learning_params['batch_size']):
-                    #         print(valid_batch[0][j],
-                    #               valid_batch[1][j],
-                    #               model.proba2labels([valid_preds[j]]))
-
-                valid_true = np.asarray(valid_true, dtype='float64')
-                valid_preds = np.asarray(valid_preds, dtype='float64')
-
-                valid_values = []
-                valid_values.append(log_loss(valid_true, valid_preds))
-                valid_values.append(accuracy_score(valid_true, model.proba2onehot(valid_preds)))
-                valid_values.append(fmeasure(valid_true, model.proba2onehot(valid_preds)))
-
-                log_metrics(names=model.metrics_names,
-                            values=valid_values,
-                            mode='valid')
-                if valid_values[0] > val_loss:
-                    val_increase += 1
-                    print("__Validation impatience %d out of %d" % (
-                        val_increase, model.learning_params['val_patience']))
-                    if val_increase == model.learning_params['val_patience']:
-                        print("___Stop training: validation is out of patience___")
-                        break
-                val_loss = valid_values[0]
-
-        print('epochs_done: %d' % epochs_done)
-
-    model.save(fname=model.opt['model_file'])
 
     test_batch_gen = dataset.batch_generator(batch_size=model.learning_params['batch_size'],
                                               data_type='test')
@@ -156,7 +87,6 @@ def main(config_name='config.json'):
     log_metrics(names=model.metrics_names,
                 values=test_values,
                 mode='test')
-
 
 
 
