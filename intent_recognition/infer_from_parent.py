b3316fa3b17a5851bfea4b8de24fe33cb30bf9ec
dilyararimovna <dilyara.rimovna@gmail.com>
2017-12-14 16:54:04 +0300

    fix: fix bags in keras_metrics class

---
+++ intent_recognition/infer_from_parent.py
@@ -0,0 +1,88 @@
+from deeppavlov.core.common.registry import _REGISTRY
+from deeppavlov.core.common.params import from_params
+from deeppavlov.core.models.trainable import Trainable
+from deeppavlov.data.dataset import Dataset
+from deeppavlov.data.dataset_readers.dataset_reader import DatasetReader
+
+from intent_recognition.intent_dataset import IntentDataset
+from intent_recognition.intent_dataset_reader import IntentDatasetReader
+from intent_recognition.utils import EmbeddingsDict
+from intent_recognition.intent_models import KerasIntentModel
+from intent_recognition.intent_model_from_parent import KerasIntentModelFromParent
+
+import os, sys
+import json
+import numpy as np
+from sklearn.metrics import log_loss, accuracy_score
+from metrics import fmeasure
+
+
+def log_metrics(names, values, updates=None, mode='train'):
+    sys.stdout.write("\r")  # back to previous line
+    print("%s -->\t" % mode, end="")
+    if updates is not None:
+        print("updates: %d\t" % updates, end="")
+
+    for id in range(len(names)):
+        print("%s: %f\t" % (names[id], values[id]), end="")
+    print(" ")  # , end='\r')
+
+
+def main(config_name='config_infer.json'):
+    with open(config_name) as f:
+        config = json.load(f)
+
+    # Reading datasets from files
+    reader_config = config['dataset_reader']
+    reader = _REGISTRY[reader_config['name']]
+    data = reader.read(train_data_path=reader_config.get('train_data_path'),
+                       valid_data_path=reader_config.get('valid_data_path'),
+                       test_data_path=reader_config.get('test_data_path'))
+
+    # Building dict of datasets
+    dataset_config = config['dataset']
+    dataset = from_params(_REGISTRY[dataset_config['name']],
+                          dataset_config, data=data)
+
+    # Merging train and valid dataset for further split on train/valid
+    dataset.merge_data(fields_to_merge=['train', 'valid'], new_field='train')
+    dataset.split_data(field_to_split='train', new_fields=['train', 'valid'], proportions=[0.9, 0.1])
+
+    # Extracting unique classes
+    intents = dataset.extract_classes()
+    print("Considered intents:", intents)
+
+    # Initializing model
+    model_config = config['model']
+    model = from_params(_REGISTRY[model_config['name']],
+                        model_config, opt=model_config, classes=intents)
+
+    test_batch_gen = dataset.batch_generator(batch_size=model.opt['batch_size'],
+                                              data_type='test')
+    test_preds = []
+    test_true = []
+    for test_id, test_batch in enumerate(test_batch_gen):
+        test_preds.extend(model.infer(test_batch[0]))
+        test_true.extend(model.labels2onehot(test_batch[1]))
+        if model_config['show_examples'] and test_id == 0:
+            for j in range(model.opt['batch_size']):
+                print(test_batch[0][j],
+                      test_batch[1][j],
+                      model.proba2labels([test_preds[j]]))
+
+    test_true = np.asarray(test_true, dtype='float64')
+    test_preds = np.asarray(test_preds, dtype='float64')
+
+    test_values = []
+    test_values.append(log_loss(test_true, test_preds))
+    test_values.append(accuracy_score(test_true, model.proba2onehot(test_preds)))
+    test_values.append(fmeasure(test_true, model.proba2onehot(test_preds)))
+
+    log_metrics(names=model.metrics_names,
+                values=test_values,
+                mode='test')
+
+
+
+if __name__ == '__main__':
+    main()
--- intent_recognition/keras_model.py
+++ intent_recognition/keras_model.py
@@ -149,7 +149,7 @@ class KerasModel(Trainable, Inferable):
 
         model_func = getattr(self, model_name, None)
         if callable(model_func):
-            model = self.model_func(params=self.opt)
+            model = model_func(params=self.opt)
         else:
             raise AttributeError("Model %s is not defined" % model_name)
 
@@ -157,13 +157,13 @@ class KerasModel(Trainable, Inferable):
 
         optimizer_func = getattr(keras.optimizers, optimizer_name, None)
         if callable(optimizer_func):
-            optimizer_ = keras.optimizers.optimizer_func(lr=lr, decay=decay)
+            optimizer_ = optimizer_func(lr=lr, decay=decay)
         else:
             raise AttributeError("Optimizer %s is not callable" % optimizer_name)
 
         loss_func = getattr(keras.losses, loss_name, None)
         if callable(loss_func):
-            loss = keras.losses.loss_func
+            loss = loss_func
         else:
             raise AttributeError("Loss %s is not defined" % loss_name)
 
@@ -172,7 +172,7 @@ class KerasModel(Trainable, Inferable):
         for i in range(len(metrics_names)):
             metrics_func = getattr(keras.metrics, metrics_names[i], None)
             if callable(metrics_func):
-                metrics_funcs.append(keras.metrics.metrics_func)
+                metrics_funcs.append(metrics_func)
             else:
                 raise AttributeError("Metric %s is not defined" % metrics_names[i])
 
