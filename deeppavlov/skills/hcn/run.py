e181e33c62cdaada61c925e439012fdcba6b6364
Olga Gureenkova <ol.gure@gmail.com>
2017-12-18 00:54:44 +0300

    chore: set usr_dir in run.py

--- deeppavlov/skills/hcn/run.py
+++ deeppavlov/skills/hcn/run.py
@@ -1,23 +1,16 @@
-from deeppavlov.core.commands.infer import interact_agent, interact_model
-from deeppavlov.core.commands.train import train_model_from_config, train_agent_models
-
-from deeppavlov.skills.hcn.babi_dataset_reader import BabiDatasetReader
-from deeppavlov.skills.hcn.models.hybrid import HybridCodeNetwork
-from deeppavlov.skills.hcn.models.bow import BoW_encoder
-from deeppavlov.skills.hcn.models.embedder import UtteranceEmbed
-from deeppavlov.skills.hcn.models.et import EntityTracker
-from deeppavlov.skills.hcn.models.at import ActionTracker
-from deeppavlov.skills.hcn.models.lstm import LSTM
-from deeppavlov.skills.dummy_skill.dummy import DummySkill
-from deeppavlov.models.commutators.random_commutator import RandomCommutator
+from deeppavlov.core.commands.infer import interact_model
+from deeppavlov.core.commands.train import train_model_from_config
+from deeppavlov.core.commands.utils import set_usr_dir
 
 ###### Train and speak to HCN_go_Dummy agent
-AGENT_CONFIG_PATH = '../../agent_configs/hcn_go_dummy.json'
-train_agent_models(AGENT_CONFIG_PATH)
-interact_agent(AGENT_CONFIG_PATH)
+# AGENT_CONFIG_PATH = '../../agent_configs/hcn_go_dummy.json'
+# train_agent_models(AGENT_CONFIG_PATH)
+# interact_agent(AGENT_CONFIG_PATH)
 
 ##### Train and speak to HCN_go skill separately
 MODEL_CONFIG_PATH = 'config.json'
+usr_dir = set_usr_dir(MODEL_CONFIG_PATH)
+
 train_model_from_config(MODEL_CONFIG_PATH)
 interact_model(MODEL_CONFIG_PATH)
 
