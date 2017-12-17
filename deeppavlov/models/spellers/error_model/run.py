24c2f1c5267a3eeca109163de7cf8a373eac5cd4
Olga Gureenkova <ol.gure@gmail.com>
2017-12-18 00:49:12 +0300

    refactor: pass usr_dir and remove custom training method

--- deeppavlov/models/spellers/error_model/run.py
+++ deeppavlov/models/spellers/error_model/run.py
@@ -1,22 +1,13 @@
 from deeppavlov.core.commands.infer import interact_model
 from deeppavlov.core.commands.utils import set_usr_dir
 
-from deeppavlov.models.spellers.models.error_model import ErrorModel
-from deeppavlov.vocabs.wiki_100k_dictionary import Wiki100KDictionary
-from deeppavlov.dataset_readers.typos_wikipedia import TyposWikipedia
-from deeppavlov.dataset_readers.typos_kartaslov import TyposKartaslov
-from deeppavlov.datasets.typos_dataset import TyposDataset
-
-from deeppavlov.models.spellers.train import train
-
-USR_DIR = 'USR_DIR'
-paths.USR_PATH = USR_DIR
+from deeppavlov.core.commands.train import train_model_from_config
 
 CONFIG_PATH = 'config_en.json'
-usr_dir = set_usr_dir(CONFIG_PATH, USR_DIR)
+usr_dir = set_usr_dir(CONFIG_PATH)
 
 ###### Train all models
-train(CONFIG_PATH, usr_dir)
+train_model_from_config(CONFIG_PATH)
 
 ##### Speak to a bot
 interact_model(CONFIG_PATH)
