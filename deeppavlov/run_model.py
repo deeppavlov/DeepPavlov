4e485224e0c8c8edd9507b8463d70d8383977e40
Olga Gureenkova <ol.gure@gmail.com>
2017-12-22 11:25:13 +0300

    feat: add universal run.py for models and agents

---
+++ deeppavlov/run_model.py
@@ -0,0 +1,17 @@
+from deeppavlov.core.commands.train import train_model_from_config
+from deeppavlov.core.commands.infer import interact_model
+from deeppavlov.core.commands.utils import set_usr_dir
+
+# HCN
+# skills/hcn/config.json
+
+# HCN_new
+# skills/hcn_new/config.json
+
+# Speller
+# models/spellers/error_model/config_en.json
+
+MODEL_CONFIG_PATH = 'config.json'
+set_usr_dir(MODEL_CONFIG_PATH)
+train_model_from_config(MODEL_CONFIG_PATH)
+interact_model(MODEL_CONFIG_PATH)
