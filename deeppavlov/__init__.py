4b0b001d65cbc9fd7cfe258bf8797dc37aa89571
Aleksey Lymar <yoptar@gmail.com>
2017-12-22 13:34:08 +0300

    feat: allow usage of a kenlm language model for spelling correction

--- deeppavlov/__init__.py
+++ deeppavlov/__init__.py
@@ -9,6 +9,7 @@ from deeppavlov.skills.dummy_skill.dummy import DummySkill
 from deeppavlov.models.commutators.random_commutator import RandomCommutator
 from deeppavlov.models.spellers.error_model.error_model import ErrorModel
 from deeppavlov.vocabs.wiki_100k_dictionary import Wiki100KDictionary
+from deeppavlov.vocabs.russian_words import RussianWordsVocab
 from deeppavlov.dataset_readers.typos_wikipedia import TyposWikipedia
 from deeppavlov.dataset_readers.typos_kartaslov import TyposKartaslov
 from deeppavlov.datasets.typos_dataset import TyposDataset
