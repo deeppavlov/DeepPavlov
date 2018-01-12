3e6675cf49368e23503c6f8cb2e42ffa3ceb627d
Aleksey Lymar <yoptar@gmail.com>
2018-01-12 16:13:22 +0300

    feat: add a dataset_reader for custom typos datasets

--- deeppavlov/__init__.py
+++ deeppavlov/__init__.py
@@ -4,8 +4,7 @@ from deeppavlov.vocabs.russian_words import RussianWordsVocab
 from deeppavlov.dataset_readers.babi_dataset_reader import BabiDatasetReader
 from deeppavlov.dataset_readers.dstc2_dataset_reader import DSTC2DatasetReader
 from deeppavlov.dataset_readers.intent_dataset_reader import IntentDatasetReader
-from deeppavlov.dataset_readers.typos_kartaslov import TyposKartaslov
-from deeppavlov.dataset_readers.typos_wikipedia import TyposWikipedia
+from deeppavlov.dataset_readers.typos import TyposWikipedia, TyposKartaslov, TyposCustom
 from deeppavlov.datasets.dstc2_datasets import DSTC2DialogDataset
 from deeppavlov.datasets.hcn_dataset import HCNDataset
 from deeppavlov.datasets.intent_dataset import IntentDataset
