4d059abef6fa92715cdf738fd893b0fee9f5bf53
Aleksey Lymar <yoptar@gmail.com>
2018-01-12 14:59:06 +0300

    fix: proper registered name for wikitionary 100K vocab component

--- deeppavlov/vocabs/wiki_100k_dictionary.py
+++ deeppavlov/vocabs/wiki_100k_dictionary.py
@@ -6,14 +6,14 @@ from deeppavlov.core.common.registry import register
 from deeppavlov.vocabs.static_dictionary import StaticDictionary
 
 
-@register('wikipedia_100K_vocab')
+@register('wikitionary_100K_vocab')
 class Wiki100KDictionary(StaticDictionary):
     dict_name = 'wikipedia_100K_vocab'
 
     @staticmethod
     def _get_source(*args, **kwargs):
         words = []
-        print('Downloading english vocab from Wikipedia', file=sys.stderr)
+        print('Downloading english vocab from Wiktionary', file=sys.stderr)
         for i in range(1, 100000, 10000):
             k = 10000 + i - 1
             url = 'https://en.wiktionary.org/wiki/Wiktionary:Frequency_lists/PG/2005/08/{}-{}'.format(i, k)
