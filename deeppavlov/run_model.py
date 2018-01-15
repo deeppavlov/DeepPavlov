41a9a006f9adb61ac4ad498c37007dc4944c3eb5
Olga Gureenkova <ol.gure@gmail.com>
2018-01-15 16:50:46 +0300

    ==Stable commit!== speller, intent classifier, ner work on train and inference

--- deeppavlov/run_model.py
+++ deeppavlov/run_model.py
@@ -23,7 +23,7 @@ from deeppavlov.core.commands.utils import set_usr_dir
 usr_dir = None
 
 try:
-    MODEL_CONFIG_PATH = 'models/classifiers/intents/config.json'
+    MODEL_CONFIG_PATH = 'models/spellers/error_model/config_en.json'
     set_usr_dir(MODEL_CONFIG_PATH)
     train_model_from_config(MODEL_CONFIG_PATH)
     interact_model(MODEL_CONFIG_PATH)
