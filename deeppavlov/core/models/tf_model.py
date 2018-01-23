2735b8432c474da52b31c26500bda37b2969f615
Olga Gureenkova <ol.gure@gmail.com>
2018-01-23 17:24:38 +0300

    fix: load and save models

--- deeppavlov/core/models/tf_model.py
+++ deeppavlov/core/models/tf_model.py
@@ -12,6 +12,7 @@ from overrides import overrides
 from deeppavlov.core.models.trainable import Trainable
 from deeppavlov.core.models.inferable import Inferable
 from deeppavlov.core.common.attributes import check_attr_true, check_path_exists
+from deeppavlov.core.common.errors import ConfigError
 from .tf_backend import TfModelMeta
 
 
@@ -72,10 +73,13 @@ class TFModel(Trainable, Inferable, metaclass=TfModelMeta):
         return self._forward(instance, *args)
 
     def save(self):
-        if self.ser_path.is_dir():
-            save_path = self.ser_path
+        save_path = self.ser_path
+        if save_path.is_dir():
+            save_path = save_path / self._ser_file
+        elif save_path.parent.is_dir():
+            pass
         else:
-            save_path = self.ser_path.parent
+            raise ConfigError("Provided ser path doesn't exists")
         print('\n:: saving model to {} \n'.format(save_path))
         self._saver().save(sess=self.sess, save_path=str(save_path), global_step=0)
         print('model saved')
