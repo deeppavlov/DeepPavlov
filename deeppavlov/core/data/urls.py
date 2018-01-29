6596b41ae367bb80d9a59ee9b673db82b5863f12
Olga Gureenkova <ol.gure@gmail.com>
2018-01-29 20:24:58 +0300

    feat: download from mipt share

---
+++ deeppavlov/core/data/urls.py
@@ -0,0 +1,20 @@
+REQ_URLS = {
+    'http://lnsigo.mipt.ru/export/deeppavlov_data/go_bot_rnn.tar.gz',
+    'http://lnsigo.mipt.ru/export/deeppavlov_data/intents.tar.gz',
+    'http://lnsigo.mipt.ru/export/deeppavlov_data/ner.tar.gz',
+    'http://lnsigo.mipt.ru/export/deeppavlov_data/error_model.tar.gz',
+    'http://lnsigo.mipt.ru/export/deeppavlov_data/vocabs.tar.gz',
+    'http://lnsigo.mipt.ru/export/deeppavlov_data/slots.tar.gz',
+    'http://lnsigo.mipt.ru/export/deeppavlov_data/embeddings/dstc2_fasttext_model_100.bin'
+}
+
+OPT_URLS = {
+    'http://lnsigo.mipt.ru/export/deeppavlov_data/embeddings/wiki.en.bin'
+}
+
+ALL_URLS = REQ_URLS.union(OPT_URLS)
+
+EMBEDDING_URLS = {
+    'http://lnsigo.mipt.ru/export/deeppavlov_data/embeddings/wiki.en.bin',
+    'http://lnsigo.mipt.ru/export/deeppavlov_data/embeddings/dstc2_fasttext_model_100.bin'
+}
--- deeppavlov/core/data/utils.py
+++ deeppavlov/core/data/utils.py
@@ -4,7 +4,12 @@ import requests
 import sys
 from tqdm import tqdm
 import tarfile
+import gzip
 import re
+
+_MARK_DONE = '.done'
+
+tqdm.monitor_interval = 0
 
 
 def download(dest_file_path, source_url):
@@ -15,7 +20,7 @@ def download(dest_file_path, source_url):
         source_url: the source URL
 
     """
-
+    CHUNK = 16 * 1024
     dest_file_path = Path(dest_file_path).absolute()
     dest_file_path.parent.mkdir(parents=True, exist_ok=True)
 
@@ -26,7 +31,7 @@ def download(dest_file_path, source_url):
         print('Downloading from {} to {}'.format(source_url, dest_file_path), file=sys.stderr)
 
         pbar = tqdm(total=total_length, unit='B', unit_scale=True)
-        for chunk in r.iter_content(chunk_size=32 * 1024):
+        for chunk in r.iter_content(chunk_size=CHUNK):
             if chunk:  # filter out keep-alive new chunks
                 pbar.update(len(chunk))
                 f.write(chunk)
@@ -49,33 +54,60 @@ def untar(file_path, extract_folder=None):
     tar.close()
 
 
-def download_untar(url, download_path, extract_path=None):
-    """Download and extract tar.gz file. The archive is deleted after extraction.
+def ungzip(file_path, extract_folder=None):
+    """Simple .gz archive extractor
+
+        Args:
+            file_path: path to the gzip file to be extracted
+            extract_folder: folder to which the files will be extracted
+
+        """
+    CHUNK = 16 * 1024
+    fname = str(file_path).split("/")[-1][:-3]
+    file_path = Path(file_path)
+    if extract_folder is None:
+        extract_folder = file_path.parent
+    extract_folder = Path(extract_folder)
+    extract_path = extract_folder / fname
+
+    with file_path.open('rb') as fin:
+        fout = gzip.open(extract_path, 'wb')
+        while True:
+            block = fin.read(CHUNK)
+            if not block:
+                break
+            fout.write(block)
+        fout.close()
+
+
+def download_decompress(url, download_path, extract_path=None):
+    """Download and extract .tar.gz or .gz file. The archive is deleted after extraction.
 
     Args:
         url: URL for file downloading
-        download_path: path to the directory where downloaded file will be stored until the end of extraction
-        extract_path: path where contents of tar file will be extracted
+        download_path: path to the directory where downloaded file will be stored
+        until the end of extraction
+        extract_path: path where contents of archive will be extracted
     """
     file_name = url.split('/')[-1]
     download_path = Path(download_path)
     if extract_path is None:
         extract_path = download_path
     extract_path = Path(extract_path)
-    tar_file_path = download_path / file_name
-    print('Extracting {} archive into {}'.format(tar_file_path, extract_path), file=sys.stderr)
-    download(tar_file_path, url)
-    untar(tar_file_path, extract_path)
-    tar_file_path.unlink()
+    arch_file_path = download_path / file_name
+    print('Extracting {} archive into {}'.format(arch_file_path, extract_path), file=sys.stderr)
+    download(arch_file_path, url)
+    if url.endswith('.tar.gz'):
+        untar(arch_file_path, extract_path)
+    elif url.endswith('.gz'):
+        ungzip(arch_file_path, extract_path)
+    arch_file_path.unlink()
 
 
 def load_vocab(vocab_path):
     vocab_path = Path(vocab_path)
     with vocab_path.open() as f:
         return f.read().split()
-
-
-_MARK_DONE = '.done'
 
 
 def mark_done(path):
