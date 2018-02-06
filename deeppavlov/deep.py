1522c6d0088a2d1d4a14cd9a70ea560ead92a6ba
alitinsky <alitinsky@gmail.com>
2018-02-06 13:38:36 +0300

    Created log_config.json, added logging to deep.py

--- deeppavlov/deep.py
+++ deeppavlov/deep.py
@@ -18,6 +18,8 @@ import argparse
 from pathlib import Path
 import sys
 import os
+import logging
+import logging.config
 
 p = (Path(__file__) / ".." / "..").resolve()
 sys.path.append(str(p))
@@ -25,7 +27,14 @@ sys.path.append(str(p))
 from deeppavlov.core.commands.utils import set_usr_dir, get_usr_dir
 from deeppavlov.core.commands.train import train_model_from_config
 from deeppavlov.core.commands.infer import interact_model
+from deeppavlov.core.common.file import read_json
 from telegram_utils.telegram_ui import interact_model_by_telegram
+
+
+log_config_path = (p / 'log_config.json').resolve()
+log_config = read_json(log_config_path)
+logging.config.dictConfig(log_config)
+log = logging.getLogger(__name__)
 
 parser = argparse.ArgumentParser()
 
@@ -49,8 +58,7 @@ def main():
             interact_model(pipeline_config_path)
         elif args.mode == 'interactbot':
             if not token:
-                print(
-                    'Token required: initiate -t parm or TELEGRAM_BOT env var with Telegram bot token')
+                log.critical('Token required: initiate -t parm or TELEGRAM_BOT env var with Telegram bot token')
             else:
                 interact_model_by_telegram(pipeline_config_path, token)
     finally:
