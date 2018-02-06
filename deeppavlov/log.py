6a4103f45d3aa7a49f45d601f2de6cfc5d5dc80f
alitinsky <alitinsky@gmail.com>
2018-02-06 18:36:05 +0300

    updated logging

--- deeppavlov/log.py
+++ deeppavlov/log.py
@@ -1,26 +1,39 @@
 import pathlib
 import json
 import logging.config
+import sys
 
 
 LOG_CONFIG_FILENAME = 'log_config.json'
-DEFAULT_LOGGER_NAME = '_default'
 
 
 def get_logger(logger_name):
-    config_dir = pathlib.PurePath(__file__).parent
-    log_config_path = pathlib.Path(config_dir, LOG_CONFIG_FILENAME).resolve()
-
-    with open(log_config_path) as log_config_json:
-        log_config = json.load(log_config_json)
-
-    logging.config.dictConfig(log_config)
-
     try:
+        config_dir = pathlib.PurePath(__file__).parent
+        log_config_path = pathlib.Path(config_dir, LOG_CONFIG_FILENAME).resolve()
+
+        with open(log_config_path) as log_config_json:
+            log_config = json.load(log_config_json)
+
+        logging.config.dictConfig(log_config)
         logger = logging.getLogger(logger_name)
+
     except Exception:
-        logger = logging.getLogger('_default')
+        logger = logging.getLogger(logger_name)
+        logger.setLevel(logging.WARNING)
+
+        formatter = logging.Formatter(
+            '%(asctime)s.%(msecs)d %(levelname)s in \'%(module)s\' at line %(lineno)d: %(message)s',
+            '%Y-%m-%d %H:%M:%S')
+
+        handler = logging.StreamHandler(sys.stderr)
+        handler.setFormatter(formatter)
+        handler.setLevel(logging.WARNING)
+
+        logger.addHandler(handler)
+
         logger.error(
-            'Can not initialise {} logger, logging to the stderr with {}'.format(logger_name, DEFAULT_LOGGER_NAME))
+            'LOGGER ERROR: Can not initialise {} logger, '
+            'logging to the stderr. Error traceback:\n'.format(logger_name), exc_info=1)
 
     return logger
