afcefa7f8b6323d2fb043af4d360546a33833e50
Mariia Vikhreva <mary.vikhreva@gmail.com>
2018-02-13 12:23:12 +0300

    style: rm unused by trai

--- deeppavlov/skills/go_bot/go_bot.py
+++ deeppavlov/skills/go_bot/go_bot.py
@@ -29,7 +29,6 @@ from deeppavlov.models.classifiers.intents.intent_model import KerasIntentModel
 from deeppavlov.models.ner.slotfill import DstcSlotFillingNetwork
 from deeppavlov.models.tokenizers.spacy_tokenizer import SpacyTokenizer
 from deeppavlov.models.trackers.default_tracker import DefaultTracker
-from deeppavlov.skills.go_bot.metrics import DialogMetrics
 from deeppavlov.skills.go_bot.network import GoalOrientedBotNetwork
 from deeppavlov.skills.go_bot.templates import Templates, DualTemplate
 from deeppavlov.core.common.attributes import check_attr_true
@@ -52,8 +51,6 @@ class GoalOrientedBot(Inferable, Trainable):
                  intent_classifier=None,
                  use_action_mask=False,
                  debug=False,
-                 num_epochs=200,
-                 val_patience=10,
                  train_now=False,
                  save_path=None,
                  **kwargs):
@@ -71,11 +68,9 @@ class GoalOrientedBot(Inferable, Trainable):
         self.tracker = tracker
         self.network = network
         self.word_vocab = vocabs['word_vocab']
-        self.num_epochs = num_epochs
-        self.val_patience = val_patience
 
         self.templates = Templates(template_type).load(template_path)
-        log.info("[using {} templates from `{}`]".format(len(self.templates), template_path))
+        log.info("[ using {} templates from `{}` ]".format(len(self.templates), template_path))
 
         # intialize parameters
         self.db_result = None
@@ -84,9 +79,6 @@ class GoalOrientedBot(Inferable, Trainable):
         if hasattr(self.intent_classifier, 'n_classes'):
             self.n_intents = self.intent_classifier.n_classes
         self.prev_action = np.zeros(self.n_actions, dtype=np.float32)
-
-        # initialize metrics
-        self.metrics = DialogMetrics(self.n_actions)
 
         # opt = {
         #    'action_size': self.n_actions,
@@ -201,93 +193,6 @@ class GoalOrientedBot(Inferable, Trainable):
 
             self.network.train(d_features, d_actions, d_masks)
 
-    @check_attr_true('train_now')
-    def train(self, data):
-        
-        if self.network.train_now is False:
-            raise ConfigError("It looks like 'train_now' of mother model is True, while"
-                              "`train_now` of submodel is False. Set `train_now` of submodel"
-                              "to True.")
-
-        print('\n:: training started')
-
-        curr_patience = self.val_patience
-        best_valid_accuracy = 0.
-        # TODO: in case val_patience is off, save model {val_patience} steps before
-        for j in range(self.num_epochs):
-
-            tr_data = data.batch_generator(1, 'train', shuffle=False)
-
-            self.reset_metrics()
-
-            for batch in tr_data:
-                for dialog in zip(*batch):
-
-                    self.reset()
-                    self.metrics.n_dialogs += 1
-                    d_features, d_actions, d_masks = [], [], []
-
-                    for context, response in zip(*dialog):
-                        features = self._encode_context(context['text'],
-                                                        context.get('db_result'))
-                        if context.get('db_result') is not None:
-                            self.db_result = context['db_result']
-                        d_features.append(features)
-
-                        action_id = self._encode_response(response['act'])
-                        # previous action is teacher-forced here
-                        self.prev_action *= 0.
-                        self.prev_action[action_id] = 1.
-                        d_actions.append(action_id)
-
-                        d_masks.append(self._action_mask())
-
-                    loss, d_preds = self.network.train(d_features, d_actions, d_masks)
-
-                    for pred_id, action_id in zip(d_preds, d_actions):
-                        pred = ""
-                        # TODO: decoding is using wrong state
-                        #self._decode_response(pred_id).lower()
-                        true = self.tokenizer.infer(response['text'].lower().split())
-
-                        # update metrics
-                        self.metrics.n_examples += 1
-                        self.metrics.train_loss += loss
-                        self.metrics.conf_matrix[pred_id, action_id] += 1
-                        #self.metrics.n_corr_examples += int(pred == true)
-                        if self.debug and ((pred == true) != (pred_id == action_id)):
-                            print("Slot filling problem: ")
-                            print("Pred = {}: {}".format(pred_id, pred))
-                            print("True = {}: {}".format(action_id, true))
-                            #print("State =", self.tracker.get_state())
-                            #print("db_result =", self.db_result)
-                            # TODO: update dialog metrics
-                    
-            print('\n\n:: {}.train {}'.format(j + 1, self.metrics.report()))
-
-            train_data = data.batch_generator(1, 'train')
-            eval_data = data.batch_generator(1, 'valid')
-            #train_data = data.iter_all('train')
-            train_metrics = self.evaluate(train_data)
-            print(':: {}.train {}'.format(j + 1, train_metrics.report()))
-            valid_metrics = self.evaluate(eval_data)
-            print(':: {}.valid {}'.format(j + 1, valid_metrics.report()))
-
-            if valid_metrics.action_accuracy < best_valid_accuracy:
-                curr_patience -= 1
-                print(":: patience decreased by 1, is equal to {}".format(curr_patience))
-            else:
-                if curr_patience != self.val_patience:
-                    curr_patience = self.val_patience
-                    print(":: patience is equal to {}".format(curr_patience))
-                best_valid_accuracy = valid_metrics.action_accuracy
-            if curr_patience < 1:
-                print("\n:: patience is over, stopped training\n")
-                break
-        else:
-            print("\n:: stopping because max number of epochs encountered\n")
-        self.save()
-
     def infer_on_batch(self, xs):
         return [self._infer_dialog(x) for x in xs]
 
@@ -333,12 +238,6 @@ class GoalOrientedBot(Inferable, Trainable):
         self.db_result = None
         self.prev_action = np.zeros(self.n_actions, dtype=np.float32)
         self.network.reset_state()
-
-    def report(self):
-        return self.metrics.report()
-
-    def reset_metrics(self):
-        self.metrics.reset()
 
     def save(self):
         """Save the parameters of the model to a file."""
