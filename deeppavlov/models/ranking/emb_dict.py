9471c0a266138319367560ddb78a5160380b017b
Aleksey Lymar <yoptar@gmail.com>
2018-02-19 14:32:15 +0300

    refactor: use pathlib where available in the ranking model

--- deeppavlov/models/ranking/emb_dict.py
+++ deeppavlov/models/ranking/emb_dict.py
@@ -1,4 +1,3 @@
-import os
 import numpy as np
 from gensim.models.wrappers import FastText
 from gensim.models import Word2Vec
@@ -25,10 +24,10 @@ class EmbeddingsDict(object):
         self.truncating = truncating
         self.embeddings = embeddings
         self.embedding_dim = embedding_dim
-        self.emb_model_file = str(expand_path(emb_model_file))
-        self.emb_vocab_file = str(expand_path(emb_vocab_file))
-        if not expand_path(emb_model_file).exists():
-            download(source_url=download_url, dest_file_path=expand_path(emb_model_file))
+        self.emb_model_file = expand_path(emb_model_file)
+        self.emb_vocab_file = expand_path(emb_vocab_file)
+        if not self.emb_model_file.exists():
+            download(source_url=download_url, dest_file_path=self.emb_model_file)
 
         self.tok2emb = {}
         self.tok_index = {}
@@ -37,9 +36,9 @@ class EmbeddingsDict(object):
         self.load_items()
 
         if self.embeddings == "fasttext":
-            self.embeddings_model = FastText.load_fasttext_format(self.emb_model_file)
+            self.embeddings_model = FastText.load_fasttext_format(str(self.emb_model_file))
         elif self.embeddings == "word2vec":
-            self.embeddings_model = Word2Vec.load(self.emb_model_file)
+            self.embeddings_model = Word2Vec.load(str(self.emb_model_file))
 
         self.add_items(toks)
         self.create_emb_matrix()
@@ -60,34 +59,30 @@ class EmbeddingsDict(object):
 
     def save_items(self):
         """Save the dictionary tok2emb to the file."""
-        if self.emb_vocab_file is not None and not os.path.isfile(self.emb_vocab_file):
-            f = open(self.emb_vocab_file, 'w')
-            string = '\n'.join([el[0] + ' ' + self.emb2str(el[1]) for el in self.tok2emb.items()])
-            f.write(string)
-            f.close()
+        if not self.emb_vocab_file.is_file():
+            with self.emb_vocab_file.open('w') as f:
+                data = '\n'.join([el[0] + ' ' + self.emb2str(el[1]) for el in self.tok2emb.items()])
+                f.write(data)
 
     def emb2str(self, vec):
         """Return the string corresponding to given embedding vectors"""
-
-        string = ' '.join([str(el) for el in vec])
-        return string
+        data = ' '.join([str(el) for el in vec])
+        return data
 
     def load_items(self):
         """Initialize embeddings from the file."""
-
-        if self.emb_vocab_file is not None:
-            if not os.path.isfile(self.emb_vocab_file):
-                # print('There is no %s file provided. Initializing new dictionary.' % self.emb_vocab_file)
-                pass
-            else:
-                # print('Loading existing dictionary  from %s.' % self.emb_vocab_file)
-                with open(self.emb_vocab_file, 'r') as f:
-                    for line in f:
-                        values = line.rsplit(sep=' ', maxsplit=self.embedding_dim)
-                        assert(len(values) == self.embedding_dim + 1)
-                        tok = values[0]
-                        coefs = np.asarray(values[1:], dtype='float32')
-                        self.tok2emb[tok] = coefs
+        if not self.emb_vocab_file.is_file():
+            # print('There is no %s file provided. Initializing new dictionary.' % self.emb_vocab_file)
+            pass
+        else:
+            # print('Loading existing dictionary  from %s.' % self.emb_vocab_file)
+            with self.emb_vocab_file.open('r') as f:
+                for line in f:
+                    values = line.rsplit(sep=' ', maxsplit=self.embedding_dim)
+                    assert(len(values) == self.embedding_dim + 1)
+                    tok = values[0]
+                    coefs = np.asarray(values[1:], dtype='float32')
+                    self.tok2emb[tok] = coefs
 
     def make_ints(self, toks_li):
         ints_li = []
@@ -106,5 +101,3 @@ class EmbeddingsDict(object):
         self.emb_matrix = np.zeros((self.emb_index, self.embedding_dim))
         for tok, i in self.tok_index.items():
             self.emb_matrix[i] = self.tok2emb.get(tok)
-
-
