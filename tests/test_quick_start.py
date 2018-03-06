25ef8eb047c9428a88f465c77f494b38245630ca
Aleksey Lymar <yoptar@gmail.com>
2018-03-06 15:16:24 +0300

    feat: tests can be run from project root

--- tests/test_quick_start.py
+++ tests/test_quick_start.py
@@ -1,10 +1,11 @@
-# this test is designed to run from 'tests' folder
 import pytest
 from pathlib import Path
 import json
 import subprocess as sp
 import shutil
 
+
+tests_dir = Path(__file__, '..').resolve()
 
 # Mapping from model names to configs and corresponding Query-Response pairs
 MCQR = {"error_model": {"configs/error_model/brillmoore_wikitypos_en.json": ("error_model", "", ""),
@@ -25,11 +26,14 @@ MCQR = {"error_model": {"configs/error_model/brillmoore_wikitypos_en.json": ("er
 
 
 def setup_module():
-    src_dir = (Path() / "../deeppavlov").resolve()
-    tests_dir = Path().resolve()
-    (tests_dir / "configs").mkdir()
+    src_dir = tests_dir.parent / 'deeppavlov'
+    test_configs_path = tests_dir / 'configs'
+
+    shutil.rmtree(str(test_configs_path), ignore_errors=True)
+    test_configs_path.mkdir()
+
     for m_name, conf_files in MCQR.items():
-        (tests_dir / "configs" / m_name).mkdir()
+        test_configs_path.joinpath(m_name).mkdir()
         for conf_file, qr in conf_files.items():
             with (src_dir / conf_file).open() as fin:
                 config = json.load(fin)
@@ -42,7 +46,8 @@ def setup_module():
 
 
 def teardown_module():
-    shutil.rmtree("configs")
+    test_configs_path = tests_dir / 'configs'
+    shutil.rmtree(str(test_configs_path))
 
 
 def download(full=None):
@@ -63,17 +68,20 @@ class TestQuickStart(object):
         return out
 
     def test_downloaded_model_exist(self, model):
-        if not (Path() / "../download").resolve().exists():
+        if not tests_dir.parent.joinpath('download').exists():
             download()
-        assert (Path() / "../download/" / model).exists(), f"{model} was not downloaded"
+        assert tests_dir.parent.joinpath('download', model).exists(), f"{model} was not downloaded"
 
     def test_interact_pretrained_model(self, model):
         for c, fqr in MCQR[model].items():
+            c = tests_dir / c
             assert self.interact(c), f"Error in interacting with pretrained {model}: {c}"
 
     def test_consecutive_training_and_interacting(self, model):
         for c, fqr in MCQR[model].items():
-            shutil.rmtree("../download/" + fqr[0],  ignore_errors=True)
+            c = tests_dir / c
+            model_path = tests_dir.parent / 'download' / fqr[0]
+            shutil.rmtree(str(model_path),  ignore_errors=True)
             p = sp.run(["python3", "-m", "deeppavlov.deep", "train", c])
             assert p.returncode == 0, f"Training process of {model} with {c} returned non-zero exit code"
             assert self.interact(c), f"Error in interacting with 1-epoch trained {model}: {c}"
