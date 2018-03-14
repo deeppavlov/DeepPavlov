a3e40d2c7a59e81906b3afe3d434a40ade854447
dilyararimovna <dilyara.rimovna@gmail.com>
2018-03-14 16:28:12 +0300

    fix: delete dowload all true

--- tests/test_quick_start.py
+++ tests/test_quick_start.py
@@ -110,8 +110,7 @@ class TestQuickStart(object):
 
     def test_downloaded_model_existence(self, model, conf_file, model_dir):
         if not download_path.exists():
-            # download()
-            download(True)
+            download()
         assert download_path.joinpath(model_dir).exists(), f"{model_dir} was not downloaded"
 
     def test_interacting_pretrained_model(self, model, conf_file, model_dir):
