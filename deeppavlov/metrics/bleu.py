c365ce7d56e3dd00fb751edba001810fa7455348
Mariia Vikhreva <mary.vikhreva@gmail.com>
2018-03-21 15:23:10 +0300

    feat: add per_item_bleu

--- deeppavlov/metrics/bleu.py
+++ deeppavlov/metrics/bleu.py
@@ -11,6 +11,15 @@ def bleu(y_true, y_predicted):
                  for y1, y2 in zip(y_true, y_predicted))
     return sum(bleu_list) / examples_len if examples_len else 0.
 
+@register_metric('per_item_bleu')
+def per_item_bleu(y_true, y_predicted):
+    y_true = list(itertools.chain(*y_true))
+    y_predicted = itertools.chain(*y_predicted)
+    examples_len = len(y_true)
+    bleu_list = (sentence_bleu([y2.lower().split()], y1.lower().split())\
+                 for y1, y2 in zip(y_true, y_predicted))
+    return sum(bleu_list) / examples_len if examples_len else 0.
+
 @register_metric('per_item_dialog_bleu')
 def per_item_dialog_bleu(y_true, y_predicted):
     y_true = [y['text'] for dialog in y_true for y in dialog]
@@ -19,3 +28,4 @@ def per_item_dialog_bleu(y_true, y_predicted):
     bleu_list = (sentence_bleu([y2.lower().split()], y1.lower().split())\
                  for y1, y2 in zip(y_true, y_predicted))
     return sum(bleu_list) / examples_len if examples_len else 0.
+
