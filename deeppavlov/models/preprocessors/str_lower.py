8cdd8eb8d754c7a282cf6d12bac4f9308c853a24
Mariia Vikhreva <mary.vikhreva@gmail.com>
2018-03-23 18:00:45 +0300

    style: minor styling

--- deeppavlov/models/preprocessors/str_lower.py
+++ deeppavlov/models/preprocessors/str_lower.py
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-from typing import List, Dict
+from typing import List
 
 from deeppavlov.core.common.registry import register
 from deeppavlov.core.models.component import Component
