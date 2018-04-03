3e60bd7f577d858c32941092c0148489f7f104bb
Olga <ol.gure@gmail.com>
2018-04-03 17:27:57 +0300

    refactor: DataFittingIterator is like DataLEarningIterator

--- deeppavlov/core/data/data_fitting_iterator.py
+++ deeppavlov/core/data/data_fitting_iterator.py
@@ -32,52 +32,29 @@ class DataFittingIterator:
     """
 
     def __init__(self, data: List[str], doc_ids: List[Any] = None,
-                 seed: int = None, shuffle: bool = False, batch_size: int = 1000,
+                 seed: int = None, shuffle: bool = True,
                  *args, **kwargs) -> None:
 
-        self._batch_size = batch_size
-        self._shuffle = shuffle
-        self._random = Random(seed)
+        self.shuffle = shuffle
+        self.random = Random(seed)
         self.data = data
         self.doc_index = doc_ids or self.get_doc_ids()
         self.data = data
 
-    @property
-    def shuffle(self):
-        return self._shuffle
-
-    @shuffle.setter
-    def shuffle(self, shuffle: bool):
-        self._shuffle = shuffle
-
-    @property
-    def random(self):
-        return self._random
-
-    @random.setter
-    def random(self, seed: int):
-        self._random = Random(seed)
-
-    @property
-    def batch_size(self):
-        return self._batch_size
-
-    @batch_size.setter
-    def batch_size(self, batch_size):
-        if batch_size < 0:
-            raise ValueError("Batch size should be a positive number!")
-        self._batch_size = batch_size
-
     def get_doc_ids(self):
         return list(range(len(self.data)))
 
-    def gen_batches(self) -> Generator[Tuple[List[list], List[int]], Any, None]:
+    def gen_batches(self, batch_size: int, shuffle: bool = None)\
+            -> Generator[Tuple[List[list], List[int]], Any, None]:
 
-        if self._shuffle:
-            self.random.shuffle(self.doc_index)
+        if shuffle is None:
+            shuffle = self.shuffle
 
-        batches = [self.doc_index[i:i + self._batch_size] for i in
-                   range(0, len(self.doc_index), self._batch_size)]
+        if shuffle:
+            self.random.shuffle(self.data)
+
+        batches = [self.doc_index[i:i + batch_size] for i in
+                   range(0, len(self.doc_index), batch_size)]
 
         # DEBUG
         # len_batches = len(batches)
