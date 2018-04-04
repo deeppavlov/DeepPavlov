1552fbacf1939773652ae0ee67b006ec9dbab92c
mu-arkhipov <arkhipov@yahoo.com>
2018-04-05 00:35:08 +0300

    feat: sanitizer added

---
+++ deeppavlov/models/preprocessors/sanitizer.py
@@ -0,0 +1,24 @@
+import unicodedata
+import sys
+
+from deeppavlov.core.models.component import Component
+from deeppavlov.core.common.registry import register
+
+
+@register('sanitizer')
+class Sanitizer(Component):
+    def __init__(self):
+        """Remove all combining characters like diacritical marks from tokens"""
+        self.combining_characters = dict.fromkeys([c for c in range(sys.maxunicode)
+                                                   if unicodedata.combining(chr(c))])
+
+    def __call__(self, tokens_batch, **kwargs):
+        """Takes batch of tokens and returns the batch with sanitized tokens"""
+        sanitized_batch = []
+        for utterance in tokens_batch:
+            sanitized_utterance = []
+            for token in utterance:
+                token = unicodedata.normalize('NFD', token)
+                sanitized_utterance.append(token.translate(self.combining_characters))
+            sanitized_batch.append(sanitized_utterance)
+        return sanitized_batch
