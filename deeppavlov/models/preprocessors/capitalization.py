e249d885136685027ea4811aa468fb4747b8bc93
mu-arkhipov <arkhipov@yahoo.com>
2018-04-04 21:10:18 +0300

    fix: fix n_features property

--- deeppavlov/models/preprocessors/capitalization.py
+++ deeppavlov/models/preprocessors/capitalization.py
@@ -12,11 +12,11 @@ class CapitalizationPreprocessor(Component):
     """
     def __init__(self, pad_zeros=True):
         self.pad_zeros = pad_zeros
-        self.n_features = 4
+        self._num_of_features = 4
 
     @property
     def n_features(self):
-        return self.n_features
+        return self._num_of_features
 
     def __call__(self, tokens_batch, **kwargs):
         cap_batch = []
@@ -27,18 +27,19 @@ class CapitalizationPreprocessor(Component):
             for token in utterance:
                 cap = np.zeros(4, np.float32)
                 # Check the case and produce corresponding one-hot
-                if token[0].islower():
-                    cap[0] = 1
-                elif len(token) == 1 and token[0].isupper():
-                    cap[1] = 1
-                elif len(token) > 1 and token[0].isupper():
-                    cap[2] = 1
-                elif all(ch.isupper() for ch in token):
-                    cap[3] = 1
-                else:
-                    raise RuntimeError('Unknown capitalization of token {}!'.format(token))
+                if len(token) > 0:
+                    if token[0].islower():
+                        cap[0] = 1
+                    elif len(token) == 1 and token[0].isupper():
+                        cap[1] = 1
+                    elif len(token) > 1 and token[0].isupper() and any(ch.islower() for ch in token):
+                        cap[2] = 1
+                    elif all(ch.isupper() for ch in token):
+                        cap[3] = 1
+                    else:
+                        raise RuntimeError('Unknown capitalization of token {}!'.format(token))
                 cap_list.append(cap)
-            cap_batch.append(cap)
+            cap_batch.append(cap_list)
         if self.pad_zeros:
             return zero_pad(cap_batch)
         else:
