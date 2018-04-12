873a63c85be4e8fdb61e7a56c1f427afdc98dbcd
mu-arkhipov <arkhipov@yahoo.com>
2018-04-12 18:48:21 +0300

    doc: docs for variational dropout

--- deeppavlov/core/layers/tf_layers.py
+++ deeppavlov/core/layers/tf_layers.py
@@ -631,6 +631,19 @@ def cudnn_bi_lstm(units,
 
 
 def variational_dropout(units, keep_prob, fixed_mask_dims=(1,)):
+    """ Dropout with the same drop mask for all fixed_mask_dims
+
+    Args:
+        units: a tensor, usually with shapes [B x T x F], where
+            B - batch size
+            T - tokens dimension
+            F - feature dimension
+        keep_prob: keep probability
+        fixed_mask_dims: in these dimensions the mask will be the same
+
+    Returns:
+        dropped units tensor
+    """
     units_shape = tf.shape(units)
     noise_shape = [units_shape[n] for n in range(len(units.shape))]
     for dim in fixed_mask_dims:
