25b49881af18371e44dbbc25a9e5277bb3318076
AlexeySorokin <alexey.sorokin@list.ru>
2018-04-17 11:35:30 +0300

    feature: add CapitalizationPreprocessor (for NER) and LowercasePreprocessor (for tagging)

---
+++ deeppavlov/models/preprocessors/capitalization.py
@@ -0,0 +1,90 @@
+import numpy as np
+import re
+
+from deeppavlov.core.common.registry import register
+from deeppavlov.core.models.component import Component
+from deeppavlov.core.data.utils import zero_pad
+
+from deeppavlov.models.tokenizers import nltk_tokenizer
+
+
+class CapitalizationPreprocessor(Component):
+    """ Patterns:
+        - no capitals
+        - single capital single character
+        - single capital multiple characters
+        - all capitals multiple characters
+    """
+    def __init__(self, pad_zeros=True):
+        self.pad_zeros = pad_zeros
+        self._num_of_features = 4
+
+    @property
+    def n_features(self):
+        return self._num_of_features
+
+    def __call__(self, tokens_batch, **kwargs):
+        cap_batch = []
+        max_batch_len = 0
+        for utterance in tokens_batch:
+            cap_list = []
+            max_batch_len = max(max_batch_len, len(utterance))
+            for token in utterance:
+                cap = np.zeros(4, np.float32)
+                # Check the case and produce corresponding one-hot
+                if len(token) > 0:
+                    if token[0].islower():
+                        cap[0] = 1
+                    elif len(token) == 1 and token[0].isupper():
+                        cap[1] = 1
+                    elif len(token) > 1 and token[0].isupper() and any(ch.islower() for ch in token):
+                        cap[2] = 1
+                    elif all(ch.isupper() for ch in token):
+                        cap[3] = 1
+                    else:
+                        raise RuntimeError('Unknown capitalization of token {}!'.format(token))
+                cap_list.append(cap)
+            cap_batch.append(cap_list)
+        if self.pad_zeros:
+            return zero_pad(cap_batch)
+        else:
+            return cap_batch
+
+
+def process_word(word, to_lower=False, append_case=None):
+    if all(x.isupper() for x in word) and len(word) > 1:
+        uppercase = "<ALL_UPPER>"
+    elif word[0].isupper():
+        uppercase = "<FIRST_UPPER>"
+    else:
+        uppercase = None
+    if to_lower:
+        word = word.lower()
+    if word.isdigit():
+        answer = ["<DIGIT>"]
+    elif word.startswith("http://") or word.startswith("www."):
+        answer = ["<HTTP>"]
+    else:
+        answer = list(word)
+    if to_lower and uppercase is not None:
+        if append_case == "first":
+            answer = [uppercase] + answer
+        elif append_case == "last":
+            answer = answer + [uppercase]
+    return tuple(answer)
+
+
+@register('lowercase_preprocessor')
+class LowercasePreprocessor(Component):
+
+    def __init__(self, to_lower=True, append_case="first", *args, **kwargs):
+        self.to_lower = to_lower
+        self.append_case = append_case
+
+    def __call__(self, tokens_batch, **kwargs):
+        answer = []
+        for elem in tokens_batch:
+            if isinstance(elem, str):
+                elem = [x for x in re.split("(\w+|[,.])", elem) if x.strip() != ""]
+            answer.append([process_word(x, self.to_lower, self.append_case) for x in elem])
+        return answer
