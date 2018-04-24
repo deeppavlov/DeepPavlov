80ce2120eac3a0eb3df50b1cb184a811937f882d
Olga <ol.gure@gmail.com>
2018-04-24 11:10:24 +0300

    refactor: add en_ to English configs

--- deeppavlov/run_model.py
+++ deeppavlov/run_model.py
@@ -30,10 +30,10 @@ from deeppavlov.core.commands.infer import interact_model
 # PIPELINE_CONFIG_PATH = 'configs/go_bot/gobot_dstc2_all.json'
 # PIPELINE_CONFIG_PATH = 'configs/go_bot/gobot_dstc2_minimal.json'
 # PIPELINE_CONFIG_PATH = 'configs/squad/squad.json'
-# PIPELINE_CONFIG_PATH = 'configs/odqa/ranker_prod.json'
+# PIPELINE_CONFIG_PATH = 'configs/odqa/en_ranker_prod.json'
 # PIPELINE_CONFIG_PATH = 'configs/seq2seq_go_bot/bot_kvret.json'
-# PIPELINE_CONFIG_PATH = 'configs/odqa/ranker_prod.json'
-# PIPELINE_CONFIG_PATH = 'configs/odqa/odqa_infer_prod.json'
+# PIPELINE_CONFIG_PATH = 'configs/odqa/en_ranker_prod.json'
+# PIPELINE_CONFIG_PATH = 'configs/odqa/en_odqa_infer_prod.json'
 PIPELINE_CONFIG_PATH = 'configs/odqa/ranker_test.json'
 
 train_model_from_config(PIPELINE_CONFIG_PATH)
