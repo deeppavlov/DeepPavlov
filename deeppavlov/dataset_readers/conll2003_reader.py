68b08c0f49e000c6f4308914d2bd11c9cdebe832
mu-arkhipov <arkhipov@yahoo.com>
2018-04-26 14:22:36 +0300

    feat: conll2003 dataset with pos

--- deeppavlov/dataset_readers/conll2003_reader.py
+++ deeppavlov/dataset_readers/conll2003_reader.py
@@ -1,5 +1,4 @@
 from deeppavlov.core.data.utils import download_decompress
-import sys
 
 from deeppavlov.core.data.dataset_reader import DatasetReader
 from pathlib import Path
@@ -10,7 +9,7 @@ from deeppavlov.core.common.registry import register
 class Conll2003DatasetReader(DatasetReader):
 
     def download_conll(self, dir_path):
-        download_decompress('http://lnsigo.mipt.ru/export/datasets/conll2003.tar.gz', dir_path)
+        download_decompress('http://lnsigo.mipt.ru/export/datasets/conll2003_v2.tar.gz', dir_path)
 
     def read(self, dir_path: str, dataset_name='conll2003'):
         dir_path = Path(dir_path)
@@ -32,17 +31,26 @@ class Conll2003DatasetReader(DatasetReader):
     def parse_ner_file(file_name: Path):
         samples = []
         with file_name.open() as f:
-            tokens = []
-            tags = []
+            tokens = ['<DOCSTART>']
+            pos_tags = ['O']
+            tags = ['O']
             for line in f:
                 # Check end of the document
-                if len(line) < 2 or 'DOCSTART' in line:
+                if 'DOCSTART' in line:
+                    if len(tokens) > 1:
+                        samples.append(((tokens, pos_tags), tags, ))
+                        tokens = ['<DOCSTART>']
+                        pos_tags = ['O']
+                        tags = ['O']
+                elif len(line) < 2:
                     if len(tokens) > 0:
-                        samples.append((tokens, tags, ))
+                        samples.append(((tokens, pos_tags), tags, ))
                         tokens = []
+                        pos_tags = []
                         tags = []
                 else:
-                    token, *_, tag = line.split()
+                    token, *_, pos, tag = line.split()
                     tokens.append(token)
+                    pos_tags.append(pos)
                     tags.append(tag)
         return samples
