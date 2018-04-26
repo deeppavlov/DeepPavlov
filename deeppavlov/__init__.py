6ce8ad0fb2254510b471a748f89ac873cc57ec85
mu-arkhipov <arkhipov@yahoo.com>
2018-04-26 12:39:22 +0300

    feat: lazy tokenizer added

--- deeppavlov/__init__.py
+++ deeppavlov/__init__.py
@@ -43,14 +43,12 @@ import deeppavlov.models.embedders.dict_embedder
 import deeppavlov.models.embedders.glove_embedder
 import deeppavlov.models.embedders.bow_embedder
 import deeppavlov.models.ner.slotfill
-import deeppavlov.models.ner.ner
 import deeppavlov.models.ner.ner_ontonotes
 import deeppavlov.models.spellers.error_model.error_model
 import deeppavlov.models.trackers.hcn_at
 import deeppavlov.models.trackers.hcn_et
 import deeppavlov.models.preprocessors.str_lower
 import deeppavlov.models.preprocessors.squad_preprocessor
-import deeppavlov.models.ner.ner
 import deeppavlov.models.tokenizers.spacy_tokenizer
 import deeppavlov.models.tokenizers.split_tokenizer
 import deeppavlov.models.squad.squad
@@ -74,6 +72,7 @@ import deeppavlov.models.preprocessors.capitalization
 import deeppavlov.models.ner.network_refactored
 import deeppavlov.models.preprocessors.field_getter
 import deeppavlov.models.preprocessors.sanitizer
+import deeppavlov.models.preprocessors.lazy_tokenizer
 
 
 import deeppavlov.metrics.accuracy
