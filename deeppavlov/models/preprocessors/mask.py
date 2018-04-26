6f6ab07ee6165ddfea222f1bb602a07f06afa142
mu-arkhipov <arkhipov@yahoo.com>
2018-04-26 14:03:43 +0300

    fix: docstring fix

--- deeppavlov/models/preprocessors/mask.py
+++ deeppavlov/models/preprocessors/mask.py
@@ -26,7 +26,7 @@ class Mask(Component):
 
     @staticmethod
     def __call__(tokens_batch, **kwargs):
-        """Takes batch of tokens and returns the lemmatized tokens"""
+        """Takes batch of tokens and returns the masks of corresponding length"""
         batch_size = len(tokens_batch)
         max_len = max(len(utt) for utt in tokens_batch)
         mask = np.zeros([batch_size, max_len], dtype=np.float32)
