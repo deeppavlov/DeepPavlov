ce5fb91e61d7a7614dd1472b8f082932c3eccded
mu-arkhipov <arkhipov@yahoo.com>
2018-04-26 09:51:41 +0300

    feat: sanitizer extended

--- deeppavlov/models/preprocessors/sanitizer.py
+++ deeppavlov/models/preprocessors/sanitizer.py
@@ -1,5 +1,6 @@
 import unicodedata
 import sys
+import re
 
 from deeppavlov.core.models.component import Component
 from deeppavlov.core.common.registry import register
@@ -7,12 +8,14 @@ from deeppavlov.core.common.registry import register
 
 @register('sanitizer')
 class Sanitizer(Component):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, diacritical=True, nums=False, *args, **kwargs):
         """Remove all combining characters like diacritical marks from tokens"""
+        self.diacritical = diacritical
+        self.nums = nums
         self.combining_characters = dict.fromkeys([c for c in range(sys.maxunicode)
                                                    if unicodedata.combining(chr(c))])
 
-    def __call__(self, tokens_batch, **kwargs):
+    def filter_diacritical(self, tokens_batch):
         """Takes batch of tokens and returns the batch with sanitized tokens"""
         sanitized_batch = []
         for utterance in tokens_batch:
@@ -22,3 +25,16 @@ class Sanitizer(Component):
                 sanitized_utterance.append(token.translate(self.combining_characters))
             sanitized_batch.append(sanitized_utterance)
         return sanitized_batch
+
+    def replace_nums(self, tokens_batch):
+        sanitized_batch = []
+        for utterance in tokens_batch:
+            sanitized_batch.append([re.sub('[0-9]', '1', token) for token in utterance])
+        return sanitized_batch
+
+    def __call__(self, tokens_batch, **kwargs):
+        if self.filter_diacritical:
+            tokens_batch = self.filter_diacritical(tokens_batch)
+        if self.nums:
+            tokens_batch = self.replace_nums(tokens_batch)
+        return tokens_batch
