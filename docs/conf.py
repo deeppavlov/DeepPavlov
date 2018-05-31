24a5607e22ac4bdf4620cb9f8f491d109dbc4c49
nikolay-bushkov <bushkov@phystech.edu>
2018-05-31 15:47:21 +0300

    docs: remove .readthedocs.yml

--- docs/conf.py
+++ docs/conf.py
@@ -6,17 +6,18 @@
 # full list see the documentation:
 # http://www.sphinx-doc.org/en/master/config
 
+import sphinx_rtd_theme
+
 # -- Path setup --------------------------------------------------------------
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 
-import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
