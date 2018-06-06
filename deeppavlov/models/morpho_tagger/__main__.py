8c195975a6da299e05ecdc49ddb8a3be2d68890c
AlexeySorokin <alexey.sorokin@list.ru>
2018-06-06 12:24:06 +0300

    refactor: rename models/morphotagger/main.py to double underscore

--- deeppavlov/models/morpho_tagger/__main__.py
+++ deeppavlov/models/morpho_tagger/__main__.py
@@ -4,7 +4,6 @@ from deeppavlov.models.morpho_tagger.common import predict_with_model
 from deeppavlov.deep import find_config
 from deeppavlov.download import deep_download
 
-
 parser = argparse.ArgumentParser()
 parser.add_argument("config_path", help="path to file with prediction configuration")
 parser.add_argument("-d", "--download", action="store_true", help="download model components")
