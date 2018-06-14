558dcb7d737e45c8dfae90840fa4ade9f165e6e0
dilyararimovna <dilyara.rimovna@gmail.com>
2018-06-14 16:26:20 +0300

    fix: mutation of choice parameter

--- deeppavlov/models/evolution/neuroevolution_param_generator.py
+++ deeppavlov/models/evolution/neuroevolution_param_generator.py
@@ -559,7 +559,8 @@ class NetworkAndParamsEvolution:
                     new_mutated_value = val
                 elif params_dict[param].get("choice"):
                     # TODO: mutation of this parameters
-                    new_mutated_value = param_value
+                    # new_mutated_value = param_value
+                    new_mutated_value = self.sample_params(**{param: params_dict[param]})[param]
                 else:
                     new_mutated_value = param_value
             else:
