71cd5b682f4918de049c5a0e156335ba755970d5
dilyararimovna <dilyara.rimovna@gmail.com>
2018-06-14 10:28:58 +0300

    fix: gpus number

--- deeppavlov/models/evolution/run_param_evolution.py
+++ deeppavlov/models/evolution/run_param_evolution.py
@@ -40,7 +40,7 @@ def score_population(population, population_size, result_file):
                 save_json(population[i], f_name)
 
                 procs.append(Popen("CUDA_VISIBLE_DEVICES={} python ./models/evolution/train_phenotype.py {}"
-                             " 1>{}/out.txt 2>{}/err.txt".format(gpus[i],
+                             " 1>{}/out.txt 2>{}/err.txt".format(gpus[j],
                                                                  str(f_name),
                                                                  str(save_path),
                                                                  str(save_path)
