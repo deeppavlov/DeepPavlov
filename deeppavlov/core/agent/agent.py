63d6ef710fc227fab97338e789bb804b3bf6f429
Aleksey Lymar <yoptar@gmail.com>
2018-06-18 11:30:41 +0300

    refactor: finalize agent.py code

--- deeppavlov/core/agent/agent.py
+++ deeppavlov/core/agent/agent.py
@@ -1,22 +1,8 @@
-#%%
 from collections import defaultdict
 from typing import List
 import random
 
 from deeppavlov.core.models.component import Component
-
-
-class RandomFilter(Component):
-    def __init__(self, size, *args, **kwargs):
-        self.size = size
-
-    def __call__(self, utterances, batch_history):
-        res = []
-        for _ in utterances:
-            r = [bool(random.getrandbits(1)) for _ in range(self.size)]
-            r[-1] = r[-1] or not any(r)
-            res.append(r)
-        return res
 
 
 class RandomSelector(Component):
@@ -28,9 +14,9 @@ class RandomSelector(Component):
 
 
 class Agent(Component):
-    def __init__(self, skills: List[Component], skills_filter=None, skills_selector=None, *args, **kwargs):
+    def __init__(self, skills: List[Component], skills_selector=None, skills_filter=None, *args, **kwargs):
         self.skills = skills
-        self.skills_filter = skills_filter or (lambda u, _: [[True] * len(skills)] * len(u)) or RandomFilter(len(skills))
+        self.skills_filter = skills_filter or (lambda u, _: [[True] * len(skills)] * len(u))
         self.skills_selector = skills_selector or RandomSelector()
         self.history = defaultdict(list)
         self.states = defaultdict(lambda: [None] * len(self.skills))
@@ -59,16 +45,3 @@ class Agent(Component):
             history.append(utterance)
             history.append(response)
         return responses
-
-
-if __name__ == '__main__':
-    #%%
-
-    c1 = lambda u, b, s: [('1', 1, None) for _ in u]
-    c2 = lambda u, b, s: [('2', 1, None) for _ in u]
-    c3 = lambda u, b, s: [('3', 1, None) for _ in u]
-
-    a = Agent([c1, c2, c3])
-
-    #%%
-    a(['Привет!', "Пока!", "И чо?", "Нет же"])
