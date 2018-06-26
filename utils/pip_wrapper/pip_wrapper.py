f88fc1bde2039dc5d0086fef5a3183049e09181f
Aleksey Lymar <yoptar@gmail.com>
2018-06-26 15:21:42 +0300

    fix: accept special characters in pip wrapper install

--- utils/pip_wrapper/pip_wrapper.py
+++ utils/pip_wrapper/pip_wrapper.py
@@ -3,4 +3,4 @@ import sys
 
 
 def install(package):
-    return subprocess.check_call(f'{sys.executable} -m pip install {package}', shell=True)
+    return subprocess.check_call([sys.executable, '-m', 'pip', 'install', package.replace(' ', '')])
