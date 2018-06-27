5a94c995bb2dc93558d23448fed45a682edc5b62
Aleksey Lymar <yoptar@gmail.com>
2018-06-27 15:59:03 +0300

    feat: allow installation of multiple packages at once with pip wrapper

--- utils/pip_wrapper/pip_wrapper.py
+++ utils/pip_wrapper/pip_wrapper.py
@@ -1,6 +1,8 @@
+import re
 import subprocess
 import sys
 
 
-def install(package):
-    return subprocess.check_call([sys.executable, '-m', 'pip', 'install', package.replace(' ', '')])
+def install(*packages):
+    return subprocess.check_call([sys.executable, '-m', 'pip', 'install',
+                                  *[re.sub(r'\s', '', package) for package in packages]])
