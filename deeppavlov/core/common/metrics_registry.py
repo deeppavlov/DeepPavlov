e13971dfe6073837e385d377c1cf84aaf35863ab
Aleksey Lymar <yoptar@gmail.com>
2018-07-03 14:51:07 +0300

    feat: add metrics registry file

--- deeppavlov/core/common/metrics_registry.py
+++ deeppavlov/core/common/metrics_registry.py
@@ -1,19 +1,39 @@
+import importlib
+from pathlib import Path
+import json
+
 from deeppavlov.core.common.errors import ConfigError
 from deeppavlov.core.common.log import get_logger
 
 
 log = get_logger(__name__)
 
-_REGISTRY = {}
+_registry_path = Path(__file__).parent / 'metrics_registry.json'
+if _registry_path.exists():
+    with _registry_path.open(encoding='utf-8') as f:
+        _REGISTRY = json.load(f)
+else:
+    _REGISTRY = {}
+
+
+def fn_from_str(name: str) -> type:
+    try:
+        module_name, fn_name = name.split(':')
+    except ValueError:
+        raise ConfigError('Expected function description in a `module.submodules:function_name` form, but got `{}`'
+                          .format(name))
+
+    return getattr(importlib.import_module(module_name), fn_name)
 
 
 def register_metric(metric_name):
-    def decorate(f):
-        if metric_name in _REGISTRY:
+    def decorate(fn):
+        fn_name = fn.__module__ + ':' + fn.__name__
+        if metric_name in _REGISTRY and _REGISTRY[metric_name] != fn_name:
             log.warning('"{}" is already registered as a metric name, the old function will be ignored'
                         .format(metric_name))
-        _REGISTRY[metric_name] = f
-        return f
+        _REGISTRY[metric_name] = fn_name
+        return fn
     return decorate
 
 
@@ -21,4 +41,4 @@ def get_metrics_by_names(names: list):
     not_found = [name for name in names if name not in _REGISTRY]
     if not_found:
         raise ConfigError('Names {} are not registered as metrics'.format(not_found))
-    return [_REGISTRY[name] for name in names]
+    return [fn_from_str(_REGISTRY[name]) for name in names]
--- utils/prepare/registry.py
+++ utils/prepare/registry.py
@@ -2,13 +2,19 @@ import pkgutil
 import json
 
 import deeppavlov
-from deeppavlov.core.common.registry import _registry_path, _REGISTRY
+from deeppavlov.core.common.registry import _registry_path as c_registry_path, _REGISTRY as C_REGISTRY
+from deeppavlov.core.common.metrics_registry import _registry_path as m_registry_path, _REGISTRY as M_REGISTRY
 
 
 if __name__ == '__main__':
-    _REGISTRY.clear()
+    C_REGISTRY.clear()
+    M_REGISTRY.clear()
+
     for _, pkg_name, _ in pkgutil.walk_packages(deeppavlov.__path__, deeppavlov.__name__+'.'):
         __import__(pkg_name)
 
-    with _registry_path.open('w', encoding='utf-8') as f:
-        json.dump(dict(sorted(_REGISTRY.items())), f, indent=2)
+    with c_registry_path.open('w', encoding='utf-8') as f:
+        json.dump(dict(sorted(C_REGISTRY.items())), f, indent=2)
+
+    with m_registry_path.open('w', encoding='utf-8') as f:
+        json.dump(dict(sorted(M_REGISTRY.items())), f, indent=2)
