faf47ce41e4a0e0f71c66d125a8525a1ef7073a3
mu-arkhipov <arkhipov@yahoo.com>
2018-08-24 13:33:18 +0300

    docs: random emb matrix assembler docs added

--- deeppavlov/models/preprocessors/assemble_embeddings_matrix.py
+++ deeppavlov/models/preprocessors/assemble_embeddings_matrix.py
@@ -85,6 +85,18 @@ class EmbeddingsMatrixAssembler:
 
 @register('random_emb_mat')
 class RandomEmbeddingsMatrix:
-    """Assembles matrix of random embeddings."""
+    """Assembles matrix of random embeddings.
+
+    Args:
+        vocab_len: length of the vocabulary (number of tokens in it)
+        emb_dim: dimensionality of the embeddings
+
+    Attributes:
+        dim: dimensionality of the embeddings
+    """
     def __init__(self, vocab_len, emb_dim, *args, **kwargs):
         self.emb_mat = np.random.randn(vocab_len, emb_dim).astype(np.float32) / np.sqrt(emb_dim)
+
+    @property
+    def dim(self):
+        return self.emb_mat.shape[1]
