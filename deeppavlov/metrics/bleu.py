95d1b1f0dc466e1f7741867a6e5994776aaba19c
com <vasili.konov@gmail.com>
2018-08-28 18:06:39 +0300

    feat: add custom bleu

--- deeppavlov/metrics/bleu.py
+++ deeppavlov/metrics/bleu.py
@@ -13,11 +13,33 @@
 # limitations under the License.
 
 import itertools
-from nltk.translate.bleu_score import corpus_bleu
+from nltk.translate.bleu_score import corpus_bleu, sentence_bleu, SmoothingFunction, brevity_penalty, closest_ref_length
 from deeppavlov.metrics.google_bleu import compute_bleu
 
 from deeppavlov.core.common.metrics_registry import register_metric
 
+smooth = SmoothingFunction()
+
+@register_metric('bleu_advanced')
+def bleu_advanced(y_true, y_predicted, weights=(1,), smoothing_function=smooth.method1, auto_reweigh=False, penalty=True):
+    examples_len = len(y_true)
+    bleu_list = []
+
+    for y1, y2 in zip(y_true, y_predicted):
+        bleu_measure = sentence_bleu([y2], y1, weights, smoothing_function, auto_reweigh)
+
+        hyp_len = len(y1)
+        hyp_lengths = hyp_len
+        ref_lengths = closest_ref_length([y2], hyp_len) # 1
+
+        bp = brevity_penalty(ref_lengths, hyp_lengths)
+
+        if penalty == True:
+            bleu_list.append(bleu_measure)
+        else:
+            bleu_list.append(bleu_measure/bp)
+
+    return sum(bleu_list) / examples_len if examples_len else 0.
 
 @register_metric('bleu')
 def bleu(y_true, y_predicted):
