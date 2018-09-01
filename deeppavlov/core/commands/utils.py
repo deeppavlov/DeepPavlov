98ccd4aecfd3b0ce07706fac4e46763101c733b7
Nikolay Bushkov <bushkov@phystech.edu>
2018-09-01 09:40:00 +0300

    docs: core and NER
    * docs: correct license info
    
    * docs: add core.commands
    
    * docs: add core.common
    
    * fix: examples for rest api
    
    * docs: correct license info
    
    * docs: embeddings matrix assembler docs added
    
    * docs: random emb matrix assembler docs added
    
    * docs: random emb matrix assembler docs added
    
    * docs: capitalization featurizer docs added
    
    * docs: sanitizer component docified
    
    * docs: typing added to the embeddings matrix assembler
    
    * docs: add core.commands
    
    * docs: add core.common
    
    * chore: remove commands.utils from rendering
    
    * docs: fix minor issues from docs/ner
    
    * docs: fix REST API examples
    
    * docs: add core.models
    
    * docs: NER and SlotFill scores and configs added to docs/intro/features.rst
    
    * docs: NER and SlotFill scores and configs added to docs/intro/features.rst
    
    * feat: add documentation in features file
    
    * docs: add comparison for speller
    
    * docs: replace random_emb_matrix

--- deeppavlov/core/commands/utils.py
+++ deeppavlov/core/commands/utils.py
@@ -1,18 +1,16 @@
-"""
-Copyright 2017 Neural Networks and Deep Learning lab, MIPT
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-"""
+# Copyright 2017 Neural Networks and Deep Learning lab, MIPT
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 from pathlib import Path
 
@@ -22,10 +20,8 @@ from deeppavlov.core.common import paths
 
 import os
 
-def set_deeppavlov_root(config: dict):
-    """
-    Make a serialization user dir.
-    """
+def set_deeppavlov_root(config: dict) -> None:
+    """Make a serialization user dir."""
     try:
         deeppavlov_root = Path(config['deeppavlov_root'])
     except KeyError:
@@ -37,12 +33,14 @@ def set_deeppavlov_root(config: dict):
 
 
 def get_deeppavlov_root() -> Path:
+    """Return DeepPavlov root directory."""
     if not paths.deeppavlov_root:
         set_deeppavlov_root({})
     return paths.deeppavlov_root
 
 
 def expand_path(path: Union[str, Path]) -> Path:
+    """Make path expansion."""
     return get_deeppavlov_root() / Path(path).expanduser()
 
 
@@ -59,12 +57,11 @@ def is_file_exist(path: Union[str, Path]):
 
 
 def is_empty(d: Path) -> bool:
-    """
-    Check if directory is empty.
-    """
+    """Check if directory is empty."""
     return not bool(list(d.iterdir()))
 
 
-def import_packages(packages: list):
+def import_packages(packages: list) -> None:
+    """Simple function to import packages from list."""
     for package in packages:
         __import__(package)
