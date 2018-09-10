272fb8d7c01601e7107eeb8cca7a8ce7c7f60924
Aleksey Lymar <yoptar@gmail.com>
2018-09-10 17:16:15 +0300

    style: use patlib instead of os in cross_validation

--- deeppavlov/core/common/cross_validation.py
+++ deeppavlov/core/common/cross_validation.py
@@ -1,31 +1,25 @@
-"""
-Copyright 2017 Neural Networks and Deep Learning lab, MIPT
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-"""
-
+# Copyright 2017 Neural Networks and Deep Learning lab, MIPT
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import shutil
 from pathlib import Path
-import sys
+
 import numpy as np
-import os
+from sklearn.model_selection import KFold
 
-p = (Path(__file__) / ".." / "..").resolve()
-sys.path.append(str(p))
-
-from deeppavlov.core.common.file import read_json, save_json
+from deeppavlov.core.common.file import read_json
 from deeppavlov.core.common.log import get_logger
 from deeppavlov.core.commands.train import train_evaluate_model_from_config, get_iterator_from_config, read_data_by_config
-from sklearn.model_selection import KFold
 from deeppavlov.core.common.params_search import ParamsSearch
 from deeppavlov.core.commands.utils import expand_path
 
@@ -41,51 +35,47 @@ def change_savepath_for_model(config):
     for p in params_helper.find_model_path(config, SAVE_PATH_ELEMENT_NAME):
         p.append(SAVE_PATH_ELEMENT_NAME)
         save_path = Path(params_helper.get_value_from_config(config, p))
-        new_save_path = str(save_path.parent.joinpath(TEMP_DIR_FOR_CV).joinpath(save_path.name))
+        new_save_path = save_path.parent / TEMP_DIR_FOR_CV / save_path.name
 
-        dir = expand_path(os.path.dirname(new_save_path))
-        dirs_for_saved_models.add(str(dir))
-        os.makedirs(dir, exist_ok=True)
+        dirs_for_saved_models.add(expand_path(new_save_path.parent))
 
-        config = params_helper.insert_value_or_dict_into_config(config, p, new_save_path)
+        config = params_helper.insert_value_or_dict_into_config(config, p, str(new_save_path))
 
     return config, dirs_for_saved_models
 
+
 def delete_dir_for_saved_models(dirs_for_saved_models):
-    for dir in dirs_for_saved_models:
-        for root, dirs, files in os.walk(dir):
-            for file in files:
-                os.remove(os.path.join(root, file))
-            for name in dirs:
-                os.rmdir(os.path.join(root, name))
-        os.rmdir(dir)
+    for new_save_dir in dirs_for_saved_models:
+        shutil.rmtree(str(new_save_dir))
 
 
 def create_dirs_to_save_models(dirs_for_saved_models):
-    for dir in dirs_for_saved_models:
-        os.makedirs(dir, exist_ok=True)
+    for new_save_dir in dirs_for_saved_models:
+        new_save_dir.mkdir(exist_ok=True, parents=True)
 
 
 def generate_train_valid(data, n_folds=5, is_loo=False):
     all_data = data['train'] + data['valid']
 
-    # for Leave One Out
     if is_loo:
+        # for Leave One Out
         for i in range(len(all_data)):
-            data_i = {}
-            data_i['train'] = all_data.copy()
+            data_i = {
+                'train': all_data.copy(),
+                'test': data['test']
+            }
             data_i['valid'] = [data_i['train'].pop(i)]
-            data_i['test'] = data['test']
 
             yield data_i
-    # for Cross Validation
     else:
+        # for Cross Validation
         kf = KFold(n_splits=n_folds, shuffle=True)
         for train_index, valid_index in kf.split(all_data):
-            data_i = {}
-            data_i['train'] = [all_data[i] for i in train_index]
-            data_i['valid'] = [all_data[i] for i in valid_index]
-            data_i['test'] = data['test']
+            data_i = {
+                'train': [all_data[i] for i in train_index],
+                'valid': [all_data[i] for i in valid_index],
+                'test': data['test']
+            }
 
             yield data_i
 
@@ -95,14 +85,14 @@ def calc_cv_score(config=None, pipeline_config_path=None, data=None, n_folds=5, 
         if pipeline_config_path is not None:
             config = read_json(pipeline_config_path)
         else:
-            raise ValueError('Both \"config\" and \"pipeline_config_path\" is None')
+            raise ValueError('Both \"config\" and \"pipeline_config_path\" are None')
 
     if data is None:
         data = read_data_by_config(config)
 
     config, dirs_for_saved_models = change_savepath_for_model(config)
 
-    all_scores=[]
+    all_scores = []
     target_metric = config['train']['metrics'][0]
     for data_i in generate_train_valid(data, n_folds=n_folds, is_loo=is_loo):
         iterator = get_iterator_from_config(config, data_i)
