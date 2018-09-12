32afce1dbd94b06455ccf51996f2d07a5c3eb36f
Vasily <vasili.konov@gmail.com>
2018-09-12 16:03:26 +0300

    fix: add type annotation

--- deeppavlov/dataset_readers/amazon_ecommerce_reader.py
+++ deeppavlov/dataset_readers/amazon_ecommerce_reader.py
@@ -1,5 +1,17 @@
+# Copyright 2018 Neural Networks and Deep Learning lab, MIPT
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import re
 from pathlib import Path
+from typing import List, Any, Dict, Iterable, Tuple
 
 from deeppavlov.core.data.dataset_reader import DatasetReader
 from deeppavlov.core.common.registry import register
@@ -8,9 +20,20 @@ from deeppavlov.core.data.utils import download_decompress, mark_done, is_done
 
 @register('amazon_ecommerce_reader')
 class AmazonEcommerceReader(DatasetReader):
-    
-    def read(self, data_path: str, catalog: str, **kwargs):
-        ec_data_global = []
+    """Class to download and load ecommerce data catalog"""
+
+    def read(self, data_path: str, catalog: str, **kwargs) -> Dict[str, List[Tuple[Any, Any]]]:
+        """Load data from specific catalog
+
+        Parameters:
+            data_path: path where to store the dataset
+            catalog: name of the specific part of the catalog
+        
+        Returns:
+            dataset: loaded dataset
+        """
+
+        ec_data_global: List[Any] = []
         data_path = Path(expand_path(data_path))
 
         if not is_done(data_path):
@@ -27,14 +50,24 @@ class AmazonEcommerceReader(DatasetReader):
         dataset["test"] = []
         return dataset
 
-    def _download_data(self, data_path):
+    def _download_data(self, data_path: str) -> None:
+        """Download dataset"""
         url = "https://github.com/SamTube405/Amazon-E-commerce-Data-set/archive/master.zip"
         download_decompress(url, data_path)
         mark_done(data_path)
 
-    def _load_amazon_ecommerce_file(self, fname):
+    def _load_amazon_ecommerce_file(self, fname: str) -> List[Dict[Any, Any]]:
+        """Parse dataset
+
+        Parameters:
+            fname: catalog file
+            
+        Returns:
+            ec_data: parsed catalog data
+        """
+
         ec_data = []
-        item = dict()
+        item: Dict = {}
         new_item_re = re.compile("ITEM *\d+")
 
         with open(fname, 'r', encoding='utf-8', errors='ignore') as file:
