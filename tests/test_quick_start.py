4f416a6569a511b4971b61f710fccc02ade2a647
Aleksey Lymar <yoptar@gmail.com>
2018-09-13 15:25:02 +0300

    tests: do not test brillmoore_ru train as its validation is taking too long.

--- tests/test_quick_start.py
+++ tests/test_quick_start.py
@@ -51,7 +51,7 @@ PARAMS = {
                 ("helllo", "hello"),
                 ("datha", "data")
             ],
-        ("spelling_correction/brillmoore_kartaslov_ru.json", "error_model", ALL_MODES):
+        ("spelling_correction/brillmoore_kartaslov_ru.json", "error_model", ('IP',)):
             [
                 ("я джва года дду эту игру", "я два года жду эту игру")
             ],
