b42b64312bf4f2f32ace5af03eb43db8f7a41d4a
Olga Gureenkova <ol.gure@gmail.com>
2018-09-27 22:47:03 +0300

    fix: update test paths

--- tests/test_quick_start.py
+++ tests/test_quick_start.py
@@ -123,8 +123,12 @@ PARAMS = {
         ("elmo/elmo_ru-news.json", "elmo_ru-news", ('IP',)): [ONE_ARGUMENT_INFER_CHECK],
     },
 
-    "ranking": {("ranking/ranking_insurance.json", "ranking", ALL_MODES): [ONE_ARGUMENT_INFER_CHECK],
-                ("ranking/en_ranker_tfidf_wiki_test.json", "ranking", ALL_MODES): [ONE_ARGUMENT_INFER_CHECK]},
+    "ranking": {
+        ("ranking/ranking_insurance.json", "ranking", ALL_MODES): [ONE_ARGUMENT_INFER_CHECK]
+    },
+    "doc_retrieval": {
+        ("doc_retrieval/en_ranker_tfidf_wiki_test.json", "doc_retrieval", ALL_MODES): [ONE_ARGUMENT_INFER_CHECK]
+    },
     "squad": {
         ("squad/squad.json", "squad_model", ALL_MODES): [TWO_ARGUMENTS_INFER_CHECK],
         ("squad/squad_ru.json", "squad_model_ru", ALL_MODES): [TWO_ARGUMENTS_INFER_CHECK]
