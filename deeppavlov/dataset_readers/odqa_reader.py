119230e3d604ec8a91bbdd9fbb980f3f11dddab4
Olga Gureenkova <ol.gure@gmail.com>
2018-10-01 11:38:54 +0300

    refactor: inline ODQAReader methods

--- deeppavlov/dataset_readers/odqa_reader.py
+++ deeppavlov/dataset_readers/odqa_reader.py
@@ -36,146 +36,143 @@ class ODQADataReader(DatasetReader):
 
     """
 
-    @staticmethod
-    def read(data_path: Union[Path, str], *args, **kwargs) -> None:
+    def read(self, data_path: Union[Path, str], *args, **kwargs) -> None:
         logger.info('Reading files...')
         if kwargs['dataset_format'] == 'sqlite':
             return
         save_path = expand_path(kwargs['save_path'])
-        _build_db(save_path, kwargs['dataset_format'], data_path)
+        self._build_db(save_path, kwargs['dataset_format'], data_path)
 
+    def iter_files(self, path: Union[Path, str]) -> Generator[Path, Any, Any]:
+        """Iterate over folder with files or a single file and generate file paths.
 
-def iter_files(path: Union[Path, str]) -> Generator[Path, Any, Any]:
-    """Iterate over folder with files or a single file and generate file paths.
+        Args:
+            path: path to a folder or a file
 
-    Args:
-        path: path to a folder or a file
+        Yields:
+            file paths one by one
 
-    Yields:
-        file paths one by one
+        Returns:
+            None
 
-    Returns:
-        None
+        """
+        path = Path(path)
+        if path.is_file():
+            yield path
+        elif path.is_dir():
+            for item in path.iterdir():
+                yield from self.iter_files(item)
+        else:
+            raise RuntimeError("Path doesn't exist: {}".format(path))
 
-    """
-    path = Path(path)
-    if path.is_file():
-        yield path
-    elif path.is_dir():
-        for item in path.iterdir():
-            yield from iter_files(item)
-    else:
-        raise RuntimeError("Path doesn't exist: {}".format(path))
+    def _build_db(self, save_path: Union[Path, str], dataset_format: str, data_path: Union[Path, str],
+                  num_workers: int = 8) -> None:
+        """Build a SQLite database in parallel and save it to a pointed path.
 
+        Args:
+            save_path: a path where the ready database should be saved
+            dataset_format: a data format, should be selected from ['sqlite', 'txt', 'json', 'wiki']
+            data_path: path to a folder/file from which to build a database
+            num_workers: a number of workers for parallel database building
 
-def _build_db(save_path: Union[Path, str], dataset_format: str, data_path: Union[Path, str],
-              num_workers: int = 4) -> None:
-    """Build a SQLite database in parallel and save it to a pointed path.
+        Raises:
+            sqlite3.OperationalError if `save_path` doesn't exist.
 
-    Args:
-        save_path: a path where the ready database should be saved
-        dataset_format: a data format, should be selected from ['sqlite', 'txt', 'json', 'wiki']
-        data_path: path to a folder/file from which to build a database
-        num_workers: a number of workers for parallel database building
+        Returns:
+            None
 
-    Raises:
-        sqlite3.OperationalError if `save_path` doesn't exist.
+        """
+        logger.info('Building the database...')
+        try:
+            conn = sqlite3.connect(str(save_path))
+        except sqlite3.OperationalError as e:
+            e.args = e.args + ("Check that DB path exists.",)
+            raise e
+        c = conn.cursor()
+        sql_table = "CREATE TABLE documents (id PRIMARY KEY, text);"
+        c.execute(sql_table)
 
-    Returns:
-        None
+        files = [f for f in self.iter_files(data_path)]
+        workers = Pool(num_workers)
 
-    """
-    logger.info('Building the database...')
-    try:
-        conn = sqlite3.connect(str(save_path))
-    except sqlite3.OperationalError as e:
-        e.args = e.args + ("Check that DB path exists.",)
-        raise e
-    c = conn.cursor()
-    sql_table = "CREATE TABLE documents (id PRIMARY KEY, text);"
-    c.execute(sql_table)
+        if dataset_format == 'txt':
+            fn = self._get_file_contents
+        elif dataset_format == 'json':
+            fn = self._get_json_contents
+        elif dataset_format == 'wiki':
+            fn = self._get_wiki_contents
+        else:
+            raise RuntimeError('Unknown dataset format.')
 
-    files = [f for f in iter_files(data_path)]
-    workers = Pool(num_workers)
+        with tqdm(total=len(files)) as pbar:
+            for data in tqdm(workers.imap_unordered(fn, files)):
+                try:
+                    c.executemany("INSERT INTO documents VALUES (?,?)", data)
+                    pbar.update()
+                except sqlite3.IntegrityError as e:
+                    logger.warning(e)
 
-    if dataset_format == 'txt':
-        fn = _get_file_contents
-    elif dataset_format == 'json':
-        fn = _get_json_contents
-    elif dataset_format == 'wiki':
-        fn = _get_wiki_contents
-    else:
-        raise RuntimeError('Unknown dataset format.')
+        conn.commit()
+        conn.close()
 
-    with tqdm(total=len(files)) as pbar:
-        for data in tqdm(workers.imap_unordered(fn, files)):
-            try:
-                c.executemany("INSERT INTO documents VALUES (?,?)", data)
-                pbar.update()
-            except sqlite3.IntegrityError as e:
-                logger.warning(e)
+    @staticmethod
+    def _get_file_contents(fpath: Union[Path, str]) -> List[Tuple[str, str]]:
+        """Extract file contents from '.txt' file.
 
-    conn.commit()
-    conn.close()
+        Args:
+            fpath: path to a '.txt' file.
 
+        Returns:
+             a list with tuple of normalized file name and file contents
 
-def _get_file_contents(fpath: Union[Path, str]) -> List[Tuple[str, str]]:
-    """Extract file contents from '.txt' file.
+        """
+        with open(fpath) as fin:
+            text = fin.read()
+            normalized_text = unicodedata.normalize('NFD', text)
+            return [(fpath.name, normalized_text)]
 
-    Args:
-        fpath: path to a '.txt' file.
+    @staticmethod
+    def _get_json_contents(fpath: Union[Path, str]) -> List[Tuple[str, str]]:
+        """Extract file contents from '.json' file. JSON files should be formatted as list with dicts
+        which contain 'title' and 'doc' keywords.
 
-    Returns:
-         a list with tuple of normalized file name and file contents
+        Args:
+            fpath: path to a '.json' file.
 
-    """
-    with open(fpath) as fin:
-        text = fin.read()
-        normalized_text = unicodedata.normalize('NFD', text)
-        return [(fpath.name, normalized_text)]
+        Returns:
+            a list with tuples of normalized file name and file contents
 
+        """
+        docs = []
+        with open(fpath) as fin:
+            for line in fin:
+                data = json.loads(line)
+                for doc in data:
+                    if not doc:
+                        continue
+                    text = doc['text']
+                    normalized_text = unicodedata.normalize('NFD', text)
+                    docs.append((doc['title'], normalized_text))
+        return docs
 
-def _get_json_contents(fpath: Union[Path, str]) -> List[Tuple[str, str]]:
-    """Extract file contents from '.json' file. JSON files should be formatted as list with dicts
-    which contain 'title' and 'doc' keywords.
+    @staticmethod
+    def _get_wiki_contents(fpath: Union[Path, str]) -> List[Tuple[str, str]]:
+        """Extract file contents from wiki extractor formatted files.
 
-    Args:
-        fpath: path to a '.json' file.
+        Args:
+            fpath: path to a '.txt' file in wiki extractor format
 
-    Returns:
-        a list with tuples of normalized file name and file contents
+        Returns:
+            a list with tuples of normalized file name and file contents
 
-    """
-    docs = []
-    with open(fpath) as fin:
-        for line in fin:
-            data = json.loads(line)
-            for doc in data:
+        """
+        docs = []
+        with open(fpath) as fin:
+            for line in fin:
+                doc = json.loads(line)
                 if not doc:
                     continue
                 text = doc['text']
                 normalized_text = unicodedata.normalize('NFD', text)
                 docs.append((doc['title'], normalized_text))
-    return docs
-
-
-def _get_wiki_contents(fpath: Union[Path, str]) -> List[Tuple[str, str]]:
-    """Extract file contents from wiki extractor formatted files.
-
-    Args:
-        fpath: path to a '.txt' file in wiki extractor format
-
-    Returns:
-        a list with tuples of normalized file name and file contents
-
-    """
-    docs = []
-    with open(fpath) as fin:
-        for line in fin:
-            doc = json.loads(line)
-            if not doc:
-                continue
-            text = doc['text']
-            normalized_text = unicodedata.normalize('NFD', text)
-            docs.append((doc['title'], normalized_text))
-    return docs
+        return docs
