982682e26b58feee5b6d24dd69659ebd6b959a2b
Aleksey Lymar <yoptar@gmail.com>
2018-10-11 17:53:03 +0300

    fix: update metrics for squad and bot_kvret tasks

--- deeppavlov/metrics/bleu.py
+++ deeppavlov/metrics/bleu.py
@@ -13,13 +13,15 @@
 # limitations under the License.
 
 import itertools
+from typing import List, Tuple, Any
+
 from nltk.translate.bleu_score import corpus_bleu, sentence_bleu, SmoothingFunction, brevity_penalty, closest_ref_length
-from deeppavlov.metrics.google_bleu import compute_bleu
-from typing import List, Tuple, Dict, Any
 
 from deeppavlov.core.common.metrics_registry import register_metric
+from deeppavlov.metrics.google_bleu import compute_bleu
 
 SMOOTH = SmoothingFunction()
+
 
 @register_metric('bleu_advanced')
 def bleu_advanced(y_true: List[Any], y_predicted: List[Any],
@@ -52,18 +54,15 @@ def bleu_advanced(y_true: List[Any], y_predicted: List[Any],
 
     return bleu_measure/bpenalty
 
+
 @register_metric('bleu')
 def bleu(y_true, y_predicted):
-    if isinstance(y_true[0], (tuple, list)):
-        y_true = (y[0] for y in y_true)
     return corpus_bleu([[y_t.lower().split()] for y_t in y_true],
                        [y_p.lower().split() for y_p in y_predicted])
 
 
 @register_metric('google_bleu')
 def google_bleu(y_true, y_predicted):
-    if isinstance(y_true[0], (tuple, list)):
-        y_true = (y[0] for y in y_true)
     return compute_bleu(([y_t.lower().split()] for y_t in y_true),
                         (y_p.lower().split() for y_p in y_predicted))[0]
 
@@ -71,8 +70,6 @@ def google_bleu(y_true, y_predicted):
 @register_metric('per_item_bleu')
 def per_item_bleu(y_true, y_predicted):
     y_predicted = itertools.chain(*y_predicted)
-    if isinstance(y_true[0][0], (tuple, list)):
-        y_true = (y[0] for y_list in y_true for y in y_list)
     return corpus_bleu([[y_t.lower().split()] for y_t in y_true],
                        [y_p.lower().split() for y_p in y_predicted])
 
