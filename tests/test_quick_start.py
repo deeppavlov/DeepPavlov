af04b31fc080aa48a97062f3c1b87a82c7bb6da8
Olga Gureenkova <ol.gure@gmail.com>
2018-10-17 16:34:13 +0300

    test: add ru doc_retrieval to tests

--- tests/test_quick_start.py
+++ tests/test_quick_start.py
@@ -139,7 +139,8 @@ PARAMS = {
                 ("ranking/paraphrase_ident_qqp_interact_test.json", "ranking", ('IP',)): [ONE_ARGUMENT_INFER_CHECK]
     },
     "doc_retrieval": {
-        ("doc_retrieval/en_ranker_tfidf_wiki_test.json", "doc_retrieval", ('TI',)): [ONE_ARGUMENT_INFER_CHECK]
+        ("doc_retrieval/en_ranker_tfidf_wiki_test.json", "doc_retrieval", ('TI',)): [ONE_ARGUMENT_INFER_CHECK],
+        ("doc_retrieval/ru_ranker_tfidf_wiki_test.json", "doc_retrieval", ('TI',)): [ONE_ARGUMENT_INFER_CHECK]
     },
     "squad": {
         ("squad/squad.json", "squad_model", ALL_MODES): [TWO_ARGUMENTS_INFER_CHECK],
