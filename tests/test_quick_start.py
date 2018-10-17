b679c9a322d4962f63953d5d7a790c0346859bf2
Olga Gureenkova <ol.gure@gmail.com>
2018-10-17 16:54:22 +0300

    test: update tests

--- tests/test_quick_start.py
+++ tests/test_quick_start.py
@@ -165,7 +165,8 @@ PARAMS = {
         ]
     },
     "odqa": {
-        ("odqa/en_odqa_infer_wiki_test.json", "odqa", ('IP',)): [ONE_ARGUMENT_INFER_CHECK]
+        ("odqa/en_odqa_infer_wiki_test.json", "odqa", ('IP',)): [ONE_ARGUMENT_INFER_CHECK],
+        ("odqa/ru_odqa_infer_wiki_test.json", "odqa", ('IP',)): [ONE_ARGUMENT_INFER_CHECK]
     },
     "morpho_tagger":{
         ("morpho_tagger/UD2.0/hu/morpho_hu_train.json", "morpho_tagger_hu", ALL_MODES): [ONE_ARGUMENT_INFER_CHECK],
