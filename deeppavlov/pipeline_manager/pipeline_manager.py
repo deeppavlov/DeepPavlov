683c10764583015490fc8c243693aa5ec0daa8f4
Maksim Petrov <maksimallist@gmail.com>
2018-10-19 17:01:09 +0300

    refactor: del trash
    * update 'linear_classification' config

--- deeppavlov/pipeline_manager/pipeline_manager.py
+++ deeppavlov/pipeline_manager/pipeline_manager.py
@@ -12,28 +12,44 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import json
 import os
+import json
 import shutil
 
 from time import time
 from tqdm import tqdm
-from copy import copy
 from os.path import join
 from shutil import rmtree
+from psutil import cpu_count
 from datetime import datetime
-from typing import Union, Dict
+from copy import copy, deepcopy
+from multiprocessing import Pool
+from typing import Union, Dict, List
 
 from deeppavlov.core.common.file import read_json
 from deeppavlov.pipeline_manager.logger import Logger
 from deeppavlov.core.common.errors import ConfigError
 from deeppavlov.pipeline_manager.pipegen import PipeGen
 from deeppavlov.pipeline_manager.utils import normal_time
+from deeppavlov.pipeline_manager.utils import get_num_gpu
+from deeppavlov.core.common.prints import RedirectedPrints
 from deeppavlov.core.common.cross_validation import calc_cv_score
-from deeppavlov.pipeline_manager.utils import results_visualization
 from deeppavlov.core.data.data_fitting_iterator import DataFittingIterator
 from deeppavlov.core.commands.train import train_evaluate_model_from_config
 from deeppavlov.core.commands.train import read_data_by_config, get_iterator_from_config
+from deeppavlov.pipeline_manager.utils import results_visualization, get_available_gpus
+
+
+def unpack_args(func):
+    from functools import wraps
+
+    @wraps(func)
+    def wrapper(args):
+        if isinstance(args, dict):
+            return func(**args)
+        else:
+            return func(*args)
+    return wrapper
 
 
 class PipelineManager:
@@ -54,9 +70,9 @@ class PipelineManager:
                        is generated. The default value is None, in this case the target metric is taken the
                        first name from those names that are specified in the config file. If the specified metric
                        is not contained in DeepPavlov will be called error.
-        plot: boolean trigger, which determines whether to draw a graph of results or not
         logger: A special class that collects auxiliary statistics and results during training, and stores all
                 the collected data in a separate log.
+        plot: boolean trigger, which determines whether to draw a graph of results or not
         pipeline_generator: A special class that generates configs for training.
     """
     def __init__(self, config_path: Union[str, Dict], exp_name: str):
@@ -69,32 +85,203 @@ class PipelineManager:
             self.exp_config = config_path
 
         self.exp_name = exp_name
-        self.gen_len = 0
-        self.pipeline_generator = None
-
         self.date = self.exp_config['enumerate'].get('date', datetime.now().strftime('%Y-%m-%d'))
         self.info = self.exp_config['enumerate'].get('info')
-        self.root = self.exp_config['enumerate'].get('root', 'experiments/')
+        self.root = self.exp_config['enumerate'].get('root', 'download/experiments/')
         self.plot = self.exp_config['enumerate'].get('plot', False)
         self.save_best = self.exp_config['enumerate'].get('save_best', False)
-        self.do_test = self.exp_config['enumerate'].get('save_best', False)
+        self.do_test = self.exp_config['enumerate'].get('do_test', False)
         self.cross_validation = self.exp_config['enumerate'].get('cross_val', False)
-        self.k_fold = self.exp_config['enumerate'].get('cross_val', 5)
+        self.k_fold = self.exp_config['enumerate'].get('k_fold', 5)
         self.sample_num = self.exp_config['enumerate'].get('sample_num', 10)
         self.target_metric = self.exp_config['enumerate'].get('target_metric')
         self.multiprocessing = self.exp_config['enumerate'].get('multiprocessing', True)
         self.max_num_workers_ = self.exp_config['enumerate'].get('max_num_workers')
         self.use_all_gpus = self.exp_config['enumerate'].get('use_all_gpus', False)
         self.use_multi_gpus = self.exp_config['enumerate'].get('use_multi_gpus')
+        self.memory_fraction = self.exp_config['enumerate'].get('gpu_memory_fraction', 1.0)
 
+        self.pipeline_generator = None
+        self.gen_len = 0
+
+        # Logger initialization
         self.save_path = join(self.root, self.date, self.exp_name, 'checkpoints')
-        self.logger = Logger(exp_name, self.root, self.info, self.date, self.plot)
-        self.start_exp = time()
+        self.logger = Logger(self.exp_name, self.root, self.info, self.date, self.plot)
 
+        # multiprocessing
+        if self.use_multi_gpus and self.use_all_gpus:
+            raise ValueError("Parameters 'use_all_gpus' and 'use_multi_gpus' can not simultaneously be not None.")
+
+        self.max_num_workers = None
+        self.available_gpu = None
+        if self.multiprocessing:
+            self.prepare_multiprocess()
+
+        # write time of experiment start
+        self.start_exp = time()
         # start test
         if self.do_test:
             self.dataset_composition = dict(train=False, valid=False, test=False)
             self.test()
+
+    def prepare_multiprocess(self):
+        cpu_num = cpu_count()
+        gpu_num = get_num_gpu()
+
+        try:
+            visible_gpu = [int(q) for q in os.environ['CUDA_VISIBLE_DEVICES'].split(',')]
+            os.environ['CUDA_VISIBLE_DEVICES'] = ""
+        except KeyError:
+            visible_gpu = []
+
+        if self.max_num_workers_ is not None and self.max_num_workers_ < 1:
+            raise ConfigError("The number of workers must be at least equal to one. "
+                              "Please check 'max_num_workers' parameter in config.")
+
+        if self.use_all_gpus:
+            if self.max_num_workers_ is None:
+                self.available_gpu = get_available_gpus(gpu_fraction=self.memory_fraction)
+                self.available_gpu = list(set(self.available_gpu) & set(visible_gpu))
+
+                if len(self.available_gpu) == 0:
+                    raise ValueError("GPU with numbers: ({}) are busy.".format(set(visible_gpu)))
+                elif len(self.available_gpu) < len(visible_gpu):
+                    print("PipelineManagerWarning: 'CUDA_VISIBLE_DEVICES' = ({0}), "
+                          "but only {1} are available.".format(visible_gpu, self.available_gpu))
+
+                if int(cpu_num * 0.7) > len(self.available_gpu):
+                    self.max_num_workers = len(self.available_gpu)
+                else:
+                    self.max_num_workers = int(cpu_num * 0.7)
+            else:
+                if self.max_num_workers_ > gpu_num:
+                    self.max_num_workers = gpu_num
+                    self.available_gpu = get_available_gpus(gpu_fraction=self.memory_fraction)
+                    self.available_gpu = list(set(self.available_gpu) & set(visible_gpu))
+
+                    if len(self.available_gpu) == 0:
+                        raise ValueError("GPU with numbers: ({}) are busy.".format(set(visible_gpu)))
+                else:
+                    self.available_gpu = get_available_gpus(num_gpus=self.max_num_workers_,
+                                                            gpu_fraction=self.memory_fraction)
+                    self.available_gpu = list(set(self.available_gpu) & set(visible_gpu))
+
+                    if len(self.available_gpu) == 0:
+                        raise ValueError("GPU with numbers: ({}) are busy.".format(set(visible_gpu)))
+
+                    self.max_num_workers = len(self.available_gpu)
+
+        elif self.use_multi_gpus:
+            self.use_multi_gpus = list(set(self.use_multi_gpus) & set(visible_gpu))
+            if len(self.use_multi_gpus) == 0:
+                raise ValueError("GPU numbers in 'use_multi_gpus' and 'CUDA_VISIBLE_DEVICES' "
+                                 "has not intersections".format(set(visible_gpu)))
+
+            self.available_gpu = get_available_gpus(gpu_select=self.use_multi_gpus, gpu_fraction=self.memory_fraction)
+
+            if len(self.available_gpu) == 0:
+                raise ValueError("All GPU from 'use_multi_gpus' are busy. "
+                                 "GPU numbers ({});".format(self.use_multi_gpus))
+
+            if not self.max_num_workers_:
+                self.max_num_workers = len(self.available_gpu)
+            else:
+                if self.max_num_workers_ > len(self.available_gpu):
+                    self.max_num_workers = len(self.available_gpu)
+                else:
+                    self.max_num_workers = self.max_num_workers_
+                    self.available_gpu = self.available_gpu[0:self.max_num_workers_]
+
+        else:
+            if self.max_num_workers_ is not None and self.max_num_workers_ > cpu_num:
+                print("PipelineManagerWarning: parameter 'max_num_workers'={0}, "
+                      "but amounts of cpu is {1}. The {1} will be assigned to 'max_num_workers' "
+                      "as default.".format(self.max_num_workers_, cpu_num))
+                self.max_num_workers_ = cpu_num
+
+            self.max_num_workers = self.max_num_workers_
+
+    @staticmethod
+    @unpack_args
+    def train_pipe(pipe_config, cross_validation, k_fold, gpu, gpu_ind, pipe_ind, out_path):
+        # modify project environment
+        if gpu:
+            os.environ['CUDA_VISIBLE_DEVICES'] = str(gpu_ind)
+        else:
+            os.environ['CUDA_VISIBLE_DEVICES'] = ''
+
+        # start pipeline
+        pipe_start = time()
+        res = dict(pipe_conf=deepcopy(pipe_config))
+
+        #
+        dataset_name = copy(res['pipe_conf']['dataset_reader']['data_path'])
+        save_path = join(out_path, dataset_name, "pipe_{}".format(pipe_ind + 1))
+        os.makedirs(save_path)
+        #
+        with RedirectedPrints(out=open(join(save_path, "out.txt"), "w")):
+            if cross_validation:
+                cv_score = calc_cv_score(pipe_config, n_folds=k_fold)
+                results = {"test": cv_score}
+            else:
+                results = train_evaluate_model_from_config(pipe_config, to_train=True, to_validate=True)
+
+        res['results'] = results
+        res['pipe_time'] = time() - pipe_start
+        return res
+
+    def update_logger(self, res_list: List[dict]):
+        dataset_res = {}
+        for i, res in enumerate(res_list):
+            pipe = res['pipe_conf']
+            results = res['results']
+            dataset_name = copy(pipe['dataset_reader']['data_path'])
+
+            if i == 0:
+                self.logger.log['experiment_info']['metrics'] = copy(pipe['train']['metrics'])
+                if self.target_metric is None:
+                    self.target_metric = pipe['train']['metrics'][0]['name']
+                self.logger.log['experiment_info']['target_metric'] = self.target_metric
+
+            self.logger.pipe_ind = i + 1
+            self.logger.pipe_conf = copy(pipe['chainer']['pipe'])
+            self.logger.dataset = dataset_name
+            self.logger.batch_size = pipe['train'].get('batch_size', "None")
+
+            # add results and pipe time to log
+            self.logger.pipe_time = normal_time(res['pipe_time'])
+            self.logger.pipe_res = results
+            # update logger
+            self.logger.get_pipe_log()
+            self.logger.write()
+            # save config in checkpoint folder
+            if not self.cross_validation:
+                self.save_config(pipe, dataset_name, i)
+
+            if self.save_best:
+                if dataset_name not in dataset_res.keys():
+                    dataset_res[dataset_name] = dict(best_score=-1, best_ind=None)
+
+                if 'test' in results.keys():
+                    if results['test'][self.target_metric] > dataset_res[dataset_name]["best_score"]:
+                        dataset_res[dataset_name]["best_score"] = results['test'][self.target_metric]
+                        dataset_res[dataset_name]["best_ind"] = i + 1
+
+                else:
+                    if results['valid'][self.target_metric] > dataset_res[dataset_name]["best_score"]:
+                        dataset_res[dataset_name]["best_score"] = results['valid'][self.target_metric]
+                        dataset_res[dataset_name]["best_ind"] = i + 1
+
+        return dataset_res
+
+    def gpu_gen(self, gpu=False):
+        if gpu:
+            for i, pipe_conf in enumerate(self.pipeline_generator()):
+                j = i - (i // len(self.available_gpu)) * len(self.available_gpu)
+                yield (deepcopy(pipe_conf), self.cross_validation, self.k_fold, True, j, i, self.save_path)
+        else:
+            for i, pipe_conf in enumerate(self.pipeline_generator()):
+                yield (deepcopy(pipe_conf), self.cross_validation, self.k_fold, False, None, i, self.save_path)
 
     def run(self):
         """
@@ -112,51 +299,37 @@ class PipelineManager:
 
         self.logger.log['experiment_info']['number_of_pipes'] = self.gen_len
 
-        dataset_res = {}
-        for i, pipe in enumerate(tqdm(self.pipeline_generator(), total=self.gen_len)):
-            if i == 0:
-                self.logger.log['experiment_info']['metrics'] = copy(pipe['train']['metrics'])
-                if self.target_metric is None:
-                    self.target_metric = pipe['train']['metrics'][0]['name']
-                self.logger.log['experiment_info']['target_metric'] = self.target_metric
+        if self.multiprocessing:
+            # start multiprocessing
+            workers = Pool(self.max_num_workers)
 
-            self.logger.pipe_ind = i + 1
-            self.logger.pipe_conf = copy(pipe['chainer']['pipe'])
-            self.logger.dataset = copy(pipe['dataset_reader']['data_path'])
-            self.logger.batch_size = pipe['train'].get('batch_size', "None")
-
-            # start pipeline time
-            pipe_start = time()
-            if self.cross_validation:
-                cv_score = calc_cv_score(pipe, n_folds=self.k_fold)
-                results = {"test": cv_score}
+            if self.available_gpu is None:
+                pipes_results = list(tqdm(workers.imap_unordered(self.train_pipe, [x for x in self.gpu_gen(gpu=False)]),
+                                          total=self.gen_len))
+                workers.close()
+                workers.join()
             else:
-                results = train_evaluate_model_from_config(pipe, to_train=True, to_validate=True)
-
-            if self.save_best:
-                if self.logger.dataset not in dataset_res.keys():
-                    dataset_res[self.logger.dataset] = dict(best_score=-1, best_ind=None)
-
-                if 'test' in results.keys():
-                    if results['test'][self.target_metric] > dataset_res[self.logger.dataset]["best_score"]:
-                        dataset_res[self.logger.dataset]["best_score"] = results['test'][self.target_metric]
-                        dataset_res[self.logger.dataset]["best_ind"] = i + 1
-
+                pipes_results = list(tqdm(workers.imap_unordered(self.train_pipe, [x for x in self.gpu_gen(gpu=True)]),
+                                          total=self.gen_len))
+                workers.close()
+                workers.join()
+        else:
+            pipes_results = []
+            for i, pipe in enumerate(tqdm(self.pipeline_generator(), total=self.gen_len)):
+                res = dict(pipe_conf=deepcopy(pipe))
+                # start pipeline time
+                pipe_start = time()
+                if self.cross_validation:
+                    cv_score = calc_cv_score(pipe, n_folds=self.k_fold)
+                    results = {"test": cv_score}
                 else:
-                    if results['valid'][self.target_metric] > dataset_res[self.logger.dataset]["best_score"]:
-                        dataset_res[self.logger.dataset]["best_score"] = results['valid'][self.target_metric]
-                        dataset_res[self.logger.dataset]["best_ind"] = i + 1
+                    results = train_evaluate_model_from_config(pipe, to_train=True, to_validate=True)
+                res['pipe_time'] = time() - pipe_start
+                res['results'] = results
 
-            # add results and pipe time to log
-            self.logger.pipe_time = normal_time(time() - pipe_start)
-            self.logger.pipe_res = results
+                pipes_results.append(res)
 
-            # save config in checkpoint folder
-            if not self.cross_validation:
-                self.save_config(pipe, self.logger.dataset, i)
-            # update logger
-            self.logger.get_pipe_log()
-            self.logger.write()
+        dataset_res = self.update_logger(pipes_results)
 
         # save log
         self.logger.log['experiment_info']['full_time'] = normal_time(time() - self.start_exp)
@@ -184,9 +357,12 @@ class PipelineManager:
                 # del all tmp files in save path
                 rmtree(join(self.save_path, name))
 
+        print("[ End of experiment ]")
         # visualization of results
+        print("[ Create an experiment report ... ]")
         path = join(self.root, self.date, self.exp_name)
         results_visualization(path, self.plot, target_metric=self.target_metric)
+        print("[ Report created ]")
         return None
 
     def test(self):
