6b500a3c67d54d38eecbdf4eb0a64bc1069e95c9
Aleksey Lymar <yoptar@gmail.com>
2018-10-23 12:07:06 +0300

    fix: fix errors found with code inspection

--- deeppavlov/models/preprocessors/ecommerce_preprocess.py
+++ deeppavlov/models/preprocessors/ecommerce_preprocess.py
@@ -8,9 +8,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+import math
 import re
 from typing import List, Any, Dict, Iterable, Optional, Tuple
+
 import spacy
 from spacy.matcher import Matcher
 
