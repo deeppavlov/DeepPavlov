d984b6c4db931d8b3b0a07796d0d7566cb212e15
Aleksey Lymar <yoptar@gmail.com>
2018-10-24 11:34:28 +0300

    feat: add a config tree object

--- deeppavlov/__init__.py
+++ deeppavlov/__init__.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from .configs import configs
 
 __version__ = '0.0.9'
 __author__ = 'Neural Networks and Deep Learning lab, MIPT'
---
+++ deeppavlov/configs/__init__.py
@@ -0,0 +1,54 @@
+from collections.abc import Mapping
+from pathlib import Path
+from typing import Iterator, Dict, Union
+
+
+class Struct(Mapping):
+    def __iter__(self) -> Iterator[str]:
+        return iter(self._keys)
+
+    def __len__(self) -> int:
+        return len(self._keys)
+
+    def __init__(self, tree: Dict[str, Union[dict, Path]]) -> None:
+        self._keys = set()
+        for key, value in tree.items():
+            key = key.replace('.', '_')
+            self._keys.add(key)
+            setattr(self, key,
+                    Struct(value) if isinstance(value, dict) else value)
+        self._keys = frozenset(self._keys)
+
+    def _to_dict(self) -> dict:
+        return {key: value._to_dict() if isinstance(value, Struct) else value
+                for key, value in self.__dict__.items() if key in self._keys}
+
+    def __getitem__(self, key: str):
+        if key not in self._keys:
+            raise KeyError(key)
+        return self._to_dict()[key]
+
+    def __str__(self):
+        return str(self._to_dict())
+
+    def __repr__(self):
+        return f'Struct({repr(self._to_dict())})'
+
+
+def _build_configs_tree():
+    root = Path(__file__).resolve().parent
+
+    tree = {}
+
+    for config in root.glob('**/*.json'):
+        leaf = tree
+        for part in config.relative_to(root).parent.parts:
+            if part not in leaf:
+                leaf[part] = {}
+            leaf = leaf[part]
+        leaf[config.stem] = config
+
+    return Struct(tree)
+
+
+configs = _build_configs_tree()
