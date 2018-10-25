5b5f11f416ed41f589a5c76051b517bf3fbbca6e
Aleksey Lymar <yoptar@gmail.com>
2018-10-25 12:13:51 +0300

    refactor: rename Sturct's _to_dict method to _asdict to conform with namedtuple's naming

--- deeppavlov/configs/__init__.py
+++ deeppavlov/configs/__init__.py
@@ -19,20 +19,20 @@ class Struct(Mapping):
                     Struct(value) if isinstance(value, dict) else value)
         self._keys = frozenset(self._keys)
 
-    def _to_dict(self) -> dict:
-        return {key: value._to_dict() if isinstance(value, Struct) else value
+    def _asdict(self) -> dict:
+        return {key: value._asdict() if isinstance(value, Struct) else value
                 for key, value in self.__dict__.items() if key in self._keys}
 
     def __getitem__(self, key: str):
         if key not in self._keys:
             raise KeyError(key)
-        return self._to_dict()[key]
+        return self._asdict()[key]
 
     def __str__(self):
-        return str(self._to_dict())
+        return str(self._asdict())
 
     def __repr__(self):
-        return f'Struct({repr(self._to_dict())})'
+        return f'Struct({repr(self._asdict())})'
 
 
 def _build_configs_tree():
