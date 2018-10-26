836fe8c78bc58f2eda69a203b72470b7f07a9793
Aleksey Lymar <yoptar@gmail.com>
2018-10-26 11:33:11 +0300

    fix: do not raise in deeppavlov.__init__ because of absent requirements

--- deeppavlov/__init__.py
+++ deeppavlov/__init__.py
@@ -13,10 +13,13 @@
 # limitations under the License.
 import sys
 
-from .configs import configs
-from .core.commands.infer import build_model
-from .core.commands.train import train_evaluate_model_from_config
-from .download import deep_download
+try:
+    from .configs import configs
+    from .core.commands.infer import build_model
+    from .core.commands.train import train_evaluate_model_from_config
+    from .download import deep_download
+except ImportError:
+    'Assuming that requirements are not yet installed'
 
 __version__ = '0.0.9'
 __author__ = 'Neural Networks and Deep Learning lab, MIPT'
