ce8e53e6c55c06dcf8fc07f80785c141040d10d7
mu-arkhipov <arkhipov@yahoo.com>
2018-11-13 11:36:14 +0300

    fix: remove interact pretrained model from tests

--- tests/test_quick_start.py
+++ tests/test_quick_start.py
@@ -102,7 +102,7 @@ PARAMS = {
         ("ner/ner_conll2003.json", "ner_conll2003", ALL_MODES): [ONE_ARGUMENT_INFER_CHECK],
         ("ner/ner_dstc2.json", "slotfill_dstc2", ALL_MODES): [ONE_ARGUMENT_INFER_CHECK],
         ("ner/ner_ontonotes.json", "ner_ontonotes", ALL_MODES): [ONE_ARGUMENT_INFER_CHECK],
-        ("ner/ner_few_shot_ru_simulate.json", "ner_fs", ALL_MODES): [ONE_ARGUMENT_INFER_CHECK],
+        ("ner/ner_few_shot_ru_simulate.json", "ner_fs", ('TI',)): [ONE_ARGUMENT_INFER_CHECK],
         ("ner/ner_rus.json", "ner_rus", ('IP')): [ONE_ARGUMENT_INFER_CHECK],
         ("ner/slotfill_dstc2.json", "slotfill_dstc2", ('IP',)):
             [
