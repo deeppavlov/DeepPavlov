c4a7e03e1de5da44fc9a614f96154173c419a484
Dilyara Baymurzina <dilyara.rimovna@gmail.com>
2018-11-14 14:58:31 +0300

    feat: russian sentiment with elmo embeddings (#580)
    * feature: config for new rusentiment
    
    * feature: config for new rusentiment
    
    * docs: info about new elmo model
    
    * fix: add tests

--- tests/test_quick_start.py
+++ tests/test_quick_start.py
@@ -17,7 +17,6 @@ import deeppavlov
 from deeppavlov.download import deep_download
 from deeppavlov.core.data.utils import get_all_elems_from_json
 from deeppavlov.core.common.paths import get_settings_path
-import utils
 from utils.server_utils.server import get_server_params, SERVER_CONFIG_FILENAME
 
 
@@ -79,7 +78,8 @@ PARAMS = {
         ("classifiers/sentiment_twitter.json", "classifiers", ALL_MODES): [ONE_ARGUMENT_INFER_CHECK],
         ("classifiers/sentiment_twitter_preproc.json", "classifiers", ALL_MODES): [ONE_ARGUMENT_INFER_CHECK],
         ("classifiers/topic_ag_news.json", "classifiers", ALL_MODES): [ONE_ARGUMENT_INFER_CHECK],
-        ("classifiers/rusentiment_cnn.json", "classifiers", ALL_MODES): [ONE_ARGUMENT_INFER_CHECK]
+        ("classifiers/rusentiment_cnn.json", "classifiers", ALL_MODES): [ONE_ARGUMENT_INFER_CHECK],
+        ("classifiers/rusentiment_elmo.json", "classifiers", ALL_MODES): [ONE_ARGUMENT_INFER_CHECK]
     },
     "snips": {
         ("classifiers/intents_snips.json", "classifiers", ('TI',)): [ONE_ARGUMENT_INFER_CHECK],
