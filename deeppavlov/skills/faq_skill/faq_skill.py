08c5c2e5d08f064f43de11ce827b0dede045313b
Nikita Velikovskiy <velik.nikita@gmail.com>
2018-12-04 18:09:50 +0300

    fix: import order, FAQSkill args, FAQSkill attributes, arguments types, using 'x_col_name' and 'y_col_name', call description, using model instead of a skill

--- deeppavlov/skills/faq_skill/faq_skill.py
+++ deeppavlov/skills/faq_skill/faq_skill.py
@@ -1,40 +1,32 @@
+from typing import Tuple, Optional
+import json
+
+from deeppavlov import train_model
 from deeppavlov.core.skill.skill import Skill
 from deeppavlov.core.common.file import read_json
-from deeppavlov.core.commands.infer import build_model
-from deeppavlov import train_model
 from deeppavlov.core.common.file import find_config
-from deeppavlov.skills.default_skill.default_skill import DefaultStatelessSkill
-
-from typing import Tuple, Optional, Union
-import json
 
 class FAQSkill(Skill):
 	"""Skill, matches utterances to questions, returns predefined answers.
 
 	Allows to create skills that give answers on frequently asked questions.
-	Every skill returns response and confidence.
+	Skill returns response and confidence.
 
 	Args:
 		data_path: Path to '.csv' file that contains two columns with Questions and Answers.
 			User's utterance will be compared with Questions column and respond will be selected
 			from matching row from Answers column.
-		data_path: URL to '.csv' file that contains two columns with Questions and Answers.
+		data_url: URL to '.csv' file that contains two columns with Questions and Answers.
 			User's utterance will be compared with Questions column and respond will be selected
 			from matching row from Answers column.
 		x_col_name: Name of the column in '.csv' file, that represents Question column.
 		y_col_name: Name of the column in '.csv' file, that represents Answer column.
 
 	Attributes:
-		data_path: Path to '.csv' file that contains two columns with Questions and Answers.
-			User's utterance will be compared with Questions column and respond will be selected
-			from matching row from Answers column.
-		data_path: URL to '.csv' file that contains two columns with Questions and Answers.
-			User's utterance will be compared with Questions column and respond will be selected
-			from matching row from Answers column.
-		x_col_name: Name of the column in '.csv' file, that represents Question column.
-		y_col_name: Name of the column in '.csv' file, that represents Answer column.
+		model: Сlassifies user's questions
 	"""
-	def __init__(self, data_path=None, data_url=None, x_col_name='Question', y_col_name='Answer'):
+	def __init__(self, data_path: str = None, data_url: str = None,
+		x_col_name: str = 'Question', y_col_name: str = 'Answer') -> None:
 		model_config = read_json(find_config('tfidf_autofaq'))
 
 		if data_path is None and data_url is None:
@@ -42,15 +34,18 @@ class FAQSkill(Skill):
 		if data_path is not None and data_url is not None:
 			raise ValueError("You can't specify both 'data_path' and 'data_url'")
 
-		if data_path != None:				
+		if data_path is not None:				
 			if 'data_url' in model_config['dataset_reader']:
 				del model_config['dataset_reader']['data_url']
 			model_config['dataset_reader']['data_path'] = data_path
 
-		if data_url != None:
+		if data_url is not None:
 			if 'data_path' in model_config['dataset_reader']:
 				del model_config['dataset_reader']['data_path']
 			model_config['dataset_reader']['data_url'] = data_url
+
+		model_config['dataset_reader']['x_col_name'] = x_col_name
+		model_config['dataset_reader']['y_col_name'] = y_col_name
 
 		for i in range(len(model_config['chainer']['pipe'])):
 			if 'save_path' in model_config['chainer']['pipe'][i]:
@@ -58,17 +53,13 @@ class FAQSkill(Skill):
 			if 'load_path' in model_config['chainer']['pipe'][i]:
 				model_config['chainer']['pipe'][i]['load_path']  = './' + model_config['chainer']['pipe'][i]['class_name'] + '.pkl'
 
-		model = train_model(model_config)
-		self.skill = DefaultStatelessSkill(model)
-		
+		self.model = train_model(model_config)		
 
 	def __call__(self, utterances_batch: list, history_batch: list,
-		states_batch: Optional[list]=None) -> Tuple[list, list]:
+		states_batch: Optional[list] = None) -> Tuple[list, list]:
 		"""Returns skill inference result.
 
-		Returns batches of skill inference results, estimated confidence
-		levels and up to date states corresponding to incoming utterance
-		batch.
+		Returns batches of skill inference results and estimated confidence levels
 
 		Args:
 			utterances_batch: A batch of utterances of any type.
@@ -81,13 +72,4 @@ class FAQSkill(Skill):
 			confidence: A batch of float typed confidence levels for each of
 				skill inference result.
 		"""
-		response, _, __ = self.skill(utterances_batch, history_batch)
-		answer = ['; '.join(resp.split('; ')[:-1]) for resp in response]
-		confidence = [float(resp.split('; ')[-1]) for resp in response]
-
-		return answer, confidence
-
-
-
-
-
+		return self.model(utterances_batch)
