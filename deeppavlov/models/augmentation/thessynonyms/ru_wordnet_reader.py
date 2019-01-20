4cdf474f322ab860ff0a531f0bc154b7863f1527
SultanovAR <sultanovazatruslanovich@gmail.com>
2019-01-20 21:51:47 +0300

    refactoring ruwordnet

--- deeppavlov/models/augmentation/thessynonyms/ru_wordnet_reader.py
+++ deeppavlov/models/augmentation/thessynonyms/ru_wordnet_reader.py
@@ -1,35 +1,36 @@
 import xml.etree.ElementTree as ET
-from pymorphy2 import MorphAnalyzer
-
-from deeppavlov.core.commands.utils import expand_path
-from deeppavlov.core.data.utils import download_decompress, mark_done, is_done
-
+import pymorphy2
+from deeppavlov.core.data.utils import download_decompress
 from pathlib import Path
 from typing import List
 
-class RuSynWordnet:
+from deeppavlov.core.data.utils import download_decompress
+from deeppavlov.core.common.log import get_logger
 
-    def _download_data(self, data_path: str) -> None:
-        """download ruthes-lite2 thesaurus to data_path"""
-        url = "https://github.com/SultanovAR/___ruthes-lite2_download/archive/master.zip"
-        download_decompress(url, data_path)
-        mark_done(data_path)      
+logger = get_logger(__name__)
 
-    def __init__(self, ruthes_path: str):
-        self.ruthes_path = Path(expand_path(ruthes_path))
+#прикрутить морфотегер от Алексея
+#он возвращает значения в universal... есть библиотека russian_tagsets которая переводит pymorphy тэги в universal...
+class RuWordnet:
+        
+    def __init__(self, dir_path: str, with_source_token: bool=False):
+        url = "https://github.com/SultanovAR/___ruthes-lite2_download/archive/master.zip"    
+        url = 'http://files.deeppavlov.ai/datasets/ruthes-lite2.tar.gz'
 
-        if not is_done(self.ruthes_path):
-            self._download_data(self.ruthes_path)
+        dir_path = Path(expand_path(dir_path))
+        required_files = ['text_entry.xml', 'synonyms.xml']
+        if not dir_path.exists()
+            dir_path.mkdir()
+        
+        if not all((dir_path/f)/exists() for f in required_files):
+            download_decompress(url, dir_path)
 
-        if self.ruthes_path.is_dir():
-            self._entry = ET.parse(next(self.ruthes_path.rglob('text_entry.xml')))
-            self._synonyms = ET.parse(next(self.ruthes_path.rglob('synonyms.xml')))
-            self.entry_root = self._entry.getroot()
-            self.synonyms_root = self._synonyms.getroot()
-            
-        self.morph = MorphAnalyzer()
+        self.with_source_token = with_source_token
+        self.entry_root = ET.parse(next(self.ruthes_path.rglob('text_entry.xml'))).getroot()
+        self.synonyms_root = ET.parse(next(self.ruthes_path.rglob('synonyms.xml'))).getroot()
+        self.morph = pymorphy2.MorphAnalyzer()
     
-    def _extract_morpho_requirements(self, tag):
+    def _extract_morpho_requirements(self, tag: pymorphy2.analyzer.Parse):
         #Not all morphems are need for inflection
         if tag.POS in ["NOUN"]:
             keys = {"NOUN", "case", "number", "animacy", "person", "gender"}
@@ -56,6 +57,7 @@ class RuSynWordnet:
 
     def _find_synonyms(self, lemma: str) -> List[str]:
         #parsing ruthes-lite-2 file
+        lemma = lemma.upper()
         #1 
         entry_id_set = set(map(lambda x: x.get('id'), self.entry_root.findall(f"./entry[lemma='{lemma}']")))
         #2
@@ -67,35 +69,54 @@ class RuSynWordnet:
         for concept_id in concept_id_set:
             syn_entry_id_set.update(set(map(lambda x: x.get('entry_id'), self.synonyms_root.findall(f"./entry_rel[@concept_id='{concept_id}']"))))
         #4
-        synset = list()
+        synlist = list()
         for syn_entry_id in syn_entry_id_set:
-            synset += list(map(lambda x: x.text, self.entry_root.findall(f"./entry[@id='{syn_entry_id}']/lemma")))
-        return synset
+            synlist += list(map(lambda x: x.text, self.entry_root.findall(f"./entry[@id='{syn_entry_id}']/lemma")))
+        return synlist
+    
+    def _filter(synlist: List[str], init_form: pymorphy2.analyzer.Parse, source_token: str) -> List[str]:
+        init_form = self._extract_morpho_requirements(init_form)
+        synset = set()
+        for syn in filter(lambda x: len(x.split())) == 1, synlist):
+            inflected_syn = self.morph.parse(syn)[0].inflect(init_form)
+            if inflected_syn:
+                if not(self.with_source_token) and inflected_syn != source_token:
+                    synlist.update([inflected_syn.word])
+                elif self.with_source_token:
+                    synlist.update([inflected_syn.word])
+        return list(synset)
 
-    def _find_filtered_synonyms(self, morphem):
-        #finds synonyms, filters them(len=1) and tries to infect it to right form
-        lemma = morphem.normal_form
-        init_form = self._extract_morpho_requirements(morphem.tag)
-        synlist = set()
-        for syn in filter(lambda x: len(x.split()) == 1, self._find_synonyms(lemma.upper())):
-            syn_inflected = self.morph.parse(syn)[0].inflect(init_form)
-            if syn_inflected:
-                if syn_inflected.word != morphem.word: #synlist without source token
-                    synlist.update([syn_inflected.word])
-        return list(synlist) if synlist else None
+    #def _find_filtered_synonyms(self, morphem):
+    #    #finds synonyms, filters them(len=1) and tries to infect it to right form
+    #    lemma = morphem.normal_form
+    #    init_form = self._extract_morpho_requirements(morphem.tag)
+    #    synlist = set()
+    #    for syn in filter(lambda x: len(x.split()) == 1, self._find_synonyms(lemma.upper())):
+    #        syn_inflected = self.morph.parse(syn)[0].inflect(init_form)
+    #        if syn_inflected:
+    #            if syn_inflected.word != morphem.word: #synlist without source token
+    #                synlist.update([syn_inflected.word])
+    #    return list(synlist) if synlist else None
 
-    def get_synset(self, prep_tokens: List[str], source_pos_tag: List[str]=None) -> List[List[str]]:
-        """Generating list of synonyms for each token in prep_tokens, that isn't equal to None
-            Args:
-                prep_tokens: preprocessed soucrce_tokens, where all tokens that do not need to search for synonyms are replaced by None
-                source_pos_tag: pos tags for source sentence, in nltk.pos_texitag format
-            Return:
-                List of list of synonyms without source token, for tokens for which no synonyms were found, return None
-        """
-        # token </s> to do
-        morphems = list(map(lambda x: self.morph.parse(x)[0] if x else None, prep_tokens))
-        result = list(map(lambda x: self._find_filtered_synonyms(x) if x else None, morphems))
-        return result
+    def get_synlist(self, token: str, pos_tag: str=None) -> List[str]:
+        if token:
+            morphem = self.morph.parse(x)[0]
+            synonyms = self._find_synonyms(morphem.normal_form)
+            synonyms = self._filter(synonyms, morphem.tag, morphem.normal_form)
+            return synonyms
+
+    #def get_synset(self, prep_tokens: List[str], source_pos_tag: List[str]=None) -> List[List[str]]:
+    #    """Generating list of synonyms for each token in prep_tokens, that isn't equal to None
+    #        Args:
+    #            prep_tokens: preprocessed soucrce_tokens, where all tokens that do not need to search for synonyms are replaced by None
+    #            source_pos_tag: pos tags for source sentence, in nltk.pos_texitag format
+    #        Return:
+    #            List of list of synonyms without source token, for tokens for which no synonyms were found, return None
+    #    """
+    #    morphems = list(map(lambda x: self.morph.parse(x)[0] if x else None, prep_tokens))
+#
+    #    result = list(map(lambda x: self._find_filtered_synonyms(x) if x else None, morphems))
+    #    return result
 
 if __name__ == "__main__":
     print("""Testing the module "RuSyn" """)
