fc30dc72045743ae26ae77e398def31da014e26e
mu-arkhipov <arkhipov@yahoo.com>
2019-01-23 16:08:57 +0300

    chore: remove unnecessary imports from max_n class

--- deeppavlov/models/simple_qa/max_n.py
+++ deeppavlov/models/simple_qa/max_n.py
@@ -1,24 +1,21 @@
 import numpy as np
 
-from deeppavlov.core.commands.utils import expand_path
-from deeppavlov.core.common.log import get_logger
 from deeppavlov.core.common.registry import register
-from deeppavlov.core.data.utils import download
 from deeppavlov.core.models.component import Component
-from deeppavlov.core.models.estimator import Estimator
+from deeppavlov.core.models.serializable import Serializable
 
 import os
 import pickle
 
 @register('max_n')
 class Max_n(Component):
-    
+
     def __init__(self, filename, *args, **kwargs):
         fl = open(filename, 'r')
         lines = fl.readlines()
         self.classes = [line.split('\t')[0] for line in lines]
-        
-    
+
+
     def __call__(self, probas_batch, *args, **kwargs):
         
         max_n_batch = []
