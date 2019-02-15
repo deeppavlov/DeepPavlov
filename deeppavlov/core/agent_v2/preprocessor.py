9ed4a051ab0d0f31e91406026eec50e16e809893
Aleksey Lymar <yoptar@gmail.com>
2019-02-15 13:50:12 +0300

    feat: use preprocessors as REST services

--- deeppavlov/core/agent_v2/preprocessor.py
+++ deeppavlov/core/agent_v2/preprocessor.py
@@ -1,44 +1,33 @@
 from concurrent.futures import ThreadPoolExecutor
-from itertools import chain
-from typing import Collection, Dict, List, Any, Optional, Union, Callable
+from functools import partial
+from typing import Dict, List, Any, Optional
+
+import requests
+
+
+def _annotator_request(name, url, payload):
+    r = requests.post(url, json=payload)
+    if r.status_code != 200:
+        raise RuntimeError(f'Got {r.status_code} status code for {url}')
+    return [{name: response} for response in r.json()['responses']]
 
 
 class Preprocessor:
 
-    def __init__(self, annotators: Dict[Callable, Union[str, List[Optional[str]]]], *,
+    def __init__(self, names: List[str], urls: List[str], *,
                  max_workers: Optional[int] = None) -> None:
         self.executor = ThreadPoolExecutor(max_workers=max_workers)
-        self.annotators = []
-        self.keys = []
-        for preprocessor, keys in annotators.items():
-            if isinstance(keys, str):
-                keys = [keys]
+        self.names = names
 
-            if len(keys) == 1:
-                old_preprocessor = preprocessor
+        self.annotators_functions = [partial(_annotator_request, name, url)
+                                     for name, url in zip(names, urls)]
 
-                def preprocessor(x):
-                    return [old_preprocessor(x)]
-            elif None in keys:
-                indexes, keys = zip(*[(i, k) for i, k in enumerate(keys) if k is not None])
-                old_preprocessor = preprocessor
-
-                def preprocessor(x):
-                    res = old_preprocessor(x)
-                    return [res[i] for i in indexes]
-
-            self.keys.extend([k.split('.') for k in keys])
-            self.annotators.append(preprocessor)
-
-    def __call__(self, utterances: Collection[str]) -> List[Dict[str, Any]]:
+    def __call__(self, payload: dict) -> List[Dict[str, Dict[str, Any]]]:
         annotations = []
-        for preprocessed in zip(*chain(*self.executor.map(lambda f: f(utterances), self.annotators))):
+        for preprocessed in zip(*self.executor.map(lambda f: f(payload), self.annotators_functions)):
             dialog_annotations = {}
-            for k, data in zip(self.keys, preprocessed):
-                target = dialog_annotations
-                for prefix in k[:-1]:
-                    target = target.setdefault(prefix, {})
-                target[k[-1]] = data
+            for data in preprocessed:
+                dialog_annotations.update(data)
 
             annotations.append(dialog_annotations)
 
