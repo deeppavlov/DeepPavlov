9f78cd8e66f1f3998aaf1dd59a1ebc2c3a9d1197
Olga Gureenkova <ol.gure@gmail.com>
2019-02-19 14:41:52 +0300

    merge: agent

--- deeppavlov/core/agent_v2/tests/state_manager_test.py
+++ deeppavlov/core/agent_v2/tests/state_manager_test.py
@@ -1,4 +1,4 @@
-from deeppavlov.core.agent_v2.states_manager import StatesManager
+from deeppavlov.core.agent_v2.state_manager import StateManager
 
-sm = StatesManager()
+sm = StateManager()
 sm.get_users(user_telegram_ids=["44d279ea-62ab-4c71-9adb-ed69143c12eb", "56f1d5b2-db1a-4128-993d-6cd1bc1b938f"])
