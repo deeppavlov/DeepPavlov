27f87cac8604999e7f4fdc7b9382755f578ee16c
Olga Gureenkova <ol.gure@gmail.com>
2019-02-19 19:33:23 +0300

    feat: update skill_manager

--- deeppavlov/core/agent_v2/skill_manager.py
+++ deeppavlov/core/agent_v2/skill_manager.py
@@ -1,22 +1,20 @@
+from deeppavlov.core.agent_v2.rest_caller import RestCaller
+from deeppavlov.core.agent_v2.config import AGENT_CONFIG
+
+
 class SkillManager:
-    def __init__(self, skills, skills_selector, response_selector):
-        self.skills = skills
+
+    def __init__(self, skills_selector, response_selector, rest_caller: RestCaller):
         self.skills_selector = skills_selector
         self.response_selector = response_selector
+        self.rest_caller = RestCaller(self.max_workers)
+        self.skills = AGENT_CONFIG['skills']
+        self.max_workers = AGENT_CONFIG['max_workers']
 
     def __call__(self, state):
-        active_skills = self.skills_selector.select_skills(state)
-        skill_responses = self.get_responses(active_skills, state)
+        active_skills = self.skills_selector.select_skills(self.skills, state)
+        active_skill_names = [s['name'] for s in active_skills]
+        active_skill_urls = [s['url'] for s in active_skills]
+        skill_responses = self.rest_caller(active_skill_names, active_skill_urls, state)
         response = self.response_selector.select_response(skill_responses, state)
         return response
-
-    def get_responses(self, active_skills, state):
-        """
-        Call to skill services, return response.
-        Args:
-            active_skills:
-
-        Returns:
-
-        """
-        ...
