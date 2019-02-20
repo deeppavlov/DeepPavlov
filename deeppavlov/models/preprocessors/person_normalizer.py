23d3d2bd11ca7fddc81a53d08f75aebc2761318a
Mariia Vikhreva <mary.vikhreva@gmail.com>
2019-02-20 10:57:41 +0300

    feat: add person_normalizer & myself_dettector

---
+++ deeppavlov/models/preprocessors/person_normalizer.py
@@ -0,0 +1,209 @@
+# Copyright 2017 Neural Networks and Deep Learning lab, MIPT
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import List, Dict, Tuple
+
+from deeppavlov.core.common.registry import register
+from deeppavlov.core.models.component import Component
+
+
+@register('person_normalizer')
+class PersonNormalizer(Component):
+    """
+    Detects mentions of mate user's name and either
+    (0) converts them to user's name taken from state
+    (1) either removes them.
+
+    Parameters:
+        person_tag: tag name that corresponds to a person entity
+    """
+    def __init__(self, person_tag: str = 'PER', **kwargs):
+        self.per_tag = person_tag
+
+    def __call__(self,
+                 tokens: List[List[str]],
+                 tags: List[List[str]],
+                 states: List[Dict]) -> Tuple[List[List[str]], List[List[str]]]:
+        out_tokens, out_tags = [], []
+        tokens, tags = self.tag_mate_gooser_name(tokens, tags, person_tag=self.per_tag)
+        for u_state, u_toks, u_tags in zip(states, tokens, tags):
+            if u_state.get('user_name'):
+                u_toks, u_tags = self.replace_mate_gooser_name(u_toks,
+                                                               u_tags,
+                                                               u_state['user_name'])
+            else:
+                u_toks, u_tags = self.remove_mate_gooser_name(u_toks, u_tags)
+            out_tokens.append(u_toks)
+            out_tags.append(u_tags)
+        return out_tokens, out_tags
+
+    @staticmethod
+    def tag_mate_gooser_name(tokens: List[str],
+                             tags: List[str],
+                             person_tag: str = 'PER',
+                             mate_tag: str = 'MATE-GOOSER') -> \
+            Tuple[List[str], List[str]]:
+        if 'B-PER' not in tags:
+            return tokens, tags
+        out_tags = []
+        i = 0
+        while (i < len(tokens)):
+            tok, tag = tokens[i], tags[i]
+            if i + 1 < len(tokens):
+                if (tok == ',') and (tags[i + 1] == 'B-' + person_tag):
+                    # it will be mate gooser name
+                    out_tags.append(tag)
+                    i += 1
+                    while (i < len(tokens)) and (tags[i][2:] == person_tag):
+                        out_tags.append(tags[i][:2] + mate_tag)
+                        i += 1
+                    continue
+            if i > 0:
+                if (tok == ',') and (tags[i - 1][2:] == 'PER'):
+                    # that was mate gooser name
+                    step = 1
+                    while (len(out_tags) >= step) and (out_tags[-step][2:] == person_tag):
+                        out_tags[-step] = out_tags[-step][:2] + mate_tag
+                        step += 1
+                    out_tags.append(tag)
+                    i += 1
+                    continue
+            out_tags.append(tag)
+            i += 1
+        return tokens, out_tags
+
+    @staticmethod
+    def replace_mate_gooser_name(tokens: List[str],
+                                 tags: List[str],
+                                 replacement: str,
+                                 mate_tag: str = 'MATE-GOOSER') ->\
+            Tuple[List[str], List[str]]:
+        if 'B-' + mate_tag not in tags:
+            return tokens, tags
+
+        repl_tokens = replacement.split()
+        repl_tags = ['B-' + mate_tag] + ['I-' + mate_tag] * (len(repl_tokens) - 1)
+
+        out_tokens, out_tags = [], []
+        i = 0
+        while (i < len(tokens)):
+            tok, tag = tokens[i], tags[i]
+            if tag == 'B-' + mate_tag:
+                out_tokens.extend(repl_tokens)
+                out_tags.extend(repl_tags)
+                i += 1
+                while (i < len(tokens)) and (tokens[i] == 'I-' + mate_tag):
+                    i += 1
+            else:
+                out_tokens.append(tok)
+                out_tags.append(tag)
+                i += 1
+        return out_tokens, out_tags
+
+    @staticmethod
+    def remove_mate_gooser_name(tokens: List[str],
+                                tags: List[str],
+                                mate_tag: str = 'MATE-GOOSER') ->\
+            Tuple[List[str], List[str]]:
+        # TODO: uppercase first letter if name was removed
+        if 'B-' + mate_tag not in tags:
+            return tokens, tags
+        out_tokens, out_tags = [], []
+        i = 0
+        while (i < len(tokens)):
+            tok, tag = tokens[i], tags[i]
+            if i + 1 < len(tokens):
+                if (tok == ',') and (tags[i + 1] == 'B-' + mate_tag):
+                    # it will be mate gooser name next, skip comma
+                    i += 1
+                    continue
+            if i > 0:
+                if (tok == ',') and (tags[i - 1][2:] == mate_tag):
+                    # that was mate gooser name, skip comma
+                    i += 1
+                    continue
+            if tag[2:] != mate_tag:
+                out_tokens.append(tok)
+                out_tags.append(tag)
+            i += 1
+        return out_tokens, out_tags
+
+
+LIST_LIST_STR_BATCH = List[List[List[str]]]
+
+
+@register('history_person_normalizer')
+class HistoryPersonNormalize(Component):
+    """
+    Takes batch of dialog histories and normalizes only bot responses.
+
+    Detects mentions of mate user's name and either
+    (0) converts them to user's name taken from state
+    (1) either removes them.
+
+    Parameters:
+        per_tag: tag name that corresponds to a person entity
+    """
+    def __init__(self, per_tag: str = 'PER', **kwargs):
+        self.per_normalizer = PersonNormalizer(per_tag=per_tag)
+
+    def __call__(self,
+                 history_tokens: LIST_LIST_STR_BATCH,
+                 tags: LIST_LIST_STR_BATCH,
+                 states: List[Dict]) -> Tuple[LIST_LIST_STR_BATCH, LIST_LIST_STR_BATCH]:
+        out_tokens, out_tags = [], []
+        for u_state, u_hist_tokens, u_hist_tags in zip(states, history_tokens, tags):
+            # TODO: normalize bot response history
+            pass
+        return out_tokens, out_tags
+
+
+@register('myself_detector')
+class MyselfDetector(Component):
+    """
+    Finds first mention of a name and sets it as a user name.
+
+    Parameters:
+        person_tag: tag name that corresponds to a person entity
+        state_slot: name of a state slot corresponding to a user's name
+
+    """
+    def __init__(self,
+                 person_tag: str = 'PER',
+                 state_slot: str = 'user_name',
+                 **kwargs):
+        self.per_tag = person_tag
+        self.state_slot = state_slot
+
+    def __call__(self,
+                 tokens: List[List[str]],
+                 tags: List[List[str]],
+                 states: List[Dict]) -> List[Dict]:
+        out_states = []
+        for u_state, u_toks, u_tags in zip(states, tokens, tags):
+            if not u_state.get('user_name'):
+                name_found = self.find_my_name(tokens, tags, person_tag=self.per_tag)
+                if name_found is not None:
+                    u_state['user_name'] = name_found
+        return out_states
+
+    @staticmethod
+    def find_my_name(tokens: List[str], tags: List[str], person_tag: str) -> str:
+        if 'B-' + person_tag not in tags:
+            return None
+        per_start = tags.index('B-' + person_tag)
+        per_excl_end = per_start + 1
+        while (per_excl_end < len(tokens)) and (tags[per_excl_end] == 'I-' + person_tag):
+            per_excl_end += 1
+        return ' '.join(tokens[per_start:per_excl_end])
