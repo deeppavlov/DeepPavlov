89d9f58bf12e03f59b5b6364be0bb2102f7bd8d2
Olga Gureenkova <ol.gure@gmail.com>
2019-02-21 17:57:49 +0300

    feat: add confidence response selector

--- deeppavlov/core/agent_v2/response_selector.py
+++ deeppavlov/core/agent_v2/response_selector.py
@@ -35,4 +35,12 @@ class ConfidenceResponseSelector(ResponseSelector):
         super().__init__()
 
     def __call__(self, responses: Dict, state: Dict) -> Tuple[List[str], List[str], List[float]]:
-        ...
+        skill_names = []
+        utterances = []
+        confidences = []
+        for r in responses:
+            sr = sorted(r.items(), key=lambda x: x[1]['confidence'], reverse=True)[0]
+            skill_names.append(sr[0])
+            utterances.append(sr[1]['text'])
+            confidences.append(sr[1]['confidence'])
+        return skill_names, utterances, confidences
