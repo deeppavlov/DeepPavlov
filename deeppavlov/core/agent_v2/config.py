3ec5b1dcb5b677d5ed4a589791ea62fd2df33d0c
Olga Gureenkova <ol.gure@gmail.com>
2019-02-26 17:56:46 +0300

    chore: update chainer out key names, add chitchat to the agent config

--- deeppavlov/core/agent_v2/config.py
+++ deeppavlov/core/agent_v2/config.py
@@ -4,10 +4,12 @@ MAX_WORKERS = 4
 SKILLS = [
     {
         "name": "odqa",
-        "url": "http://0.0.0.0:2080/odqa"
-     },
+        "url": "http://0.0.0.0:2080/odqa",
+        "path": "deeppavlov/configs/dp_assistant/agent_ru_odqa_infer_wiki.json"
+    },
     {
-        "name": "faq",
-        "url": "http://0.0.0.0:2081/faq"
+        "name": "chitchat",
+        "url": "http://0.0.0.0:2081/chitchat",
+        "path": "deeppavlov/configs/dp_assistant/ranking_chitchat_2staged_tfidf_smn_v4.json"
     }
 ]
