4a181165264814eae5c5c5ee5719e4252ca9f670
Olga Gureenkova <ol.gure@gmail.com>
2019-02-27 12:04:06 +0300

    chore: update agent config

--- deeppavlov/core/agent_v2/config.py
+++ deeppavlov/core/agent_v2/config.py
@@ -1,4 +1,3 @@
-
 MAX_WORKERS = 4
 
 SKILLS = [
@@ -10,6 +9,31 @@ SKILLS = [
     {
         "name": "chitchat",
         "url": "http://0.0.0.0:2081/chitchat",
-        "path": "deeppavlov/configs/dp_assistant/ranking_chitchat_2staged_tfidf_smn_v4.json"
+        "path": "deeppavlov/configs/dp_assistant/agent_ranking_chitchat_2staged_tfidf_smn_v4.json"
     }
 ]
+
+ANNOTATORS = [
+    {
+        "name": "ner",
+        "url": "http://",
+        "path": "deeppavlov/configs/dp_assistant/preproc_ner_rus.json"
+    },
+    {
+        "name": "sentiment",
+        "url": "http://",
+        "path": ""
+    }
+]
+
+SKILL_SELECTORS = [
+    {
+        "name": "chitchat_odqa",
+        "utl": "http://0.0.0.0:2082/chitchat_odqa_selector",
+        "path": ""
+    }
+]
+
+RESPONSE_SELECTORS = []
+
+# TODO include Bot?
