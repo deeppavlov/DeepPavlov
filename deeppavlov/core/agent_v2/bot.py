7ef5f4f5ba59db39c2ce300675df17d6ca8c072f
Aleksey Lymar <yoptar@gmail.com>
2019-02-27 16:20:16 +0300

    feat: create a bot user if it's not there

--- deeppavlov/core/agent_v2/bot.py
+++ deeppavlov/core/agent_v2/bot.py
@@ -1,8 +1,8 @@
 from deeppavlov.core.agent_v2.state_schema import User
 from deeppavlov.core.agent_v2.connection import connect
 
-# Create BOT
-# BOT = User(user_type='bot')
-# BOT.save()
-
-BOT = User.objects(id__exact='5c68347a0110b32ee73b2d97')[0]
+try:
+    BOT = User.objects(id__exact='5c68347a0110b32ee73b2d97')[0]
+except IndexError:
+    BOT = User(id='5c68347a0110b32ee73b2d97', user_type='bot')
+    BOT.save()
