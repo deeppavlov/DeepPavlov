967ae88d5ccf8dc0989bc3b8decbdc7a4667e1a2
litinsky <alitinsky@gmail.com>
2019-03-14 12:22:11 +0300

    feat: agent yaml config (#754)
    * Implemented cofiguration from yaml file
    
    * Added database config section
    
    * Minor fixes in config.yaml
    
    * Swiched off yaml config usage by default
    
    * style: fix imports order in the new agent_v2.config
    
    * chore: add PyYAML to project requirements
    
    * chore: set mongoengine requirement to a specific version
    
    * style: use proper paths in config.yaml instead of config object attrs
    
    * style: imports order in connection.py

--- deeppavlov/core/agent_v2/connection.py
+++ deeppavlov/core/agent_v2/connection.py
@@ -1,7 +1,25 @@
+import sys
+from pathlib import Path
+
+import yaml
 from mongoengine import connect
 
 DB_NAME = 'test'
 HOST = 'localhost'
 PORT = 27017
+
+_run_config_path: Path = Path(__file__).resolve().parent / 'config.yaml'
+_module = sys.modules[__name__]
+
+if _run_config_path.is_file():
+    with _run_config_path.open('r', encoding='utf-8') as f:
+        config: dict = yaml.safe_load(f)
+
+    if config.get('use_config', False) is True:
+        config = config.get('db_config', {})
+
+        DB_NAME = config.get('DB_NAME', DB_NAME)
+        HOST = config.get('HOST', HOST)
+        PORT = config.get('PORT', PORT)
 
 state_storage = connect(host=HOST, port=PORT, db=DB_NAME)
