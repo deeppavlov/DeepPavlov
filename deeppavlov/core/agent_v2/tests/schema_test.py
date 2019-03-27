372ec5a689d022bdd937824f4bb3c7fdd2fb7fb6
Mariia Vikhreva <mary.vikhreva@gmail.com>
2019-03-27 11:57:42 +0300

    docs: update state version

--- deeppavlov/core/agent_v2/tests/schema_test.py
+++ deeppavlov/core/agent_v2/tests/schema_test.py
@@ -37,7 +37,7 @@ print(b_utt_3.to_dict())
 # for d in Dialog.objects:
 #    print(d.to_dict())
 
-state = {'version': '0.9.4', 'dialogs': []}
+state = {'version': '0.10.1', 'dialogs': []}
 for d in Dialog.objects:
     state['dialogs'].append(d.to_dict())
 
