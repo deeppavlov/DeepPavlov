ab672129a03ca047b37285bbbac43c2c847074b0
Mariia Vikhreva <mary.vikhreva@gmail.com>
2019-03-27 11:01:13 +0300

    fix: name retrieve from state

--- deeppavlov/core/agent_v2/postprocessor.py
+++ deeppavlov/core/agent_v2/postprocessor.py
@@ -14,18 +14,24 @@ class Postprocessor:
 
 class DefaultPostprocessor(Postprocessor):
     def __init__(self) -> None:
-        self.per_normali23zer = PersonNormalizer(per_tag='PER')
+        self.person_normalizer = PersonNormalizer(per_tag='PER')
 
-    def __call__(self, states: Sequence[dict]) -> Sequence[str]:
+    def __call__(self, state: dict) -> Sequence[str]:
         new_responses = []
-        for state in states:
+        for d_state in state['dialogs']:
             # get tokens & tags
-            response = state['dialogs']['utterances'][-1]
-            response_toks, response_tags = response['annotations']['ner']
+            response = d_state['utterances'][-1]
+            ner_annotations = response['annotations']['ner']
+            user_name = d_state['user']['profile']['name']
             # replace names with user name
-            response_toks_norm, _ = self.person_normalizer([response_toks],
-                                                           [response_tags],
-                                                           [state])
-            # detokenize
-            new_responses.append(detokenize(response_toks_norm))
+            if ner_annotations:
+                response_toks_norm, _ = \
+                    self.person_normalizer([ner_annotations['tokens']],
+                                           [ner_annotations['tags']],
+                                           [user_name])
+                response_toks_norm = response_toks_norm[0]
+                # detokenize
+                new_responses.append(detokenize(response_toks_norm))
+            else:
+                new_responses.append(response['text'])
         return new_responses
