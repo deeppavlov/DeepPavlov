0a5b6a8ed90a90a4eee721ce29d9de290293478c
Mariia Vikhreva <mary.vikhreva@gmail.com>
2019-03-28 10:17:52 +0300

    feat: use annotations in preprocessor, do not reload ner

--- deeppavlov/models/dp_assistant/states_parser.py
+++ deeppavlov/models/dp_assistant/states_parser.py
@@ -27,10 +27,34 @@ class DialogsParser(Component):
 
             last_utterances.append(utterances_history[-1])
             utterances_histories.append(utterances_history)
-            last_annotations = annotations_history[-1]
+            last_annotations.append(annotations_history[-1])
             annotations_histories.append(annotations_history)
 
             dialog_ids.append(dialog['id'])
             user_ids.append(dialog['user']['id'])
 
         return last_utterances, last_annotations, utterances_histories, annotations_histories, dialog_ids, user_ids
+
+
+@register('annotations_parser')
+class AnnotationsParser(Component):
+    """ Inputs utterance annotations and gets recursive values.
+
+    Example:
+        > parser = AnnotaionsParser(keys=['ner.tokens', 'ner.tags'])
+        > parser([{'ner': {'tokens': ['I'], 'tags': ['O']}}])
+        [['I']], [['O']]
+    """
+
+    def __init__(self, keys, **kwargs):
+        self.keys = [k.split('.') for k in keys]
+
+    def __call__(self, annotations: List[dict]) -> List[List]:
+        ann_values = [[]] * len(self.keys)
+        for ann in annotations:
+            for i, key_rec in enumerate(self.keys):
+                val = ann
+                for j in range(len(key_rec)):
+                    val = val[key_rec[j]]
+                ann_values[i] = ann_values[i] + [val]
+        return ann_values
