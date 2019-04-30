387bb02bb54d6bec3b3618e62a97aa8618acca8c
Evgeny <theaswert@yandex.ru>
2019-04-30 16:35:28 +0300

    Add SMN, DAM, DAM-USE-T ranking models (#817)
    * Add SMN, DAM, DAM-USE-T ranking models
    
    * Update doc features.rst
    
    * Fix typo in docstrings
    
    * Update docs for ranking
    
    * Update test
    
    * Apply equested changes
    
    * Change type of emb_matrix to Optoinal
    
    * Remove codecs import

---
+++ deeppavlov/models/ranking/deep_attention_matching_network.py
@@ -0,0 +1,245 @@
+# Copyright 2018 Neural Networks and Deep Learning lab, MIPT
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from logging import getLogger
+from typing import Optional
+
+import numpy as np
+import tensorflow as tf
+
+from deeppavlov.core.common.registry import register
+from deeppavlov.models.ranking.tf_base_matching_model import TensorflowBaseMatchingModel
+from deeppavlov.models.ranking.matching_models.dam_utils import layers
+from deeppavlov.models.ranking.matching_models.dam_utils import operations as op
+
+log = getLogger(__name__)
+
+
+@register('dam_nn')
+class DAMNetwork(TensorflowBaseMatchingModel):
+    """
+    Tensorflow implementation of Deep Attention Matching Network (DAM)
+
+    ```
+    @inproceedings{ ,
+      title={Multi-Turn Response Selection for Chatbots with Deep Attention Matching Network},
+      author={Xiangyang Zhou, Lu Li, Daxiang Dong, Yi Liu, Ying Chen, Wayne Xin Zhao, Dianhai Yu and Hua Wu},
+      booktitle={Proceedings of the 56th Annual Meeting of the Association for Computational Linguistics (Volume 1: Long Papers)},
+      volume={1},
+      pages={  --  },
+      year={2018}
+    }
+    ```
+    http://aclweb.org/anthology/P18-1103
+
+    Based on authors' Tensorflow code: https://github.com/baidu/Dialogue/tree/master/DAM
+
+    Args:
+        num_context_turns (int): A number of ``context`` turns in data samples.
+        max_sequence_length(int): A maximum length of text sequences in tokens.
+            Longer sequences will be truncated and shorter ones will be padded.
+        learning_rate (float): Initial learning rate.
+        emb_matrix (np.ndarray): An embeddings matrix to initialize an embeddings layer of a model.
+        trainable_embeddings (bool): Whether train embeddings matrix or not.
+        embedding_dim (int): Dimensionality of token (word) embeddings.
+        is_positional (bool): Adds a bunch of sinusoids of different frequencies to an embeddings.
+        filters2_conv3d (int): number of filters in the second conv3d layer (cnn aggregation). Default: 16.
+        stack_num (int): Number of stack layers, default is 5.
+        seed (int): Random seed.
+        decay_steps (int): Number of steps after which is to decay the learning rate.
+    """
+
+    def __init__(self,
+                 embedding_dim: int = 200,
+                 num_context_turns: int = 10,
+                 max_sequence_length: int = 50,
+                 learning_rate: float = 1e-3,
+                 emb_matrix: Optional[np.ndarray] = None,
+                 trainable_embeddings: bool = False,
+                 is_positional: bool = True,
+                 filters2_conv3d: int = 16,
+                 stack_num: int = 5,
+                 seed: int = 65,
+                 decay_steps: int = 600,
+                 *args,
+                 **kwargs):
+
+        self.seed = seed
+        tf.set_random_seed(self.seed)
+
+        self.num_context_turns = num_context_turns
+        self.max_sentence_len = max_sequence_length
+        self.word_embedding_size = embedding_dim
+        self.trainable = trainable_embeddings
+        self.is_positional = is_positional
+        self.stack_num = stack_num
+        self.filters2_conv3d = filters2_conv3d
+
+        self.learning_rate = learning_rate
+        self.emb_matrix = emb_matrix
+        self.decay_steps = decay_steps
+
+        self.sess_config = tf.ConfigProto(allow_soft_placement=True)
+        self.sess_config.gpu_options.allow_growth = True
+        self.sess = tf.Session(config=self.sess_config)
+        self._init_graph()
+        self.sess.run(tf.global_variables_initializer())
+
+        super(DAMNetwork, self).__init__(*args, **kwargs)
+
+        if self.load_path is not None:
+            self.load()
+
+    def _init_placeholders(self):
+        with tf.variable_scope('inputs'):
+            # Utterances and their lengths
+            self.utterance_ph = tf.placeholder(tf.int32, shape=(None, self.num_context_turns, self.max_sentence_len))
+            self.all_utterance_len_ph = tf.placeholder(tf.int32, shape=(None, self.num_context_turns))
+
+            # Responses and their lengths
+            self.response_ph = tf.placeholder(tf.int32, shape=(None, self.max_sentence_len))
+            self.response_len_ph = tf.placeholder(tf.int32, shape=(None,))
+
+            # Labels
+            self.y_true = tf.placeholder(tf.int32, shape=(None,))
+
+    def _init_graph(self):
+        self._init_placeholders()
+
+        with tf.variable_scope('embedding_matrix_init'):
+            word_embeddings = tf.get_variable("word_embeddings_v",
+                                              initializer=tf.constant(self.emb_matrix, dtype=tf.float32),
+                                              trainable=self.trainable)
+        with tf.variable_scope('embedding_lookup'):
+            response_embeddings = tf.nn.embedding_lookup(word_embeddings, self.response_ph)
+
+        Hr = response_embeddings
+        if self.is_positional and self.stack_num > 0:
+            with tf.variable_scope('positional'):
+                Hr = op.positional_encoding_vector(Hr, max_timescale=10)
+
+        Hr_stack = [Hr]
+
+        for index in range(self.stack_num):
+            with tf.variable_scope('self_stack_' + str(index)):
+                Hr = layers.block(
+                    Hr, Hr, Hr,
+                    Q_lengths=self.response_len_ph, K_lengths=self.response_len_ph, attention_type='dot')
+                Hr_stack.append(Hr)
+
+        # context part
+        # a list of length max_turn_num, every element is a tensor with shape [batch, max_turn_len]
+        list_turn_t = tf.unstack(self.utterance_ph, axis=1)
+        list_turn_length = tf.unstack(self.all_utterance_len_ph, axis=1)
+
+        sim_turns = []
+        # for every turn_t calculate matching vector
+        for turn_t, t_turn_length in zip(list_turn_t, list_turn_length):
+            Hu = tf.nn.embedding_lookup(word_embeddings, turn_t)  # [batch, max_turn_len, emb_size]
+
+            if self.is_positional and self.stack_num > 0:
+                with tf.variable_scope('positional', reuse=True):
+                    Hu = op.positional_encoding_vector(Hu, max_timescale=10)
+            Hu_stack = [Hu]
+
+            for index in range(self.stack_num):
+                with tf.variable_scope('self_stack_' + str(index), reuse=True):
+                    Hu = layers.block(
+                        Hu, Hu, Hu,
+                        Q_lengths=t_turn_length, K_lengths=t_turn_length, attention_type='dot')
+
+                    Hu_stack.append(Hu)
+
+            r_a_t_stack = []
+            t_a_r_stack = []
+            for index in range(self.stack_num + 1):
+
+                with tf.variable_scope('t_attend_r_' + str(index)):
+                    try:
+                        t_a_r = layers.block(
+                            Hu_stack[index], Hr_stack[index], Hr_stack[index],
+                            Q_lengths=t_turn_length, K_lengths=self.response_len_ph, attention_type='dot')
+                    except ValueError:
+                        tf.get_variable_scope().reuse_variables()
+                        t_a_r = layers.block(
+                            Hu_stack[index], Hr_stack[index], Hr_stack[index],
+                            Q_lengths=t_turn_length, K_lengths=self.response_len_ph, attention_type='dot')
+
+                with tf.variable_scope('r_attend_t_' + str(index)):
+                    try:
+                        r_a_t = layers.block(
+                            Hr_stack[index], Hu_stack[index], Hu_stack[index],
+                            Q_lengths=self.response_len_ph, K_lengths=t_turn_length, attention_type='dot')
+                    except ValueError:
+                        tf.get_variable_scope().reuse_variables()
+                        r_a_t = layers.block(
+                            Hr_stack[index], Hu_stack[index], Hu_stack[index],
+                            Q_lengths=self.response_len_ph, K_lengths=t_turn_length, attention_type='dot')
+
+                t_a_r_stack.append(t_a_r)
+                r_a_t_stack.append(r_a_t)
+
+            t_a_r_stack.extend(Hu_stack)
+            r_a_t_stack.extend(Hr_stack)
+
+            t_a_r = tf.stack(t_a_r_stack, axis=-1)
+            r_a_t = tf.stack(r_a_t_stack, axis=-1)
+
+            # log.info(t_a_r, r_a_t)  # debug
+
+            # calculate similarity matrix
+            with tf.variable_scope('similarity'):
+                # sim shape [batch, max_turn_len, max_turn_len, 2*stack_num+1]
+                # divide sqrt(200) to prevent gradient explosion
+                sim = tf.einsum('biks,bjks->bijs', t_a_r, r_a_t) / tf.sqrt(float(self.word_embedding_size))
+
+            sim_turns.append(sim)
+
+        # cnn and aggregation
+        sim = tf.stack(sim_turns, axis=1)
+        log.info('sim shape: %s' % sim.shape)
+        with tf.variable_scope('cnn_aggregation'):
+            final_info = layers.CNN_3d(sim, 32, self.filters2_conv3d)
+            # for douban
+            # final_info = layers.CNN_3d(sim, 16, 16)
+
+        # loss and train
+        with tf.variable_scope('loss'):
+            self.loss, self.logits = layers.loss(final_info, self.y_true, clip_value=10.)
+            self.y_pred = tf.nn.softmax(self.logits, name="y_pred")
+            tf.summary.scalar('loss', self.loss)
+
+            self.global_step = tf.Variable(0, trainable=False)
+            initial_learning_rate = self.learning_rate
+            self.learning_rate = tf.train.exponential_decay(
+                initial_learning_rate,
+                global_step=self.global_step,
+                decay_steps=self.decay_steps,
+                decay_rate=0.9,
+                staircase=True)
+
+            Optimizer = tf.train.AdamOptimizer(self.learning_rate)
+            self.grads_and_vars = Optimizer.compute_gradients(self.loss)
+
+            for grad, var in self.grads_and_vars:
+                if grad is None:
+                    log.info(var)
+
+            self.capped_gvs = [(tf.clip_by_value(grad, -1., 1.), var) for grad, var in self.grads_and_vars]
+            self.train_op = Optimizer.apply_gradients(
+                self.capped_gvs,
+                global_step=self.global_step)
+
+        # Debug
+        self.print_number_of_parameters()
---
+++ deeppavlov/models/ranking/matching_models/dam_utils/operations.py
@@ -0,0 +1,395 @@
+# Copyright 2018 Neural Networks and Deep Learning lab, MIPT
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# @inproceedings{ ,
+#   title={Multi-Turn Response Selection for Chatbots with Deep Attention Matching Network},
+#   author={Xiangyang Zhou, Lu Li, Daxiang Dong, Yi Liu, Ying Chen, Wayne Xin Zhao, Dianhai Yu and Hua Wu},
+#   booktitle={Proceedings of the 56th Annual Meeting of the Association for Computational Linguistics (Volume 1: Long Papers)},
+#   volume={1},
+#   pages={  --  },
+#   year={2018}
+# }
+# ```
+# http://aclweb.org/anthology/P18-1103
+#
+# Based on authors' Tensorflow code: https://github.com/baidu/Dialogue/tree/master/DAM
+
+from logging import getLogger
+import math
+
+import numpy as np
+import tensorflow as tf
+
+from scipy.stats import multivariate_normal
+
+log = getLogger(__name__)
+
+
+def learning_rate(step_num, d_model=512, warmup_steps=4000):
+    a = step_num**(-0.5)
+    b = step_num*warmup_steps**(-1.5)
+    return a, b, d_model**(-0.5) * min(step_num**(-0.5), step_num*(warmup_steps**(-1.5))) 
+
+def selu(x):
+    alpha = 1.6732632423543772848170429916717
+    scale = 1.0507009873554804934193349852946
+    log.info('use selu')
+    return scale*tf.where(x>=0.0, x, alpha*tf.nn.elu(x))
+
+def bilinear_sim_4d(x, y, is_nor=True):
+    '''calulate bilinear similarity with two 4d tensor.
+    
+    Args:
+        x: a tensor with shape [batch, time_x, dimension_x, num_stacks]
+        y: a tensor with shape [batch, time_y, dimension_y, num_stacks]
+
+    Returns:
+        a tensor with shape [batch, time_x, time_y, num_stacks]
+
+    Raises:
+        ValueError: if
+            the shapes of x and y are not match;
+            bilinear matrix reuse error.
+    '''
+    M = tf.get_variable(
+        name="bilinear_matrix", 
+        shape=[x.shape[2], y.shape[2], x.shape[3]],
+        dtype=tf.float32,
+        initializer=tf.orthogonal_initializer())
+    sim = tf.einsum('biks,kls,bjls->bijs', x, M, y)
+
+    if is_nor:
+        scale = tf.sqrt(tf.cast(x.shape[2] * y.shape[2], tf.float32))
+        scale = tf.maximum(1.0, scale)
+        return sim / scale
+    else:
+        return sim
+
+
+def bilinear_sim(x, y, is_nor=True):
+    '''calculate bilinear similarity with two tensor.
+    Args:
+        x: a tensor with shape [batch, time_x, dimension_x]
+        y: a tensor with shape [batch, time_y, dimension_y]
+    
+    Returns:
+        a tensor with shape [batch, time_x, time_y]
+    Raises:
+        ValueError: if
+            the shapes of x and y are not match;
+            bilinear matrix reuse error.
+    '''
+    M = tf.get_variable(
+        name="bilinear_matrix", 
+        shape=[x.shape[-1], y.shape[-1]],
+        dtype=tf.float32,
+        # initializer=tf.orthogonal_initializer())
+        initializer=tf.keras.initializers.glorot_uniform(seed=42))
+    sim = tf.einsum('bik,kl,bjl->bij', x, M, y)
+
+    if is_nor:
+        scale = tf.sqrt(tf.cast(x.shape[-1] * y.shape[-1], tf.float32))
+        scale = tf.maximum(1.0, scale)
+        return sim / scale
+    else:
+        return sim
+
+def dot_sim(x, y, is_nor=True):
+    '''calculate dot similarity with two tensor.
+
+    Args:
+        x: a tensor with shape [batch, time_x, dimension]
+        y: a tensor with shape [batch, time_y, dimension]
+    
+    Returns:
+        a tensor with shape [batch, time_x, time_y]
+    Raises:
+        AssertionError: if
+            the shapes of x and y are not match.
+    '''
+    assert x.shape[-1] == y.shape[-1]
+
+    sim = tf.einsum('bik,bjk->bij', x, y)
+
+    if is_nor:
+        scale = tf.sqrt(tf.cast(x.shape[-1], tf.float32))
+        scale = tf.maximum(1.0, scale)
+        return sim / scale
+    else:
+        return sim
+
+def layer_norm(x, axis=None, epsilon=1e-6):
+    '''Add layer normalization.
+
+    Args:
+        x: a tensor
+        axis: the dimensions to normalize
+
+    Returns:
+        a tensor the same shape as x.
+
+    Raises:
+    '''
+    log.info('wrong version of layer_norm')
+    scale = tf.get_variable(
+        name='scale',
+        shape=[1],
+        dtype=tf.float32,
+        initializer=tf.ones_initializer())
+    bias = tf.get_variable(
+        name='bias',
+        shape=[1],
+        dtype=tf.float32,
+        initializer=tf.zeros_initializer())
+
+    if axis is None:
+        axis = [-1]
+
+    mean = tf.reduce_mean(x, axis=axis, keepdims=True)
+    variance = tf.reduce_mean(tf.square(x - mean), axis=axis, keepdims=True)
+    norm = (x-mean) * tf.rsqrt(variance + epsilon)
+    return scale * norm + bias
+
+def layer_norm_debug(x, axis = None, epsilon=1e-6):
+    '''Add layer normalization.
+
+    Args:
+        x: a tensor
+        axis: the dimensions to normalize
+
+    Returns:
+        a tensor the same shape as x.
+
+    Raises:
+    '''
+    if axis is None:
+        axis = [-1]
+    shape = [x.shape[i] for i in axis]
+
+    scale = tf.get_variable(
+        name='scale',
+        shape=shape,
+        dtype=tf.float32,
+        initializer=tf.ones_initializer())
+    bias = tf.get_variable(
+        name='bias',
+        shape=shape,
+        dtype=tf.float32,
+        initializer=tf.zeros_initializer())
+
+    mean = tf.reduce_mean(x, axis=axis, keepdims=True)
+    variance = tf.reduce_mean(tf.square(x - mean), axis=axis, keepdims=True)
+    norm = (x-mean) * tf.rsqrt(variance + epsilon)
+    return scale * norm + bias
+
+def dense(x, out_dimension=None, add_bias=True, initializer=tf.orthogonal_initializer()):
+    '''Add dense connected layer, Wx + b.
+
+    Args:
+        x: a tensor with shape [batch, time, dimension]
+        out_dimension: a number which is the output dimension
+
+    Return:
+        a tensor with shape [batch, time, out_dimension]
+
+    Raises:
+    '''
+    if out_dimension is None:
+        out_dimension = x.shape[-1]
+
+    W = tf.get_variable(
+        name='weights',
+        shape=[x.shape[-1], out_dimension],
+        dtype=tf.float32,
+        initializer=initializer)
+    if add_bias:
+        bias = tf.get_variable(
+            name='bias',
+            shape=[1],
+            dtype=tf.float32,
+            initializer=tf.zeros_initializer())
+        return tf.einsum('bik,kj->bij', x, W) + bias
+    else:
+        return tf.einsum('bik,kj->bij', x, W)
+
+def matmul_2d(x, out_dimension, drop_prob=None):
+    '''Multiplies 2-d tensor by weights.
+
+    Args:
+        x: a tensor with shape [batch, dimension]
+        out_dimension: a number
+
+    Returns:
+        a tensor with shape [batch, out_dimension]
+
+    Raises:
+    '''
+    W = tf.get_variable(
+        name='weights',
+        shape=[x.shape[1], out_dimension],
+        dtype=tf.float32,
+        initializer=tf.orthogonal_initializer())
+    if drop_prob is not None:
+        W = tf.nn.dropout(W, drop_prob)
+        log.info('W is dropout')
+
+    return tf.matmul(x, W)
+
+def gauss_positional_encoding_vector(x, role=0, value=0):
+    position = int(x.shape[1])
+    dimension = int(x.shape[2])
+    log.info('position: %s' %position)
+    log.info('dimension: %s' %dimension)
+
+    _lambda = tf.get_variable(
+        name='lambda',
+        shape=[position],
+        dtype=tf.float32,
+        initializer=tf.constant_initializer(value))
+    _lambda = tf.expand_dims(_lambda, axis=-1)
+
+    mean = [position/2.0, dimension/2.0]
+
+    #cov = [[position/3.0, 0], [0, dimension/3.0]]
+    sigma_x = position/math.sqrt(4.0*dimension)
+    sigma_y = math.sqrt(dimension/4.0)
+    cov = [[sigma_x*sigma_x, role*sigma_x*sigma_y], 
+            [role*sigma_x*sigma_y, sigma_y*sigma_y]]
+
+    pos = np.dstack(np.mgrid[0:position, 0:dimension])
+
+    
+    rv = multivariate_normal(mean, cov)
+    signal = rv.pdf(pos) 
+    signal = signal - np.max(signal)/2.0
+
+    signal = tf.multiply(_lambda, signal)
+    signal = tf.expand_dims(signal, axis=0)
+
+    log.info('gauss positional encoding')
+
+    return x + _lambda * signal
+
+def positional_encoding(x, min_timescale=1.0, max_timescale=1.0e4, value=0):
+    '''Adds a bunch of sinusoids of different frequencies to a tensor.
+
+    Args:
+        x: a tensor with shape [batch, length, channels]
+        min_timescale: a float
+        max_timescale: a float
+
+    Returns:
+        a tensor the same shape as x.
+
+    Raises:
+    '''
+    length = x.shape[1]
+    channels = x.shape[2]
+    _lambda = tf.get_variable(
+        name='lambda',
+        shape=[1],
+        dtype=tf.float32,
+        initializer=tf.constant_initializer(value))
+
+    position = tf.to_float(tf.range(length))
+    num_timescales = channels // 2
+    log_timescale_increment = (
+        math.log(float(max_timescale) / float(min_timescale)) /
+        (tf.to_float(num_timescales) - 1))
+    inv_timescales = min_timescale * tf.exp(
+        tf.to_float(tf.range(num_timescales)) * -log_timescale_increment)
+    scaled_time = tf.expand_dims(position, 1) * tf.expand_dims(inv_timescales, 0)
+    signal = tf.concat([tf.sin(scaled_time), tf.cos(scaled_time)], axis=1)
+    signal = tf.pad(signal, [[0, 0], [0, tf.mod(channels, 2)]])
+    #signal = tf.reshape(signal, [1, length, channels])
+    signal = tf.expand_dims(signal, axis=0)
+
+    return x + _lambda * signal
+
+
+def positional_encoding_vector(x, min_timescale=1.0, max_timescale=1.0e4, value=0):
+    '''Adds a bunch of sinusoids of different frequencies to a tensor.
+
+    Args:
+        x: a tensor with shape [batch, length, channels]
+        min_timescale: a float
+        max_timescale: a float
+
+    Returns:
+        a tensor the same shape as x.
+
+    Raises:
+    '''
+    length = x.shape[1]
+    channels = x.shape[2]
+    _lambda = tf.get_variable(
+        name='lambda',
+        shape=[length],
+        dtype=tf.float32,
+        initializer=tf.constant_initializer(value))
+    _lambda = tf.expand_dims(_lambda, axis=-1)
+
+    position = tf.to_float(tf.range(length))
+    num_timescales = channels // 2
+    log_timescale_increment = (
+        math.log(float(max_timescale) / float(min_timescale)) /
+        (tf.to_float(num_timescales) - 1))
+    inv_timescales = min_timescale * tf.exp(
+        tf.to_float(tf.range(num_timescales)) * -log_timescale_increment)
+    scaled_time = tf.expand_dims(position, 1) * tf.expand_dims(inv_timescales, 0)
+    signal = tf.concat([tf.sin(scaled_time), tf.cos(scaled_time)], axis=1)
+    signal = tf.pad(signal, [[0, 0], [0, tf.mod(channels, 2)]])
+
+    signal = tf.multiply(_lambda, signal)
+    signal = tf.expand_dims(signal, axis=0)
+
+    return x + signal
+
+def mask(row_lengths, col_lengths, max_row_length, max_col_length):
+    '''Return a mask tensor representing the first N positions of each row and each column.
+
+    Args:
+        row_lengths: a tensor with shape [batch]
+        col_lengths: a tensor with shape [batch]
+
+    Returns:
+        a mask tensor with shape [batch, max_row_length, max_col_length]
+
+    Raises:
+    '''
+    row_mask = tf.sequence_mask(row_lengths, max_row_length) #bool, [batch, max_row_len]
+    col_mask = tf.sequence_mask(col_lengths, max_col_length) #bool, [batch, max_col_len]
+
+    row_mask = tf.cast(tf.expand_dims(row_mask, -1), tf.float32)
+    col_mask = tf.cast(tf.expand_dims(col_mask, -1), tf.float32)
+
+    return tf.einsum('bik,bjk->bij', row_mask, col_mask)
+
+def weighted_sum(weight, values):
+    '''Calcualte the weighted sum.
+
+    Args:
+        weight: a tensor with shape [batch, time, dimension]
+        values: a tensor with shape [batch, dimension, values_dimension]
+
+    Return:
+        a tensor with shape [batch, time, values_dimension]
+
+    Raises:
+    '''
+    return tf.einsum('bij,bjk->bik', weight, values)
+
+
+        
+
