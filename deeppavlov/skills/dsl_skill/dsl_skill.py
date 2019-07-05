8304a62466fd60d314b588176e083f02323e6b25
Nitrogen <ilgizar1313@gmail.com>
2019-07-05 17:31:10 +0300

    Docstrings

--- deeppavlov/skills/dsl_skill/dsl_skill.py
+++ deeppavlov/skills/dsl_skill/dsl_skill.py
@@ -52,6 +52,11 @@ class DSLMeta(ABCMeta):
         DSLMeta.__add_to_collection(cls)
 
     def __init__class(cls, on_invalid_command: str = "Простите, я вас не понял", *args, **kwargs):
+        """
+        Initialize Skill class
+        Args:
+            on_invalid_command:  message to be sent on message with no associated handler
+        """
         # message to be sent on message with no associated handler
         cls.on_invalid_command = on_invalid_command
 
@@ -59,12 +64,34 @@ class DSLMeta(ABCMeta):
                        utterances_batch: List,
                        history_batch: List = None,
                        states_batch: List = None) -> Tuple[List, ...]:
+        """Returns skill inference result.
+        Returns batches of skill inference results, estimated confidence
+        levels and up to date states corresponding to incoming utterance
+        batch.
+        Args:
+            utterances_batch: A batch of utterances of str type.
+            history_batch: A batch of list typed histories for each utterance.
+            states_batch:  A batch of arbitrary typed states for
+                each utterance.
+        Returns:
+            response: A batch of arbitrary typed skill inference results.
+            confidence: A batch of float typed confidence levels for each of
+                skill inference result.
+            output_states_batch:  A batch of arbitrary typed states for
+                each utterance.
+
+        """
         history_batch = history_batch or []
         states_batch = states_batch or []
         return unzip(starmap(cls.handle, zip_longest(utterances_batch, history_batch, states_batch)))
 
     @staticmethod
     def __add_to_collection(cls: 'DSLMeta'):
+        """
+        Adds Skill class to Skill classes collection
+        Args:
+            cls: Skill class
+        """
         DSLMeta.skill_collection[cls.name] = cls
 
     @staticmethod
@@ -75,13 +102,15 @@ class DSLMeta(ABCMeta):
         """
         Handles what is going to be after a message from user arrived.
         Simple usage:
-        ExampleSkill.handle(<message>, <user_id>)
+        ExampleSkill.handle(Request(<message>, <user_id>))
 
-        :param cls: instance of callee's class
-        :param utterance: a message to be handled
-        :param history: history of dialog
-        :param state: state
-        :return: handler function's result if succeeded
+        Args:
+            cls: instance of callee's class
+            utterance: a message to be handled
+            history: history of dialog
+            state: state
+        Returns:
+            result: handler function's result if succeeded
         """
         current_handler = cls.__select_handler(utterance, history, state)
         return cls.__run_handler(current_handler, utterance, history, state)
@@ -91,7 +120,8 @@ class DSLMeta(ABCMeta):
                          state: str) -> Optional[Callable]:
         """
         Selects handler with the highest priority that could be triggered from the passed state and message.
-        :return: handler function that is selected and None if no handler fits request
+        Returns:
+             handler function that is selected and None if no handler fits request
         """
         available_handlers = cls.state_to_handler[state]
         available_handlers.extend(cls.universal_handlers)
@@ -106,8 +136,10 @@ class DSLMeta(ABCMeta):
                       state: str) -> ResponseType:
         """
         Runs specified handler for current message and context
-        :param handler: handler to be run. If None, on_invalid_command is returned
-        :return: ResponseType
+        Args:
+            handler: handler to be run. If None, on_invalid_command is returned
+        Returns:
+             ResponseType
         """
         if handler is None:
             return ResponseType(cls.on_invalid_command, 0.0, None)
@@ -129,15 +161,17 @@ class DSLMeta(ABCMeta):
             def __greeting(message: str):
                 ...
 
-        :param priority: integer value to indicate priority. If multiple handlers satisfy
+        Args:
+            priority: integer value to indicate priority. If multiple handlers satisfy
                           all the requirements, the handler with the greatest priority value will be used
-        :param context_condition: function that takes context and
+            context_condition: function that takes context and
                                   returns True if this handler should be enabled
                                   and False otherwise. If None, no condition is checked
-        :param commands: phrases/regexs on what the function wrapped
+            commands: phrases/regexs on what the function wrapped
                          by this decorator will trigger
-        :param state: state name
-        :return: function decorated into Handler class
+            state: state name
+        Returns:
+            function decorated into Handler class
         """
         if commands is None:
             commands = [".*"]
