abd12dc365d37db3886a5c3f2bf5eeaa273568fd
Dilyara Baymurzina <dilyara.rimovna@gmail.com>
2019-08-26 13:39:29 +0300

    feat: max_proba returns one value instead of a list of one value (#973)
    * chore: one class is not covered to list anymore
    
    * chore: one class is not covered to list anymore
    
    * fix: change sets_accuracy to accuracy for all one-class configs
    
    * fix: change sets_accuracy to accuracy for all one-class configs from tests
    
    * fix: typing for proba2labels

--- deeppavlov/models/classifiers/proba2labels.py
+++ deeppavlov/models/classifiers/proba2labels.py
@@ -54,7 +54,7 @@ class Proba2Labels(Component):
         self.top_n = top_n
 
     def __call__(self, data: Union[np.ndarray, List[List[float]], List[List[int]]],
-                 *args, **kwargs) -> Union[List[List[str]], List[str]]:
+                 *args, **kwargs) -> Union[List[List[int]], List[int]]:
         """
         Process probabilities to labels
 
@@ -70,7 +70,7 @@ class Proba2Labels(Component):
             return [list(np.where(np.array(d) > self.confident_threshold)[0])
                     for d in data]
         elif self.max_proba:
-            return [[np.argmax(d)] for d in data]
+            return [np.argmax(d) for d in data]
         elif self.top_n:
             return [np.argsort(d)[::-1][:self.top_n] for d in data]
         else:
