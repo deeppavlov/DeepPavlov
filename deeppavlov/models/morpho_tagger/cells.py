3d8d9dfec4a17dee0abb3079be1ffa77cefc7a31
Aleksei Lymar <yoptar@gmail.com>
2019-10-25 18:35:17 +0300

    refactor: use tf.keras instead of keras in morpho-tagger cells

--- deeppavlov/models/morpho_tagger/cells.py
+++ deeppavlov/models/morpho_tagger/cells.py
@@ -12,24 +12,24 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import keras.activations as kact
-import keras.backend as kb
-import keras.initializers as kinit
-import keras.layers as kl
 import numpy as np
-from keras.engine.topology import InputSpec
+import tensorflow.keras.backend as K
+from tensorflow.keras import activations
+from tensorflow.keras.initializers import Constant
+from tensorflow.keras.layers import InputSpec
+from tensorflow.keras.layers import Layer, Lambda, Dropout, Multiply
 
 INFTY = -100
 
 
-class Highway(kl.Layer):
+class Highway(Layer):
 
     def __init__(self, activation=None, bias_initializer=-1, **kwargs):
-        super(Highway, self).__init__(**kwargs)
-        self.activation = kact.get(activation)
+        super().__init__(**kwargs)
+        self.activation = activations.get(activation)
         self.bias_initializer = bias_initializer
         if isinstance(self.bias_initializer, int):
-            self.bias_initializer = kinit.constant(self.bias_initializer)
+            self.bias_initializer = Constant(self.bias_initializer)
         self.input_spec = [InputSpec(min_ndim=2)]
 
     def build(self, input_shape):
@@ -48,11 +48,11 @@ class Highway(kl.Layer):
         self.built = True
 
     def call(self, inputs, **kwargs):
-        gate = kb.dot(inputs, self.gate_kernel)
-        gate = kb.bias_add(gate, self.gate_bias, data_format="channels_last")
+        gate = K.dot(inputs, self.gate_kernel)
+        gate = K.bias_add(gate, self.gate_bias, data_format="channels_last")
         gate = self.activation(gate)
-        new_value = kb.dot(inputs, self.dense_kernel)
-        new_value = kb.bias_add(new_value, self.dense_bias, data_format="channels_last")
+        new_value = K.dot(inputs, self.dense_kernel)
+        new_value = K.bias_add(new_value, self.dense_bias, data_format="channels_last")
         return gate * new_value + (1.0 - gate) * inputs
 
     def compute_output_shape(self, input_shape):
@@ -61,13 +61,14 @@ class Highway(kl.Layer):
 
 def weighted_sum(first, second, sigma, first_threshold=-np.inf, second_threshold=np.inf):
     logit_probs = first * sigma + second * (1.0 - sigma)
-    infty_tensor = kb.ones_like(logit_probs) * INFTY
-    logit_probs = kb.switch(kb.greater(first, first_threshold), logit_probs, infty_tensor)
-    logit_probs = kb.switch(kb.greater(second, second_threshold), logit_probs, infty_tensor)
+    infty_tensor = K.ones_like(logit_probs) * INFTY
+    logit_probs = K.switch(K.greater(first, first_threshold), logit_probs, infty_tensor)
+    logit_probs = K.switch(K.greater(second, second_threshold), logit_probs, infty_tensor)
     return logit_probs
 
 
-class WeightedCombinationLayer(kl.Layer):
+class WeightedCombinationLayer(Layer):
+
     """
     A class for weighted combination of probability distributions
     """
@@ -85,7 +86,7 @@ class WeightedCombinationLayer(kl.Layer):
         self.use_dimension_bias = use_dimension_bias
         self.use_intermediate_layer = use_intermediate_layer
         self.intermediate_dim = intermediate_dim
-        self.intermediate_activation = kact.get(intermediate_activation)
+        self.intermediate_activation = activations.get(intermediate_activation)
         self.from_logits = from_logits
         self.return_logits = return_logits
         self.bias_initializer = bias_initializer
@@ -107,7 +108,7 @@ class WeightedCombinationLayer(kl.Layer):
         self.features_kernel = self.add_weight(
             shape=(features_dim, 1), initializer="random_uniform", name='kernel')
         self.features_bias = self.add_weight(
-            shape=(1,), initializer=kinit.Constant(self.bias_initializer), name='bias')
+            shape=(1,), initializer=Constant(self.bias_initializer), name='bias')
         if self.use_dimension_bias:
             self.dimensions_bias = self.add_weight(
                 shape=(input_dim,), initializer="random_uniform", name='dimension_bias')
@@ -117,29 +118,29 @@ class WeightedCombinationLayer(kl.Layer):
         assert isinstance(inputs, list) and len(inputs) == 3
         first, second, features = inputs[0], inputs[1], inputs[2]
         if not self.from_logits:
-            first = kb.clip(first, 1e-10, 1.0)
-            second = kb.clip(second, 1e-10, 1.0)
-            first_, second_ = kb.log(first), kb.log(second)
+            first = K.clip(first, 1e-10, 1.0)
+            second = K.clip(second, 1e-10, 1.0)
+            first_, second_ = K.log(first), K.log(second)
         else:
             first_, second_ = first, second
         # embedded_features.shape = (M, T, 1)
         if self.use_intermediate_layer:
-            features = kb.dot(features, self.first_kernel)
-            features = kb.bias_add(features, self.first_bias, data_format="channels_last")
+            features = K.dot(features, self.first_kernel)
+            features = K.bias_add(features, self.first_bias, data_format="channels_last")
             features = self.intermediate_activation(features)
-        embedded_features = kb.dot(features, self.features_kernel)
-        embedded_features = kb.bias_add(
+        embedded_features = K.dot(features, self.features_kernel)
+        embedded_features = K.bias_add(
             embedded_features, self.features_bias, data_format="channels_last")
         if self.use_dimension_bias:
-            tiling_shape = [1] * (kb.ndim(first) - 1) + [kb.shape(first)[-1]]
-            embedded_features = kb.tile(embedded_features, tiling_shape)
-            embedded_features = kb.bias_add(
+            tiling_shape = [1] * (K.ndim(first) - 1) + [K.shape(first)[-1]]
+            embedded_features = K.tile(embedded_features, tiling_shape)
+            embedded_features = K.bias_add(
                 embedded_features, self.dimensions_bias, data_format="channels_last")
-        sigma = kb.sigmoid(embedded_features)
+        sigma = K.sigmoid(embedded_features)
 
         result = weighted_sum(first_, second_, sigma,
                               self.first_threshold, self.second_threshold)
-        probs = kb.softmax(result)
+        probs = K.softmax(result)
         if self.return_logits:
             return [probs, result]
         return probs
@@ -158,13 +159,13 @@ def TemporalDropout(inputs, dropout=0.0):
     # TO DO: adapt for >3D tensors
     if dropout == 0.0:
         return inputs
-    inputs_func = lambda x: kb.ones_like(inputs[:, :, 0:1])
-    inputs_mask = kl.Lambda(inputs_func)(inputs)
-    inputs_mask = kl.Dropout(dropout)(inputs_mask)
-    tiling_shape = [1, 1, kb.shape(inputs)[2]] + [1] * (kb.ndim(inputs) - 3)
-    inputs_mask = kl.Lambda(kb.tile, arguments={"n": tiling_shape},
-                            output_shape=inputs._keras_shape[1:])(inputs_mask)
-    answer = kl.Multiply()([inputs, inputs_mask])
+    inputs_func = lambda x: K.ones_like(inputs[:, :, 0:1])
+    inputs_mask = Lambda(inputs_func)(inputs)
+    inputs_mask = Dropout(dropout)(inputs_mask)
+    tiling_shape = [1, 1, K.shape(inputs)[2]] + [1] * (K.ndim(inputs) - 3)
+    inputs_mask = Lambda(K.tile, arguments={"n": tiling_shape},
+                         output_shape=inputs._keras_shape[1:])(inputs_mask)
+    answer = Multiply()([inputs, inputs_mask])
     return answer
 
 
@@ -174,6 +175,6 @@ def positions_func(inputs, pad=0):
     1+ln(1+i) when it contains a meaningful symbol
     and with 0 when it contains PAD
     """
-    position_inputs = kb.cumsum(kb.ones_like(inputs, dtype="float32"), axis=1)
-    position_inputs *= kb.cast(kb.not_equal(inputs, pad), "float32")
-    return kb.log(1.0 + position_inputs)
+    position_inputs = K.cumsum(K.ones_like(inputs, dtype="float32"), axis=1)
+    position_inputs *= K.cast(K.not_equal(inputs, pad), "float32")
+    return K.log(1.0 + position_inputs)
