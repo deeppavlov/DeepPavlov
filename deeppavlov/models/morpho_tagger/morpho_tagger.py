aac31a834c0e5224d606540adc0dc61a3032e6c4
Aleksei Lymar <yoptar@gmail.com>
2019-10-25 18:46:54 +0300

    refactor: use tf.keras instead of keras in morpho-tagger

--- deeppavlov/models/morpho_tagger/morpho_tagger.py
+++ deeppavlov/models/morpho_tagger/morpho_tagger.py
@@ -16,12 +16,13 @@ from logging import getLogger
 from pathlib import Path
 from typing import List, Optional, Union, Tuple
 
-import keras.backend as kb
-import keras.layers as kl
-import keras.optimizers as ko
-import keras.regularizers as kreg
 import numpy as np
-from keras import Model
+import tensorflow.keras.backend as K
+from tensorflow.keras import Model
+from tensorflow.keras.layers import (Input, Dense, Lambda, Concatenate, Conv2D, Dropout, LSTM, Bidirectional,
+                                     TimeDistributed)
+from tensorflow.keras.optimizers import Nadam
+from tensorflow.keras.regularizers import l2
 
 from deeppavlov.core.common.registry import register
 from deeppavlov.core.data.simple_vocab import SimpleVocabulary
@@ -75,7 +76,6 @@ class MorphoTagger(KerasModel):
 
     A subclass of :class:`~deeppavlov.core.models.keras_model.KerasModel`
     """
-
     def __init__(self,
                  symbols: SimpleVocabulary,
                  tags: SimpleVocabulary,
@@ -100,7 +100,7 @@ class MorphoTagger(KerasModel):
                  regularizer: float = None,
                  verbose: int = 1, **kwargs):
         # Calls parent constructor. Results in creation of save_folder if it doesn't exist
-        super().__init__(save_path=save_path, load_path=load_path, mode=mode)
+        super().__init__(save_path=save_path, load_path=load_path, mode=mode, **kwargs)
         self.symbols = symbols
         self.tags = tags
         self.word_rnn = word_rnn
@@ -121,6 +121,7 @@ class MorphoTagger(KerasModel):
         self.regularizer = regularizer
         self.verbose = verbose
         self._initialize()
+        self.model_ = None
         self.build()
 
         # Tries to load the model from model `load_path`, if it is available
@@ -160,25 +161,25 @@ class MorphoTagger(KerasModel):
         if self.word_vectorizers is None:
             self.word_vectorizers = []
         if self.regularizer is not None:
-            self.regularizer = kreg.l2(self.regularizer)
+            self.regularizer = l2(self.regularizer)
         if self.verbose > 0:
             log.info("{} symbols, {} tags in CharacterTagger".format(len(self.symbols), len(self.tags)))
 
     def build(self):
         """Builds the network using Keras.
         """
-        word_inputs = kl.Input(shape=(None, MAX_WORD_LENGTH + 2), dtype="int32")
+        word_inputs = Input(shape=(None, MAX_WORD_LENGTH+2), dtype="int32")
         inputs = [word_inputs]
         word_outputs = self._build_word_cnn(word_inputs)
         if len(self.word_vectorizers) > 0:
-            additional_word_inputs = [kl.Input(shape=(None, input_dim), dtype="float32")
+            additional_word_inputs = [Input(shape=(None, input_dim), dtype="float32")
                                       for input_dim, dense_dim in self.word_vectorizers]
             inputs.extend(additional_word_inputs)
-            additional_word_embeddings = [kl.Dense(dense_dim)(additional_word_inputs[i])
+            additional_word_embeddings = [Dense(dense_dim)(additional_word_inputs[i])
                                           for i, (_, dense_dim) in enumerate(self.word_vectorizers)]
-            word_outputs = kl.Concatenate()([word_outputs] + additional_word_embeddings)
+            word_outputs = Concatenate()([word_outputs] + additional_word_embeddings)
         outputs, lstm_outputs = self._build_basic_network(word_outputs)
-        compile_args = {"optimizer": ko.nadam(lr=0.002, clipnorm=5.0),
+        compile_args = {"optimizer": Nadam(lr=0.002, clipnorm=5.0),
                         "loss": "categorical_crossentropy", "metrics": ["accuracy"]}
         self.model_ = Model(inputs, outputs)
         self.model_.compile(**compile_args)
@@ -189,9 +190,9 @@ class MorphoTagger(KerasModel):
     def _build_word_cnn(self, inputs):
         """Builds word-level network
         """
-        inputs = kl.Lambda(kb.one_hot, arguments={"num_classes": len(self.symbols)},
-                           output_shape=lambda x: tuple(x) + (len(self.symbols),))(inputs)
-        char_embeddings = kl.Dense(self.char_embeddings_size, use_bias=False)(inputs)
+        inputs = Lambda(K.one_hot, arguments={"num_classes": len(self.symbols)},
+                        output_shape=lambda x: tuple(x) + (len(self.symbols),))(inputs)
+        char_embeddings = Dense(self.char_embeddings_size, use_bias=False)(inputs)
         conv_outputs = []
         self.char_output_dim_ = 0
         for window_size, filters_number in zip(self.char_window_size, self.char_filters):
@@ -199,27 +200,27 @@ class MorphoTagger(KerasModel):
             curr_filters_number = (min(self.char_filter_multiple * window_size, 200)
                                    if filters_number is None else filters_number)
             for _ in range(self.char_conv_layers - 1):
-                curr_output = kl.Conv2D(curr_filters_number, (1, window_size),
-                                        padding="same", activation="relu",
-                                        data_format="channels_last")(curr_output)
+                curr_output = Conv2D(curr_filters_number, (1, window_size),
+                                     padding="same", activation="relu",
+                                     data_format="channels_last")(curr_output)
                 if self.conv_dropout > 0.0:
-                    curr_output = kl.Dropout(self.conv_dropout)(curr_output)
-            curr_output = kl.Conv2D(curr_filters_number, (1, window_size),
-                                    padding="same", activation="relu",
-                                    data_format="channels_last")(curr_output)
+                    curr_output = Dropout(self.conv_dropout)(curr_output)
+            curr_output = Conv2D(curr_filters_number, (1, window_size),
+                                 padding="same", activation="relu",
+                                 data_format="channels_last")(curr_output)
             conv_outputs.append(curr_output)
             self.char_output_dim_ += curr_filters_number
         if len(conv_outputs) > 1:
-            conv_output = kl.Concatenate(axis=-1)(conv_outputs)
+            conv_output = Concatenate(axis=-1)(conv_outputs)
         else:
             conv_output = conv_outputs[0]
-        highway_input = kl.Lambda(kb.max, arguments={"axis": -2})(conv_output)
+        highway_input = Lambda(K.max, arguments={"axis": -2})(conv_output)
         if self.intermediate_dropout > 0.0:
-            highway_input = kl.Dropout(self.intermediate_dropout)(highway_input)
+            highway_input = Dropout(self.intermediate_dropout)(highway_input)
         for i in range(self.char_highway_layers - 1):
             highway_input = Highway(activation="relu")(highway_input)
             if self.highway_dropout > 0.0:
-                highway_input = kl.Dropout(self.highway_dropout)(highway_input)
+                highway_input = Dropout(self.highway_dropout)(highway_input)
         highway_output = Highway(activation="relu")(highway_input)
         return highway_output
 
@@ -229,22 +230,23 @@ class MorphoTagger(KerasModel):
         transforming word embeddings to intermediate outputs
         """
         if self.word_dropout > 0.0:
-            lstm_outputs = kl.Dropout(self.word_dropout)(word_outputs)
+            lstm_outputs = Dropout(self.word_dropout)(word_outputs)
         else:
             lstm_outputs = word_outputs
-        for j in range(self.word_lstm_layers - 1):
-            lstm_outputs = kl.Bidirectional(
-                kl.LSTM(self.word_lstm_units[j], return_sequences=True,
-                        dropout=self.lstm_dropout))(lstm_outputs)
-        lstm_outputs = kl.Bidirectional(
-            kl.LSTM(self.word_lstm_units[-1], return_sequences=True,
-                    dropout=self.lstm_dropout))(lstm_outputs)
-        pre_outputs = kl.TimeDistributed(
-            kl.Dense(len(self.tags), activation="softmax",
-                     activity_regularizer=self.regularizer),
-            name="p")(lstm_outputs)
+        for j in range(self.word_lstm_layers-1):
+            lstm_outputs = Bidirectional(
+                LSTM(self.word_lstm_units[j], return_sequences=True,
+                     dropout=self.lstm_dropout))(lstm_outputs)
+        lstm_outputs = Bidirectional(
+                LSTM(self.word_lstm_units[-1], return_sequences=True,
+                     dropout=self.lstm_dropout))(lstm_outputs)
+        pre_outputs = TimeDistributed(
+                Dense(len(self.tags), activation="softmax",
+                      activity_regularizer=self.regularizer),
+                name="p")(lstm_outputs)
         return pre_outputs, lstm_outputs
 
+    # noinspection PyPep8Naming
     def _transform_batch(self, data, labels=None, transform_to_one_hot=True):
         data, additional_data = data[0], data[1:]
         L = max(len(x) for x in data)
@@ -271,9 +273,11 @@ class MorphoTagger(KerasModel):
         #   data: a batch of word sequences
         #   labels: a batch of correct tag sequences
         *data, labels = args
+        # noinspection PyPep8Naming
         X, Y = self._transform_batch(data, labels)
         self.model_.train_on_batch(X, Y)
 
+    # noinspection PyPep8Naming
     def predict_on_batch(self, data: Union[List[np.ndarray], Tuple[np.ndarray]],
                          return_indexes: bool = False) -> List[List[str]]:
         """
@@ -292,13 +296,13 @@ class MorphoTagger(KerasModel):
         objects_number, lengths = len(X[0]), [len(elem) for elem in data[0]]
         Y = self.model_.predict_on_batch(X)
         labels = np.argmax(Y, axis=-1)
-        answer: List[List[str]] = [None] * objects_number
+        answer: List[Optional[List[str]]] = [None] * objects_number
         for i, (elem, length) in enumerate(zip(labels, lengths)):
             elem = elem[:length]
             answer[i] = elem if return_indexes else self.tags.idxs2toks(elem)
         return answer
 
-    def __call__(self, *x_batch, **kwargs) -> Union[List, np.ndarray]:
+    def __call__(self, *x_batch: np.ndarray, **kwargs) -> Union[List, np.ndarray]:
         """
         Predicts answers on batch elements.
 
@@ -322,14 +326,14 @@ class MorphoTagger(KerasModel):
             in j-th word of i-th input sentence.
         """
         bucket_length = bucket_length or len(sent)
-        answer = np.zeros(shape=(bucket_length, MAX_WORD_LENGTH + 2), dtype=np.int32)
+        answer = np.zeros(shape=(bucket_length, MAX_WORD_LENGTH+2), dtype=np.int32)
         for i, word in enumerate(sent):
             answer[i, 0] = self.tags["BEGIN"]
             m = min(len(word), MAX_WORD_LENGTH)
             for j, x in enumerate(word[-m:]):
-                answer[i, j + 1] = self.symbols[x]
-            answer[i, m + 1] = self.tags["END"]
-            answer[i, m + 2:] = self.tags["PAD"]
+                answer[i, j+1] = self.symbols[x]
+            answer[i, m+1] = self.tags["END"]
+            answer[i, m+2:] = self.tags["PAD"]
         return answer
 
     def _make_tags_vector(self, tags, bucket_length=None) -> np.ndarray:
