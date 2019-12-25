a64fbafce5763d4b3ca1eee20a01ff6d66fea973
evg3307 <evg3307@yandex.ru>
2019-12-25 14:31:10 +0300

    Replace os.path with Path

--- deeppavlov/models/go_bot/launch_train_bot.py
+++ deeppavlov/models/go_bot/launch_train_bot.py
@@ -1,8 +1,9 @@
 import argparse
 import json
 import logging
-import os
 from logging import getLogger
+from pathlib import Path
+from typing import Dict, Any
 
 from deeppavlov import configs, evaluate_model, train_model
 from deeppavlov.core.common.file import read_json
@@ -14,16 +15,22 @@ from deeppavlov.download import download_decompress
 log = getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(levelname)s] [%(asctime)s] %(message)s",
+    datefmt="%y-%m-%d %H:%M:%S"
+)
+
 formatter = logging.Formatter('[%(levelname)s] [%(asctime)s] %(message)s',"%y-%m-%d %H:%M:%S", "%")
 fh = logging.FileHandler("log")
 fh.setFormatter(formatter)
 log.addHandler(fh)
 
 
-def load_database(bot_data_dir, data):
+def load_database(bot_data_dir: Path, data: Dict[str, Any]):
     database = Sqlite3Database(
         primary_keys=["name"],
-        save_path=os.path.join(bot_data_dir, "db.sqlite")
+        save_path=str(bot_data_dir / "db.sqlite")
     )
 
     iterator = DialogDatasetIterator(data)
@@ -38,21 +45,16 @@ def load_database(bot_data_dir, data):
         database.fit(db_results)
 
 
-def load_bot_data(bot_data_dir, max_train_data):
-    data = SimpleDSTC2DatasetReader().read(bot_data_dir)
+def load_bot_data(bot_data_dir: Path, max_train_data: int = None):
+    data = SimpleDSTC2DatasetReader().read(str(bot_data_dir))
 
     if max_train_data is not None:
-        src = os.path.join(bot_data_dir, 'simple-dstc2-trn.json')
-        dst = os.path.join(bot_data_dir, 'simple-dstc2-trn_full.json')
+        src = bot_data_dir / 'simple-dstc2-trn.json'
+        dst = bot_data_dir / 'simple-dstc2-trn_full.json'
 
-        with open(src, 'rt') as fin:
-            old_data = json.load(fin)
-
-        with open(dst, 'wt') as fout:
-            json.dump(old_data, fout, indent=2)
-
-        with open(src, 'wt') as fout:
-            json.dump(old_data[:max_train_data], fout, indent=2)
+        old_data = json.loads(src.read_text(encoding='utf-8'))
+        dst.write_text(json.dumps(old_data, indent=2), encoding='utf-8')
+        src.write_text(json.dumps(old_data[:max_train_data], indent=2), encoding='utf-8')
 
         log.info(f"Train set is reduced to {max_train_data} dialogues (out of {len(data)}).")
 
@@ -65,25 +67,21 @@ def load_bot_data(bot_data_dir, max_train_data):
     load_database(bot_data_dir, data)
 
 
-def create_bot_configs(bot_data_dir, bot_model_dir, load_data=False, max_train_data=None):
-    if not os.path.exists(bot_data_dir) or load_data:
+def create_bot_configs(bot_data_dir: Path, bot_model_dir: Path, load_data: bool = False, max_train_data: int = None):
+    if not bot_data_dir.exists() or load_data:
         load_bot_data(bot_data_dir, max_train_data)
 
     slotfill_config = read_json(configs.ner.slotfill_simple_dstc2_raw)
-    slotfill_config['metadata']['variables']['DATA_PATH'] = bot_data_dir
+    slotfill_config['metadata']['variables']['DATA_PATH'] = str(bot_data_dir)
 
     slot_vals_file = 'dstc_slot_vals.json'
-    slotfill_config['metadata']['variables']['SLOT_VALS_PATH'] = os.path.join(
-        bot_data_dir, slot_vals_file
-    )
+    slotfill_config['metadata']['variables']['SLOT_VALS_PATH'] = str(bot_data_dir / slot_vals_file)
 
-    if not os.path.exists(bot_model_dir):
-        os.makedirs(bot_model_dir)
+    bot_model_dir.mkdir(parents=True, exist_ok=True)
 
-    path_to_slotfill_config = os.path.join(bot_model_dir, 'slotfill_config.json')
-    with open(path_to_slotfill_config, 'w') as f:
-        json.dump(slotfill_config, f, indent=2)
-        log.info(f'slot_fill config is saved to {path_to_slotfill_config}')
+    path_to_slotfill_config = bot_model_dir / 'slotfill_config.json'
+    path_to_slotfill_config.write_text(json.dumps(slotfill_config, indent=2), encoding='utf-8')
+    log.info(f'slot_fill config is saved to {path_to_slotfill_config}')
 
     gobot_config = read_json(configs.go_bot.gobot_simple_dstc2)
     gobot_config['chainer']['pipe'][-1]['embedder'] = None
@@ -92,20 +90,17 @@ def create_bot_configs(bot_data_dir, bot_model_dir, load_data=False, max_train_d
     gobot_config['chainer']['pipe'][-1]['database'] = {
         'class_name': 'sqlite_database',
         'primary_keys': ['name'],
-        'save_path': os.path.join(bot_data_dir, db_path)
+        'save_path': str(bot_data_dir / db_path)
     }
 
-    gobot_config['chainer']['pipe'][-1]['slot_filler']['config_path'] = path_to_slotfill_config
-
-    slot_names_dstc2 = ['pricerange', 'this', 'area', 'food']
-
-    gobot_config['chainer']['pipe'][-1]['tracker']['slot_names'] = slot_names_dstc2
+    gobot_config['chainer']['pipe'][-1]['slot_filler']['config_path'] = str(path_to_slotfill_config)
+    gobot_config['chainer']['pipe'][-1]['tracker']['slot_names'] = ['pricerange', 'this', 'area', 'food']
 
     gobot_config['chainer']['pipe'][-1]['template_type'] = 'DefaultTemplate'
-    gobot_config['chainer']['pipe'][-1]['template_path'] = os.path.join(bot_data_dir, 'simple-dstc2-templates.txt')
+    gobot_config['chainer']['pipe'][-1]['template_path'] = str(bot_data_dir / 'simple-dstc2-templates.txt')
 
-    gobot_config['metadata']['variables']['DATA_PATH'] = bot_data_dir
-    gobot_config['metadata']['variables']['MODEL_PATH'] = bot_model_dir
+    gobot_config['metadata']['variables']['DATA_PATH'] = str(bot_data_dir)
+    gobot_config['metadata']['variables']['MODEL_PATH'] = str(bot_model_dir)
 
     gobot_config['train']['epochs'] = 200
     gobot_config['train']['batch_size'] = 8
@@ -114,10 +109,9 @@ def create_bot_configs(bot_data_dir, bot_model_dir, load_data=False, max_train_d
     gobot_config['train']['val_every_n_batches'] = 40
     gobot_config['train']['log_every_n_batches'] = 40
 
-    path_to_gobot_config = os.path.join(bot_model_dir, 'gobot_config.json')
-    with open(path_to_gobot_config, 'w') as f:
-        json.dump(gobot_config, f, indent=2)
-        log.info(f'gobot config is saved to {path_to_gobot_config}')
+    path_to_gobot_config = bot_model_dir / 'gobot_config.json'
+    path_to_gobot_config.write_text(json.dumps(gobot_config, indent=2), encoding='utf-8')
+    log.info(f'gobot config is saved to {path_to_gobot_config}')
 
     return gobot_config
 
@@ -126,13 +120,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     # Required parameters
-    parser.add_argument("--bot_data_dir", default=None, type=str, required=True,
+    parser.add_argument("--bot_data_dir", default=None, type=Path, required=True,
                         help="Relative path from working directory to a directory where go-bot data will be saved")
-    parser.add_argument("--bot_model_dir", default=None, type=str, required=True,
+    parser.add_argument("--bot_model_dir", default=None, type=Path, required=True,
                         help="Relative path from working directory to a directory where go-bot model will be saved")
 
     # Other parameters
-    parser.add_argument("--working_directory", default="", type=str, required=False,
+    parser.add_argument("--working_directory", default="", type=Path, required=False,
                         help="Directory from which relative paths are built")
     parser.add_argument("--load_data", action='store_true',
                         help="Whether to load data")
@@ -145,8 +139,8 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    bot_data_dir = os.path.join(args.working_directory, args.bot_data_dir)
-    bot_model_dir = os.path.join(args.working_directory, args.bot_model_dir)
+    bot_data_dir = args.working_directory / args.bot_data_dir
+    bot_model_dir = args.working_directory / args.bot_model_dir
 
     gobot_config = create_bot_configs(bot_data_dir, bot_model_dir, args.load_data, args.max_train_data)
 
