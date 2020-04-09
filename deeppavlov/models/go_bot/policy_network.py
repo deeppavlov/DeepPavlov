038f57f382fe37a12f6a6e600f514df9f7e35e83
oserikov <srkvoa@gmail.com>
2020-04-09 16:56:53 +0300

    wip go_bot refactoring. PolicyNetwok logging and minimal init() refactoring

--- deeppavlov/models/go_bot/policy_network.py
+++ deeppavlov/models/go_bot/policy_network.py
@@ -98,17 +98,23 @@ class PolicyNetwork(LRScheduledTFModel):
     the Policy Network is a ML model whose goal is to choose the right system response when in dialogue with user.
     """
 
-    GRAPH_PARAMS = ["hidden_size", "action_size", "dense_size", "attention_mechanism"]
+    GRAPH_PARAMS = ["hidden_size", "action_size", "dense_size", "attention_params"]
     SERIALIZABLE_FIELDS = ["hidden_size", "action_size", "dense_size", "dropout_rate", "l2_reg_coef",
-                           "attention_mechanism"]
+                           "attention_params"]
 
     def __init__(self, network_params_passed: PolicyNetworkParams,
                  tokens_dims: TokensVectorRepresentationParams,
                  features_params: SharedGoBotParams,
                  load_path,
                  save_path,
+                 debug=False,
                  **kwargs):
-
+        self.debug = debug
+        if self.debug:
+            log.debug(f"BEFORE {self.__class__.__name__} init(): "
+                      f"network_params_passed={network_params_passed}, tokens_dims={tokens_dims}, "
+                      f"features_params={features_params}, load_path={load_path}, save_path={save_path}, "
+                      f"debug={debug}, kwargs={kwargs}")
         if network_params_passed.get_learning_rate():
             kwargs['learning_rate'] = network_params_passed.get_learning_rate()  # todo :(
 
@@ -120,29 +126,46 @@ class PolicyNetwork(LRScheduledTFModel):
         self.l2_reg_coef = network_params_passed.get_l2_reg_coef()
         self.dense_size = network_params_passed.get_dense_size()
 
-        self.input_size = self.calc_input_size(tokens_dims, features_params)
-
         attn_params_passed = network_params_passed.get_attn_params()
-        if attn_params_passed:
-            self.attention_mechanism = self.configure_attn(attn_params_passed, tokens_dims, features_params)
-            self.input_size -= self.attention_mechanism.token_size
-        else:
-            self.attention_mechanism = None
+        self.attention_params = self.configure_attn(attn_params_passed, tokens_dims, features_params)  # todo :(
+        self.input_size = self.calc_input_size(tokens_dims, features_params, self.attention_params)  # todo :(
+
+        if self.debug:
+            log.debug(f"INSIDE {self.__class__.__name__} init(). calculated NN hyperparams: "
+                      f"attention_params={self.attention_params}, "
+                      f"hidden_size={self.hidden_size}, action_size={self.action_size}, "
+                      f"dropout_rate={self.dropout_rate}, l2_reg_coef={self.l2_reg_coef}, "
+                      f"dense_size={self.dense_size}, input_size={self.input_size}")
 
         self._build_graph()
+        if self.debug:
+            log.debug(f"INSIDE {self.__class__.__name__} init(). build graph done.")
         self.sess = tf.Session()
         self.sess.run(tf.global_variables_initializer())
+        if self.debug:
+            log.debug(f"INSIDE {self.__class__.__name__} init(). "
+                      f"Session() initialization and global_variables_initializer() done.")
 
         if self.train_checkpoint_exists():
+            log.info(f"INSIDE {self.__class__.__name__} init(). Initializing {self.__class__.__name__} from checkpoint.")
             self.load()
         else:
-            log.info(f"[initializing `{self.__class__.__name__}` from scratch]")
+            log.info(f"INSIDE {self.__class__.__name__} init(). Initializing {self.__class__.__name__} from scratch.")
+
+        if self.debug:
+            log.debug(f"AFTER {self.__class__.__name__} init(): "
+                      f"network_params_passed={network_params_passed}, tokens_dims={tokens_dims}, "
+                      f"features_params={features_params}, load_path={load_path}, save_path={save_path}, "
+                      f"debug={debug}, kwargs={kwargs}")
 
     @staticmethod
-    def calc_input_size(tokens_dims: TokensVectorRepresentationParams, shared_go_bot_params: SharedGoBotParams) -> int:
+    def calc_input_size(tokens_dims: TokensVectorRepresentationParams,
+                        shared_go_bot_params: SharedGoBotParams,
+                        attention_params: Optional[GobotAttnParams]) -> int:
         """
         :param tokens_dims: the tokens vectors dimensions
         :param shared_go_bot_params: GO-bot hyperparams used in various parts of the pipeline
+        :param attention_params: the params of attention mechanism of the network for which input size is calculated
         :return: the calculated input shape of policy network
         """
         input_size = 6 + shared_go_bot_params.num_tracker_features + shared_go_bot_params.num_actions  # todo: why 6
@@ -152,12 +175,20 @@ class PolicyNetwork(LRScheduledTFModel):
             input_size += tokens_dims.embedding_dim
         if shared_go_bot_params.num_intents:
             input_size += shared_go_bot_params.num_intents
+        if attention_params is not None:
+            input_size -= attention_params.token_size
+
         return input_size
 
     @staticmethod
-    def configure_attn(attn,
+    def configure_attn(attn: dict,
                        tokens_dims: TokensVectorRepresentationParams,
                        features_params: SharedGoBotParams):
+        # todo store params in proper class objects not in dicts
+
+        if not attn:
+            return None
+
         curr_attn_token_size = attn.get('token_size')
         curr_attn_action_as_key = attn.get('action_as_key')
         curr_attn_intent_as_key = attn.get('intent_as_key')
@@ -206,14 +237,14 @@ class PolicyNetwork(LRScheduledTFModel):
         :param tracker_prev_action: one-hot-encoded previous executed action
         :return: vector representing an attention key
         """
-        # todo dto-like class for the attended features?
+        # todo dto-like class for the attn features?
 
         attn_key = np.array([], dtype=np.float32)
 
-        if self.attention_mechanism:
-            if self.attention_mechanism.action_as_key:
+        if self.attention_params:
+            if self.attention_params.action_as_key:
                 attn_key = np.hstack((attn_key, tracker_prev_action))
-            if self.attention_mechanism.intent_as_key:
+            if self.attention_params.intent_as_key:
                 attn_key = np.hstack((attn_key, intent_features))
             if len(attn_key) == 0:
                 attn_key = np.array([1], dtype=np.float32)
@@ -262,18 +293,18 @@ class PolicyNetwork(LRScheduledTFModel):
         _initial_state_h = tf.placeholder_with_default(zero_state, shape=[None, self.hidden_size])
         self._initial_state = tf.nn.rnn_cell.LSTMStateTuple(_initial_state_c, _initial_state_h)
 
-        if self.attention_mechanism:
-            _emb_context_shape = [None, None, self.attention_mechanism.max_num_tokens,
-                                  self.attention_mechanism.token_size]
+        if self.attention_params:
+            _emb_context_shape = [None, None, self.attention_params.max_num_tokens,
+                                  self.attention_params.token_size]
             self._emb_context = tf.placeholder(tf.float32, _emb_context_shape, name='emb_context')
-            self._key = tf.placeholder(tf.float32, [None, None, self.attention_mechanism.key_size], name='key')
+            self._key = tf.placeholder(tf.float32, [None, None, self.attention_params.key_size], name='key')
 
     def _build_body(self) -> Tuple[tf.Tensor, tf.Tensor]:
         # input projection
         _units = tf.layers.dense(self._features, self.dense_size,
                                  kernel_regularizer=tf.nn.l2_loss, kernel_initializer=xav())
 
-        if self.attention_mechanism:
+        if self.attention_params:
             _attn_output = self._build_attn_body()
             _units = tf.concat([_units, _attn_output], -1)
 
@@ -297,34 +328,34 @@ class PolicyNetwork(LRScheduledTFModel):
         return _logits, _state
 
     def _build_attn_body(self):
-        attn_scope = f"attention_mechanism/{self.attention_mechanism.type_}"
+        attn_scope = f"attention_params/{self.attention_params.type_}"
         with tf.variable_scope(attn_scope):
-            if self.attention_mechanism.type_ == 'general':
+            if self.attention_params.type_ == 'general':
                 _attn_output = am.general_attention(self._key, self._emb_context,
-                                                    hidden_size=self.attention_mechanism.hidden_size,
-                                                    projected_align=self.attention_mechanism.projected_align)
-            elif self.attention_mechanism.type_ == 'bahdanau':
+                                                    hidden_size=self.attention_params.hidden_size,
+                                                    projected_align=self.attention_params.projected_align)
+            elif self.attention_params.type_ == 'bahdanau':
                 _attn_output = am.bahdanau_attention(self._key, self._emb_context,
-                                                     hidden_size=self.attention_mechanism.hidden_size,
-                                                     projected_align=self.attention_mechanism.projected_align)
-            elif self.attention_mechanism.type_ == 'cs_general':
+                                                     hidden_size=self.attention_params.hidden_size,
+                                                     projected_align=self.attention_params.projected_align)
+            elif self.attention_params.type_ == 'cs_general':
                 _attn_output = am.cs_general_attention(self._key, self._emb_context,
-                                                       hidden_size=self.attention_mechanism.hidden_size,
-                                                       depth=self.attention_mechanism.depth,
-                                                       projected_align=self.attention_mechanism.projected_align)
-            elif self.attention_mechanism.type_ == 'cs_bahdanau':
+                                                       hidden_size=self.attention_params.hidden_size,
+                                                       depth=self.attention_params.depth,
+                                                       projected_align=self.attention_params.projected_align)
+            elif self.attention_params.type_ == 'cs_bahdanau':
                 _attn_output = am.cs_bahdanau_attention(self._key, self._emb_context,
-                                                        hidden_size=self.attention_mechanism.hidden_size,
-                                                        depth=self.attention_mechanism.depth,
-                                                        projected_align=self.attention_mechanism.projected_align)
-            elif self.attention_mechanism.type_ == 'light_general':
+                                                        hidden_size=self.attention_params.hidden_size,
+                                                        depth=self.attention_params.depth,
+                                                        projected_align=self.attention_params.projected_align)
+            elif self.attention_params.type_ == 'light_general':
                 _attn_output = am.light_general_attention(self._key, self._emb_context,
-                                                          hidden_size=self.attention_mechanism.hidden_size,
-                                                          projected_align=self.attention_mechanism.projected_align)
-            elif self.attention_mechanism.type_ == 'light_bahdanau':
+                                                          hidden_size=self.attention_params.hidden_size,
+                                                          projected_align=self.attention_params.projected_align)
+            elif self.attention_params.type_ == 'light_bahdanau':
                 _attn_output = am.light_bahdanau_attention(self._key, self._emb_context,
-                                                           hidden_size=self.attention_mechanism.hidden_size,
-                                                           projected_align=self.attention_mechanism.projected_align)
+                                                           hidden_size=self.attention_params.hidden_size,
+                                                           projected_align=self.attention_params.projected_align)
             else:
                 raise ValueError("wrong value for attention mechanism type")
         return _attn_output
@@ -334,20 +365,20 @@ class PolicyNetwork(LRScheduledTFModel):
 
     def get_attn_hyperparams(self) -> Optional[GobotAttnParams]:
         attn_hyperparams = None
-        if self.attention_mechanism:
-            attn_hyperparams = self.attention_mechanism
+        if self.attention_params:
+            attn_hyperparams = self.attention_params
         return attn_hyperparams
 
     def has_attn(self):
         """:returns: True if the model has an attention mechanism"""
-        return self.attention_mechanism is not None
+        return self.attention_params is not None
 
     def get_attn_window_size(self):
         """
         :returns: the length of the window the model looks with attn if the attention mechanism is configured.
                   if the model has no attention mechanism returns None.
         """
-        return self.attention_mechanism.max_num_tokens if self.has_attn() else None
+        return self.attention_params.max_num_tokens if self.has_attn() else None
 
     def __call__(self, batch_dialogues_features: BatchDialoguesFeatures,
                  states_c: np.ndarray, states_h: np.ndarray, prob: bool = False,
@@ -363,7 +394,7 @@ class PolicyNetwork(LRScheduledTFModel):
             self._features: batch_dialogues_features.b_featuress,
             self._action_mask: batch_dialogues_features.b_action_masks
         }
-        if self.attention_mechanism:
+        if self.attention_params:
             feed_dict[self._emb_context] = batch_dialogues_features.b_tokens_embeddings_paddeds
             feed_dict[self._key] = batch_dialogues_features.b_attn_keys
 
@@ -385,7 +416,7 @@ class PolicyNetwork(LRScheduledTFModel):
             self._action_mask: batch_dialogues_features.b_action_masks
         }
 
-        if self.attention_mechanism:
+        if self.attention_params:
             feed_dict[self._emb_context] = batch_dialogues_features.b_tokens_embeddings_paddeds
             feed_dict[self._key] = batch_dialogues_features.b_attn_keys
 
@@ -396,29 +427,53 @@ class PolicyNetwork(LRScheduledTFModel):
                 'momentum': self.get_momentum()}
 
     def load(self, *args, **kwargs) -> None:
-        log.info(f"[initializing `{self.__class__.__name__}` from saved]")
+        # todo move load_nn_params here?
         self._load_nn_params()
         super().load(*args, **kwargs)
 
     def _load_nn_params(self) -> None:
+        if self.debug:
+            log.debug(f"BEFORE {self.__class__.__name__} _load_nn_params()")
+
         path = str(self.load_path.with_suffix('.json').resolve())
+
+        if self.debug:
+            log.debug(f"INSIDE {self.__class__.__name__} _load_nn_params(): path={path}")
         # log.info(f"[loading parameters from {path}]")
         with open(path, 'r', encoding='utf8') as fp:
             params = json.load(fp)
+        if self.debug:
+            log.debug(f"INSIDE {self.__class__.__name__} _load_nn_params(): "
+                      f"params={params}, GRAPH_PARAMS={self.GRAPH_PARAMS}")
+
         for p in self.GRAPH_PARAMS:
-            if self.__getattribute__(p) != params.get(p) and p not in {'attn', 'attention_mechanism'}:
+            if self.__getattribute__(p) != params.get(p) and p not in {'attn', 'attention_mechanism', 'attention_params'}:
                 # todo backward-compatible attention serialization
                 raise ConfigError(f"`{p}` parameter must be equal to saved"
                                   f" model parameter value `{params.get(p)}`,"
                                   f" but is equal to `{self.__getattribute__(p)}`")
 
+        if self.debug:
+            log.debug(f"AFTER {self.__class__.__name__} _load_nn_params()")
+
     def save(self, *args, **kwargs) -> None:
         super().save(*args, **kwargs)
+        # todo move save_nn_params here?
         self._save_nn_params()
 
     def _save_nn_params(self) -> None:
+        if self.debug:
+            log.debug(f"BEFORE {self.__class__.__name__} _save_nn_params()")
+
         path = str(self.save_path.with_suffix('.json').resolve())
+        if self.debug:
+            log.debug(f"INSIDE {self.__class__.__name__} _save_nn_params(): path={path}")
         nn_params = {opt: self.__getattribute__(opt) for opt in self.SERIALIZABLE_FIELDS}
+        if self.debug:
+            log.debug(f"INSIDE {self.__class__.__name__} _save_nn_params(): nn_params={nn_params}")
         # log.info(f"[saving parameters to {path}]")
         with open(path, 'w', encoding='utf8') as fp:
             json.dump(nn_params, fp)
+
+        if self.debug:
+            log.debug(f"AFTER {self.__class__.__name__} _save_nn_params()")
