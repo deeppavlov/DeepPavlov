86d2a2e9531fc77c3c739ec6ca9a51b08ca780d9
Дмитрий Евсеев <evseev@gpu6.ipavlov.mipt.ru>
2020-04-21 17:13:01 +0300

    add import Union

--- deeppavlov/models/kbqa/wiki_parser.py
+++ deeppavlov/models/kbqa/wiki_parser.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import List, Optional
+from typing import List, Optional, Union
 
 from hdt import HDTDocument
 
