6017f17772d67a9fa4b7a037773e35f411169767
Aleksei Lymar <yoptar@gmail.com>
2020-05-21 17:26:50 +0300

    feat: allow explicitly skipping datatypes in classification data reader
    Should resolve #1218

--- deeppavlov/dataset_readers/basic_classification_reader.py
+++ deeppavlov/dataset_readers/basic_classification_reader.py
@@ -74,6 +74,9 @@ class BasicClassificationDatasetReader(DatasetReader):
                 "test": []}
         for data_type in data_types:
             file_name = kwargs.get(data_type, '{}.{}'.format(data_type, format))
+            if file_name is None:
+                continue
+            
             file = Path(data_path).joinpath(file_name)
             if file.exists():
                 if format == 'csv':
