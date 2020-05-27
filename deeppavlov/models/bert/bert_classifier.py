14f2beffc8ebbcd95df92119194b6fdb43f61498
Aleksei Lymar <yoptar@gmail.com>
2020-05-27 17:04:44 +0300

    fix: allow excluding load_path argument for bert models (#1215)
    * fix: allow excluding load_path argument for bert_classifier
    
    fixes #1160
    
    * fix: allow excluding load_path argument for bert ranker, sequence tagger and squad

--- deeppavlov/models/bert/bert_classifier.py
+++ deeppavlov/models/bert/bert_classifier.py
@@ -95,7 +95,7 @@ class BertClassifierModel(LRScheduledTFModel):
             pretrained_bert = str(expand_path(pretrained_bert))
 
             if tf.train.checkpoint_exists(pretrained_bert) \
-                    and not tf.train.checkpoint_exists(str(self.load_path.resolve())):
+                    and not (self.load_path and tf.train.checkpoint_exists(str(self.load_path.resolve()))):
                 logger.info('[initializing model with Bert from {}]'.format(pretrained_bert))
                 # Exclude optimizer and classification variables from saved variables
                 var_list = self._get_saveable_variables(
--- deeppavlov/models/bert/bert_squad.py
+++ deeppavlov/models/bert/bert_squad.py
@@ -92,7 +92,7 @@ class BertSQuADModel(LRScheduledTFModel):
             pretrained_bert = str(expand_path(pretrained_bert))
 
             if tf.train.checkpoint_exists(pretrained_bert) \
-                    and not tf.train.checkpoint_exists(str(self.load_path.resolve())):
+                    and not (self.load_path and tf.train.checkpoint_exists(str(self.load_path.resolve()))):
                 logger.info('[initializing model with Bert from {}]'.format(pretrained_bert))
                 var_list = self._get_saveable_variables(
                     exclude_scopes=('Optimizer', 'learning_rate', 'momentum', 'squad'))
