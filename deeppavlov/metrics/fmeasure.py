d8a0762a7f01d01dfe4372add8562e86dfbf7b56
dimakarp1996 <dimakarp1996@yandex.ru>
2020-06-01 12:54:43 +0300

    Update fmeasure.py

--- deeppavlov/metrics/fmeasure.py
+++ deeppavlov/metrics/fmeasure.py
@@ -19,7 +19,7 @@ from logging import getLogger
 
 import numpy as np
 from sklearn.metrics import f1_score
-
+from sklearn.preprocessing import MultiLabelBinarizer
 from deeppavlov.core.common.metrics_registry import register_metric
 
 log = getLogger(__name__)
@@ -215,8 +215,11 @@ def round_f1_macro(y_true, y_predicted):
         predictions = [np.round(x) for x in y_predicted]
     except TypeError:
         predictions = y_predicted
-
-    return f1_score(np.array(y_true), np.array(predictions), average="macro")
+    binarizer = MultiLabelBinarizer()
+    binarizer.fit(y_true)
+    y_true = binarizer.transform(y_true)
+    predictions = binarizer.transform(predictions)
+    return f1_score(y_true, predictions, average="macro")
 
 
 @register_metric('f1_weighted')
