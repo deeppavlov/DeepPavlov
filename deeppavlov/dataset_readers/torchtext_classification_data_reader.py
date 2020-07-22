528043a8f89424f6f5428b7c2089d4c7f7ea2c86
dilyararimovna <dilyara.rimovna@gmail.com>
2020-07-22 11:27:12 +0300

    feat: torchtext data reader/loader working

--- deeppavlov/dataset_readers/torchtext_classification_data_reader.py
+++ deeppavlov/dataset_readers/torchtext_classification_data_reader.py
@@ -37,33 +37,31 @@ def _init_fn():
 
 @register("torchtext_classifiction_data_reader")
 class TorchtextClassificationDataReader(DatasetReader):
-    """Class initializes datasets as a key of `torchtext.datasets.DATASETS` or
-        as an attribute of `torchtext.datasets`
+    """Class initializes datasets as an attribute of `torchtext.datasets`
     """
     @overrides
-    def read(self, dataset_title: str, data_path: str, batch_size: int, valid_portion=0.05,
+    def read(self, dataset_title: str, data_path: str, batch_size: int,
+             splits=["train", "valid", "test"], valid_portion=None,
              vocab=None, ngrams=1, split_seed=42, tokenize="spacy", *args, **kwargs) -> dict:
 
-        if dataset_title in torch_texts.DATASETS:
-            log.info(f"Dataset {dataset_title} is used as a key of `torchtext.datasets.DATASETS`.")
-            train_dataset, test_dataset = torch_texts.DATASETS[dataset_title](
-                root=data_path, ngrams=ngrams, vocab=vocab)
-
-            train_len = int(len(train_dataset) * (1. - valid_portion))
-            train_dataset, valid_dataset = random_split(train_dataset, [train_len, len(train_dataset) - train_len])
-        elif hasattr(torch_texts, dataset_title) and callable(getattr(torch_texts, dataset_title)):
+        if hasattr(torch_texts, dataset_title) and callable(getattr(torch_texts, dataset_title)):
             log.info(f"Dataset {dataset_title} is used as an attribute of `torchtext.datasets`.")
             _text = torchtext.data.Field(tokenize=tokenize)
             _label = torchtext.data.LabelField(dtype=torch.float)
-            train_dataset, test_dataset = getattr(torch_texts, dataset_title).splits(_text, _label)
-            train_dataset, valid_dataset = train_dataset.split(1 - valid_portion, random_state=random.seed(split_seed))
+            data_splits = getattr(torch_texts, dataset_title).splits(_text, _label, root=data_path)
+            assert len(data_splits) == len(splits)
+            data_splits = {data_field: data_splits[i] for i, data_field in enumerate(splits)}
+
+            if "valid" not in splits and valid_portion is not None:
+                log.info("Valid not in `splits` and `valid_portion` is given. Split `train` to `train` and `valid`")
+                data_splits["train"], data_splits["valid"] = data_splits["train"].split(
+                    1 - valid_portion, random_state=random.seed(split_seed))
         else:
             raise NotImplementedError(f"Dataset {dataset_title} was not found.")
 
-        trainloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, worker_init_fn=_init_fn(),
-                                                  shuffle=True, num_workers=2)
-        validloader = torch.utils.data.DataLoader(valid_dataset, batch_size=batch_size, worker_init_fn=_init_fn(),
-                                                  shuffle=False, num_workers=1)
-        testloader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=1)
-
-        return {"train": trainloader, "valid": validloader, "test": testloader}
+        data = {}
+        for data_field in data_splits:
+            data[data_field] = torch.utils.data.DataLoader(
+                data_splits[data_field], batch_size=batch_size, worker_init_fn=_init_fn(),
+                shuffle=True if data_field == "train" else False, num_workers=2)
+        return data
