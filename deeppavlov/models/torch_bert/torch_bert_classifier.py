60ade5bc91c4055b99f9e1be3df04dbbfdad17de
dilyararimovna <dilyara.rimovna@gmail.com>
2020-07-24 14:14:55 +0300

    fix: check if not none

--- deeppavlov/models/torch_bert/torch_bert_classifier.py
+++ deeppavlov/models/torch_bert/torch_bert_classifier.py
@@ -170,7 +170,7 @@ class TorchBertClassifierModel(TorchModel):
 
         self.optimizer = getattr(torch.optim, self.optimizer_name)(
             self.model.parameters(), **self.optimizer_parameters)
-        if self.lr_scheduler_name:
+        if self.lr_scheduler_name is not None:
             self.lr_scheduler = getattr(torch.optim.lr_scheduler, self.lr_scheduler_name)(
                 self.optimizer, **self.lr_scheduler_parameters)
 
