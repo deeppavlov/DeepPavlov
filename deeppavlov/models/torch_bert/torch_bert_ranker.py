170666ea34c83475879b1921481bfef8c8bc0f03
dilyararimovna <dilyara.rimovna@gmail.com>
2020-07-28 11:21:25 +0300

    fix: ubuntu optimizer

--- deeppavlov/models/torch_bert/torch_bert_ranker.py
+++ deeppavlov/models/torch_bert/torch_bert_ranker.py
@@ -40,9 +40,14 @@ class TorchBertRankerModel(TorchBertClassifierModel):
     """
 
     def __init__(self, pretrained_bert=None, bert_config_file=None,
-                 n_classes=2, keep_prob=0.9, return_probas=True, **kwargs) -> None:
+                 n_classes=2, keep_prob=0.9, return_probas=True,
+                 optimizer="AdamW",
+                 optimizer_parameters={"lr": 2e-5, "weight_decay": 0.01, "betas": (0.9, 0.999), "eps": 1e-6},
+                 **kwargs) -> None:
         super().__init__(pretrained_bert=pretrained_bert, bert_config_file=bert_config_file,
-                         n_classes=n_classes, keep_prob=keep_prob, return_probas=return_probas, **kwargs)
+                         n_classes=n_classes, keep_prob=keep_prob, return_probas=return_probas,
+                         optimizer=optimizer, optimizer_parameters=optimizer_parameters,
+                         **kwargs)
 
     def train_on_batch(self, features_li: List[List[InputFeatures]], y: Union[List[int], List[List[int]]]) -> Dict:
         """Train the model on the given batch.
