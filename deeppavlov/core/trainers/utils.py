978093919370416af1733c972c2b622dce0c12ce
dimakarp1996 <dimakarp1996@yandex.ru>
2020-07-29 12:27:46 +0300

    Empty str

--- deeppavlov/core/trainers/utils.py
+++ deeppavlov/core/trainers/utils.py
@@ -15,8 +15,8 @@
 from collections import OrderedDict, namedtuple
 from json import JSONEncoder
 from typing import List, Tuple, Union, Iterable
-
 import numpy
+
 from deeppavlov.core.common.metrics_registry import get_metric_by_name
 
 Metric = namedtuple('Metric', ['name', 'fn', 'inputs', 'alias'])
