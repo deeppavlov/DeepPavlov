fix: probas after softmax in ranker

deeppavlov/models/torch_bert/torch_bert_ranker.py
@@ -110,14 +110,15 @@ class TorchBertRankerModel(TorchBertClassifierModel):
             with torch.no_grad():
                 # Forward pass, calculate logit predictions
                 logits = self.model(b_input_ids, token_type_ids=None, attention_mask=b_input_masks)
-
-            # Move logits and labels to CPU and to numpy arrays
-            logits = logits[0].detach().cpu().numpy()
+                logits = logits[0]
 
             if self.return_probas:
-                pred = logits[:, 1]
+                pred = torch.nn.functional.softmax(logits, dim=-1)[:, 1]
+                pred = pred.detach().cpu().numpy()
             else:
+                logits = logits.detach().cpu().numpy()
                 pred = np.argmax(logits, axis=1)
+
             predictions.append(pred)
 
         if len(features_li) == 1: