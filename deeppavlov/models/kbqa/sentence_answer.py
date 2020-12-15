ca260ac342b3f4fb7ea2f9c0e20f670a01939afa
Fedor Ignatov <ignatov.fedor@gmail.com>
2020-12-15 22:01:43 +0300

    feat: removed pyinflect

--- deeppavlov/models/kbqa/sentence_answer.py
+++ deeppavlov/models/kbqa/sentence_answer.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import pyinflect
 import re
+
 import spacy
 
 nlp = spacy.load('en_core_web_sm')
