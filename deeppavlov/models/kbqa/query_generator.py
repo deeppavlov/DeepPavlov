0f46acad056767dc8ab06ebe45ed29472143bc27
Дмитрий Евсеев <dmitrij.euseew@yandex.ru>
2021-01-09 22:01:53 +0300

    add exception handling

--- deeppavlov/models/kbqa/query_generator.py
+++ deeppavlov/models/kbqa/query_generator.py
@@ -80,14 +80,17 @@ class QueryGenerator(QueryGeneratorBase):
 
         candidate_outputs_batch = []
         template_answers_batch = []
-        for question, question_sanitized, template_type, entities_from_ner, types_from_ner in \
-                zip(question_batch, question_san_batch, template_type_batch,
-                    entities_from_ner_batch, types_from_ner_batch):
-            candidate_outputs, template_answer = self.find_candidate_answers(question, question_sanitized,
-                                                                             template_type, entities_from_ner,
-                                                                             types_from_ner)
-            candidate_outputs_batch.append(candidate_outputs)
-            template_answers_batch.append(template_answer)
+        try:
+            for question, question_sanitized, template_type, entities_from_ner, types_from_ner in \
+                    zip(question_batch, question_san_batch, template_type_batch,
+                        entities_from_ner_batch, types_from_ner_batch):
+                candidate_outputs, template_answer = self.find_candidate_answers(question, question_sanitized,
+                                                                                 template_type, entities_from_ner,
+                                                                                 types_from_ner)
+                candidate_outputs_batch.append(candidate_outputs)
+                template_answers_batch.append(template_answer)
+        except:
+            log.info("query generator is broken")
         if self.return_answers:
             answers = self.rel_ranker(question_batch, candidate_outputs_batch, entities_from_ner_batch,
                                       template_answers_batch)
