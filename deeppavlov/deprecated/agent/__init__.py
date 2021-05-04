f9b29400bce67ca1c1345328072e68ade52ee7b5
Fedor Ignatov <ignatov.fedor@gmail.com>
2021-05-04 15:21:13 +0300

    deeppavlov.deprecated removed (#1433)
    * feat: del deeppavlov.deprecated
    
    * docs: library version and breaking changes have updated

--- deeppavlov/deprecated/agent/__init__.py
+++
@@ -1,4 +0,0 @@
-from .agent import Agent, SkillWrapper
-from .filter import Filter
-from .processor import Processor
-from .rich_content import RichControl, RichMessage
--- deeppavlov/deprecated/agents/rich_content/__init__.py
+++
@@ -1,1 +0,0 @@
-from .default_rich_content import Button, ButtonsFrame, PlainText
