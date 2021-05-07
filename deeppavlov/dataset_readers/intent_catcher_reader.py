29197ce217fee95f202ee4f15c6a5c5dd1da5b0b
oserikov <srkvoa@gmail.com>
2021-05-08 00:46:58 +0300

    wip unify md_yaml_reader and intent_catcher_reader

--- deeppavlov/dataset_readers/intent_catcher_reader.py
+++ deeppavlov/dataset_readers/intent_catcher_reader.py
@@ -10,6 +10,8 @@
 # limitations under the License.
 
 from deeppavlov.core.common.file import read_json
+from deeppavlov.core.common.file import read_yaml
+from collections import defaultdict
 from logging import getLogger
 from pathlib import Path
 from typing import Dict, List, Tuple
@@ -22,12 +24,33 @@ log = getLogger(__file__)
 
 @register('intent_catcher_reader')
 class IntentCatcherReader(DatasetReader):
-    """Reader for Intent Catcher dataset in json format"""
+    """Reader for Intent Catcher dataset in json or YAML (RASA v2) format"""
 
-    def read(self, data_path: str, *args, **kwargs) -> Dict[str, List[Tuple[str, str]]]:
+    def parse_rasa_example(self, example: str, regex: bool = False):
+        search_entities_re = re.compile(
+            "\[[ a-zA-Z0-9]+\]\([ a-zA-Z0-9]+\)")
+        example = example[2:]
+        if not regex:
+            search_entities = search_entities_re.search(example)
+            while search_entities is not None:
+                start, end = search_entities.span()
+                example = example[:start] + re.sub("\]\([ a-zA-Z0-9]+\)", "", example[start:end])[
+                    1:] + example[end:]
+                search_entities = search_entities_re.search(example)
+            example = re.sub("\?", "\?", example)
+        return example
+
+    def read(self, data_path: str, format: str = 'json', *args, **kwargs) -> Dict[str, List[Tuple[str, str]]]:
         data_types = ["train", "valid", "test"]
 
-        train_file = kwargs.get('train', 'train.json')
+        if format == 'yaml':
+            fmt = 'yml'
+        elif format == 'json':
+            fmt = 'json'
+        else:
+            raise Exception("Wrong file format. ")
+
+        train_file = kwargs.get('train', f'train.{fmt}')
 
         if not Path(data_path, train_file).exists():
             raise Exception(
@@ -39,13 +62,40 @@ class IntentCatcherReader(DatasetReader):
                 "test": []}
 
         for data_type in data_types:
-            file_name = kwargs.get(data_type, '{}.{}'.format(data_type, "json"))
+            file_name = kwargs.get(data_type, '{}.{}'.format(data_type, fmt))
             if file_name is None:
                 continue
 
             file = Path(data_path).joinpath(file_name)
             if file.exists():
-                ic_file_content = read_json(file)
+                if format == 'json':
+                    ic_file_content = read_json(file)
+                elif format == 'yaml':
+                    domain_file = Path(data_path, "domain.yml")
+                    if domain_file.exists():
+                        domain = read_yaml(domain_file)['intents']
+                    else:
+                        raise Exception("domain.yml in data path {} does not exist!".format(data_path))
+
+                    ic_file_content = read_yaml(file)
+                    file_data = defaultdict(list)
+                    for part in ic_file_content['nlu']:
+                        if part.get('intent', '') in domain:
+                            intent = part['intent']
+                            regex = False
+                        elif part.get('regex', '') in domain:
+                            intent = part['regex']
+                            regex = True
+                        else:
+                            continue
+                        file_data[intent].extend([
+                                self.parse_rasa_example(example, regex) for example in part.get('examples', '').split("\n")
+                        ])
+                        if file['version'] == 'dp_2.0':
+                            file_data[intent].extend([self.parse_rasa_example(example, True) for example in part.get('regex_examples', '').split("\n")])
+                    ic_file_content = file_data
+
+                # noinspection PyUnboundLocalVariable
                 data[data_type] = ic_file_content
             else:
                 log.warning("Cannot find {} file".format(file))
