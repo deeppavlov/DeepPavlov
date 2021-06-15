b587c580445d441b8f26d81dba9b495baea76dfc
Дмитрий Евсеев <dmitrij.euseew@yandex.ru>
2021-06-15 22:38:19 +0300

    fixes

--- deeppavlov/models/entity_linking/entity_linking_sep.py
+++ deeppavlov/models/entity_linking/entity_linking_sep.py
@@ -884,13 +884,14 @@ class EntityLinkerSep(Component, Serializable):
             high_conf_entities = []
             high_conf_nums = []
             for elem_num, (entity, conf) in enumerate(zip(top_entities, top_conf)):
-                if conf[0] == 1.0 and conf[1] > 55 and conf[2] > 0.8:
+                if len(conf) == 3 and conf[0] == 1.0 and conf[1] > 55 and conf[2] > 0.8:
                     high_conf_entities.append((entity,)+conf)
                     high_conf_nums.append(elem_num)
             
             high_conf_entities = sorted(high_conf_entities, key=lambda x: (x[1], x[3], x[2]), reverse=True)
             for n, elem_num in enumerate(high_conf_nums):
-                del top_entities[elem_num - n]
+                if elem_num - n >= 0 and elem_num - n < len(top_entities):
+                    del top_entities[elem_num - n]
                 
             top_entities = [elem[0] for elem in high_conf_entities] + top_entities
             top_conf = [elem[1:] for elem in high_conf_entities] + top_conf
