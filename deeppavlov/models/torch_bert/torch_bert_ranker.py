0468df8aa6a53f8df8de1552640209e59c9d4d94
slowwavesleep <44175589+slowwavesleep@users.noreply.github.com>
2021-07-07 13:53:22 +0300

    Unlink ranker from classifier

--- deeppavlov/models/torch_bert/torch_bert_ranker.py
+++ deeppavlov/models/torch_bert/torch_bert_ranker.py
@@ -14,19 +14,24 @@
 
 from logging import getLogger
 from typing import List, Dict, Union, Optional
+from pathlib import Path
 
 import numpy as np
 import torch
+from overrides import overrides
 from transformers.data.processors.utils import InputFeatures
+from transformers import AutoModelForSequenceClassification, AutoConfig
 
 from deeppavlov.core.common.registry import register
-from deeppavlov.models.torch_bert.torch_transformers_classifier import TorchTransformersClassifierModel
+from deeppavlov.core.models.torch_model import TorchModel
+from deeppavlov.core.common.errors import ConfigError
+from deeppavlov.core.commands.utils import expand_path
 
-logger = getLogger(__name__)
+log = getLogger(__name__)
 
 
 @register('torch_bert_ranker')
-class TorchBertRankerModel(TorchTransformersClassifierModel):
+class TorchBertRankerModel(TorchModel):
     """BERT-based model for interaction-based text ranking on PyTorch.
 
     Linear transformation is trained over the BERT pooled output from [CLS] token.
@@ -56,11 +61,15 @@ class TorchBertRankerModel(TorchTransformersClassifierModel):
                                     "betas": (0.9, 0.999),
                                     "eps": 1e-6}
 
-        super().__init__(pretrained_bert=pretrained_bert,
-                         bert_config_file=bert_config_file,
-                         n_classes=n_classes,
-                         return_probas=return_probas,
-                         optimizer=optimizer,
+        self.return_probas = return_probas
+        self.pretrained_bert = pretrained_bert
+        self.bert_config_file = bert_config_file
+        self.n_classes = n_classes
+
+        if self.return_probas and self.n_classes == 1:
+            raise RuntimeError('Set return_probas to False for regression task!')
+
+        super().__init__(optimizer=optimizer,
                          optimizer_parameters=optimizer_parameters,
                          **kwargs)
 
@@ -111,7 +120,7 @@ class TorchBertRankerModel(TorchTransformersClassifierModel):
         """
         if len(features_li) == 1 and len(features_li[0]) == 1:
             msg = f"It is not intended to use the {self.__class__} in the interact mode."
-            logger.error(msg)
+            log.error(msg)
             return [msg]
 
         predictions = []
@@ -143,3 +152,79 @@ class TorchBertRankerModel(TorchTransformersClassifierModel):
             predictions = np.hstack([np.expand_dims(el, 1) for el in predictions])
 
         return predictions
+
+    @overrides
+    def load(self, fname=None):
+        if fname is not None:
+            self.load_path = fname
+
+        if self.pretrained_bert:
+            log.info(f"From pretrained {self.pretrained_bert}.")
+            config = AutoConfig.from_pretrained(self.pretrained_bert,
+                                                # num_labels=self.n_classes,
+                                                output_attentions=False,
+                                                output_hidden_states=False)
+
+            self.model = AutoModelForSequenceClassification.from_pretrained(self.pretrained_bert, config=config)
+
+            try:
+                hidden_size = self.model.classifier.out_proj.in_features
+
+                if self.n_classes != self.model.num_labels:
+                    self.model.classifier.out_proj.weight = torch.nn.Parameter(torch.randn(self.n_classes, hidden_size))
+                    self.model.classifier.out_proj.bias = torch.nn.Parameter(torch.randn(self.n_classes))
+                    self.model.classifier.out_proj.out_features = self.n_classes
+                    self.model.num_labels = self.n_classes
+
+            except torch.nn.modules.module.ModuleAttributeError:
+                hidden_size = self.model.classifier.in_features
+
+                if self.n_classes != self.model.num_labels:
+                    self.model.classifier.weight = torch.nn.Parameter(torch.randn(self.n_classes, hidden_size))
+                    self.model.classifier.bias = torch.nn.Parameter(torch.randn(self.n_classes))
+                    self.model.classifier.out_features = self.n_classes
+                    self.model.num_labels = self.n_classes
+
+
+        elif self.bert_config_file and Path(self.bert_config_file).is_file():
+            self.bert_config = AutoConfig.from_json_file(str(expand_path(self.bert_config_file)))
+            if self.attention_probs_keep_prob is not None:
+                self.bert_config.attention_probs_dropout_prob = 1.0 - self.attention_probs_keep_prob
+            if self.hidden_keep_prob is not None:
+                self.bert_config.hidden_dropout_prob = 1.0 - self.hidden_keep_prob
+            self.model = AutoModelForSequenceClassification.from_config(config=self.bert_config)
+        else:
+            raise ConfigError("No pre-trained BERT model is given.")
+
+        self.model.to(self.device)
+
+        self.optimizer = getattr(torch.optim, self.optimizer_name)(
+            self.model.parameters(), **self.optimizer_parameters)
+        if self.lr_scheduler_name is not None:
+            self.lr_scheduler = getattr(torch.optim.lr_scheduler, self.lr_scheduler_name)(
+                self.optimizer, **self.lr_scheduler_parameters)
+
+        if self.load_path:
+            log.info(f"Load path {self.load_path} is given.")
+            if isinstance(self.load_path, Path) and not self.load_path.parent.is_dir():
+                raise ConfigError("Provided load path is incorrect!")
+
+            weights_path = Path(self.load_path.resolve())
+            weights_path = weights_path.with_suffix(f".pth.tar")
+            if weights_path.exists():
+                log.info(f"Load path {weights_path} exists.")
+                log.info(f"Initializing `{self.__class__.__name__}` from saved.")
+
+                # now load the weights, optimizer from saved
+                log.info(f"Loading weights from {weights_path}.")
+                checkpoint = torch.load(weights_path, map_location=self.device)
+                # set strict flag to False if position_ids are missing
+                # this is needed to load models trained on older versions
+                # of transformers library
+                strict_load_flag = bool([key for key in checkpoint["model_state_dict"].keys()
+                                         if key.endswith("embeddings.position_ids")])
+                self.model.load_state_dict(checkpoint["model_state_dict"], strict=strict_load_flag)
+                self.optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
+                self.epochs_done = checkpoint.get("epochs_done", 0)
+            else:
+                log.info(f"Init from scratch. Load path {weights_path} does not exist.")
