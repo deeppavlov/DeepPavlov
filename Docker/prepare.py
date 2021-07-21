584967078b706b07628cd73b29a6c4b8916b3bd0
Дмитрий Евсеев <dmitrij.euseew@yandex.ru>
2021-07-21 14:25:54 +0300

    update

--- Docker/prepare.py
+++ Docker/prepare.py
@@ -2,5 +2,5 @@ from main import initial_setup
 from deeppavlov import deep_download
 
 if __name__ == '__main__':
-    deep_download('entity_linking_vx_sep_cpu.json')
+    deep_download('entity_linking_vx_siam.json')
     initial_setup()
