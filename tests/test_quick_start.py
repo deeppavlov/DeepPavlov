b8b7c38a7124286c916cf4afba0410ff670f01d4
agsedova <anastasiia.sedova@gmail.com>
2021-08-19 11:16:21 +0200

    fix tests

--- tests/test_quick_start.py
+++ tests/test_quick_start.py
@@ -54,6 +54,27 @@ FOUR_ARGUMENTS_INFER_CHECK = ('Dummy text', 'Dummy text', 'Dummy text', 'Dummy_t
 
 # Mapping from model name to config-model_dir-ispretrained and corresponding queries-response list.
 PARAMS = {
+    "relation_extraction": {
+        ("relation_extraction/re_docred.json", "relation_extraction", ('IP', )):
+            [
+                (
+                    [["Barack", "Obama", "is", "married", "to", "Michelle", "Obama", ",", "born", "Michelle",
+                      "Robinson", "."]],
+                    [[[(0, 2)], [(5, 7), (9, 11)]]],
+                    [["PER", "PER"]]
+                ),
+                (['P26'], ['spouse'])
+            ],
+        ("relation_extraction/re_rured.json", "relation_extraction", ('IP', )):
+            [
+                (
+                    [["Илон", "Маск", "живет", "в", "Сиэттле", "."]],
+                    [[[(0, 2)], [(4, 6)]]],
+                    [["PERSON", "CITY"]]
+                ),
+                (['P551'], ['место жительства'])
+            ]
+    },
     "faq": {
         ("faq/tfidf_logreg_en_faq.json", "faq_tfidf_logreg_en", ALL_MODES): [ONE_ARGUMENT_INFER_CHECK],
         ("faq/tfidf_autofaq.json", "faq_tfidf_cos", ALL_MODES): [ONE_ARGUMENT_INFER_CHECK],
@@ -299,27 +320,6 @@ PARAMS = {
     },
     "nemo": {
         ("nemo/tts2asr_test.json", "nemo", ('IP',)): [ONE_ARGUMENT_INFER_CHECK]
-    },
-    "relation_extraction": {
-        ("relation_extraction/re_docred.json", "relation_extraction", ('IP', )):
-            [
-                (
-                    [["Barack", "Obama", "is", "married", "to", "Michelle", "Obama", ",", "born", "Michelle",
-                      "Robinson", "."]],
-                    [[[(0, 2)], [(5, 7), (9, 11)]]],
-                    [["PER", "PER"]]
-                ),
-                (['P26'], ['spouse'])
-            ],
-        ("relation_extraction/re_rured.json", "relation_extraction", ('IP', )):
-            [
-                (
-                    [["Илон", "Маск", "живет", "в", "Сиэттле", "."]],
-                    [[[(0, 2)], [(4, 6)]]],
-                    [["PERSON", "CITY"]]
-                ),
-                (['P551'], ['место жительства'])
-            ]
     }
 }
 
