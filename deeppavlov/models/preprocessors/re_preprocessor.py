3320a49c12bcf4b3f3a31e22797f6ffc1b434302
agsedova <anastasiia.sedova@gmail.com>
2021-08-20 15:45:06 +0200

    test fix

--- deeppavlov/models/preprocessors/re_preprocessor.py
+++ deeppavlov/models/preprocessors/re_preprocessor.py
@@ -92,22 +92,35 @@ class REPreprocessor(Component):
         #     entity_pos = ast.literal_eval(entity_pos[0])
         #     entity_tags = ast.literal_eval(entity_tags[0])
         #
-        # log.info(str(tokens))
-        # log.info("\n")
-        # log.info(str(entity_pos))
-        # log.info("\n")
-        # log.info(str(entity_tags))
-        # log.info("\n")
-        # log.info(type(tokens))
-        # log.info("\n")
-        # log.info(type(entity_tags))
-        # log.info("\n")
-        # log.info(type(entity_tags))
+        log.info(str(tokens))
+        log.info("\n")
+        log.info(type(tokens))
+        log.info("\n")
+        log.info(str(entity_pos))
+        log.info("\n")
+        log.info(type(entity_pos))
+        log.info("\n")
+        log.info(str(entity_tags))
+        log.info("\n")
+        log.info(type(entity_tags))
 
         for doc, ent_pos, ent_tags in zip(tokens, entity_pos, entity_tags):
 
             count = 0
             doc_wordpiece_tokens = []
+
+            log.info(f"doc: {str(doc)}")
+            log.info("\n")
+            log.info(f"doc type: {type(doc)}")
+            log.info("\n")
+            log.info(f"ent_pos: {str(ent_pos)}")
+            log.info("\n")
+            log.info(f"ent_pos type: {type(ent_pos)}")
+            log.info("\n")
+            log.info(f"ent_tags: {str(ent_tags)}")
+            log.info("\n")
+            log.info(f"ent_tags type: {type(ent_tags)}")
+            log.info("\n")
 
             entity1_pos_start = list(zip(*ent_pos[0]))[0]  # first entity mentions' start positions
             entity1_pos_end = list(zip(*ent_pos[0]))[1]  # first entity mentions' end positions
