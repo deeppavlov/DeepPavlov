88351b062da5c02ad1e8fc531aab958486b43d01
agsedova <anastasiia.sedova@gmail.com>
2021-08-22 23:50:48 +0200

    add docstring and change default value

--- deeppavlov/models/relation_extraction/relation_extraction_bert.py
+++ deeppavlov/models/relation_extraction/relation_extraction_bert.py
@@ -22,10 +22,9 @@ class REBertModel(TorchModel):
             model_name: str,
             num_ner_tags: int,
             pretrained_bert: str = None,
-            bert_config_file: Optional[str] = None,
             criterion: str = "CrossEntropyLoss",
             optimizer: str = "AdamW",
-            optimizer_parameters: Dict = {"lr": 5e-5, "weight_decay": 0.01, "eps": 1e-6},
+            optimizer_parameters: Dict = None,
             return_probas: bool = False,
             attention_probs_keep_prob: Optional[float] = None,
             hidden_keep_prob: Optional[float] = None,
@@ -33,11 +32,28 @@ class REBertModel(TorchModel):
             threshold: Optional[float] = None,
             device: str = "cpu",
             **kwargs
-    ):
+    ) -> None:
+        """
+        Transformer-based model on PyTorch for relation extraction. It predicts a relation hold between entities in a
+        text sample (one or several sentences).
+        Args:
+            n_classes: number of output classes
+            model_name: the model which will be used for extracting the relations
+            num_ner_tags: number of NER tags
+            pretrained_bert: key title of pretrained Bert model (e.g. "bert-base-uncased")
+            criterion: criterion name from `torch.nn`
+            optimizer: optimizer name from `torch.optim`
+            optimizer_parameters: dictionary with optimizer's parameters
+            return_probas: set this to `True` if you need the probabilities instead of raw answers
+            attention_probs_keep_prob: keep_prob for Bert self-attention layers
+            hidden_keep_prob: keep_prob for Bert hidden layers
+            clip_norm: clip gradients by norm
+            threshold: manually set value for defining the positively predicted classes (instead of adaptive one)
+            device: cpu/gpu device to use for training the model
+        """
         self.n_classes = n_classes
         self.num_ner_tags = num_ner_tags
         self.pretrained_bert = pretrained_bert
-        self.bert_config_file = bert_config_file
         self.return_probas = return_probas
         self.attention_probs_keep_prob = attention_probs_keep_prob
         self.hidden_keep_prob = hidden_keep_prob
@@ -47,6 +63,11 @@ class REBertModel(TorchModel):
 
         if self.n_classes == 0:
             raise ConfigError("Please provide a valid number of classes.")
+
+        if optimizer_parameters is None:
+            self.optimizer_parameters = {"lr": 5e-5, "weight_decay": 0.01, "eps": 1e-6}
+        else:
+            self.optimizer_parameters = optimizer_parameters
 
         super().__init__(
             n_classes=n_classes,
