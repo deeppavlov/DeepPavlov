3bac78541e90bea845450ad3886e7f2ee1eab741
NoBugsHere <eugene200020@gmail.com>
2021-11-11 12:44:21 +0300

    add roberta support

--- deeppavlov/models/torch_bert/torch_transformers_squad.py
+++ deeppavlov/models/torch_bert/torch_transformers_squad.py
@@ -128,7 +128,7 @@ class TorchTransformersSquad(TorchModel):
         y_end = [x[0] for x in y_end]
         b_y_st = torch.from_numpy(np.array(y_st)).to(self.device)
         b_y_end = torch.from_numpy(np.array(y_end)).to(self.device)
-        
+       
         input_ = {
             'input_ids': b_input_ids,
             'attention_mask': b_input_masks,
@@ -184,7 +184,9 @@ class TorchTransformersSquad(TorchModel):
         b_input_ids = torch.cat(input_ids, dim=0).to(self.device)
         b_input_masks = torch.cat(input_masks, dim=0).to(self.device)
         b_input_type_ids = torch.cat(input_type_ids, dim=0).to(self.device)
-        
+        if self.pretrained_bert == 'roberta-base':
+            b_input_type_ids = b_input_type_ids.unsqueeze(1).expand(-1, b_input_ids.shape[-1])
+
         input_ = {
             'input_ids': b_input_ids,
             'attention_mask': b_input_masks,
