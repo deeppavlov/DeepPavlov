af607447774bdb9795ac2f9fe9a913427559edc9
Vasily <vaskonov@users.noreply.github.com>
2021-12-16 22:12:04 +0300

    Feat/glue superglue update (#1508)
    * Add wnli config
    
    * Update copa config
    
    * Fix path
    
    * Fix record path
    
    * Exclude train from evaluation
    
    * Exclude train from evaluation
    
    * add ranker
    
    * update ranker
    
    * feat: deeppavlov version update
    
    Co-authored-by: Fedor Ignatov <ignatov.fedor@gmail.com>
    Co-authored-by: slowwavesleep <44175589+slowwavesleep@users.noreply.github.com>

--- deeppavlov/_meta.py
+++ deeppavlov/_meta.py
@@ -1,4 +1,4 @@
-__version__ = '0.17.1'
+__version__ = '0.17.2'
 __author__ = 'Neural Networks and Deep Learning lab, MIPT'
 __description__ = 'An open source library for building end-to-end dialog systems and training chatbots.'
 __keywords__ = ['NLP', 'NER', 'SQUAD', 'Intents', 'Chatbot']
