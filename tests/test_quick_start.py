a63e6ec897cbec1ca16da2105157d0b9bd2a98d8
Fedor Ignatov <ignatov.fedor@gmail.com>
2022-02-04 09:48:01 +0300

    Revert "refactor: ranking_ubuntu_v2_torch_bert_uncased -> ranking_ubuntu_v2_bert_uncased"
    This reverts commit e656079b70ae6260af065ea47ce37d9ff46c73dc.

--- tests/test_quick_start.py
+++ tests/test_quick_start.py
@@ -233,7 +233,8 @@ PARAMS = {
         ("ranking/ranking_ubuntu_v2_mt_word2vec_dam_transformer.json", "ranking", ('IP',)):
             [(' & & & & & & & & bonhoeffer  whar drives do you want to mount what &  i have an ext3 usb drive  '
               '& look with fdisk -l & hello there & fdisk is all you need',
-              None)]
+              None)],
+        ("ranking/ranking_ubuntu_v2_torch_bert_uncased.json", "ranking", ('TI',)): [ONE_ARGUMENT_INFER_CHECK]
     },
     "doc_retrieval": {
         ("doc_retrieval/en_ranker_tfidf_wiki_test.json", "doc_retrieval", ('TI',)): [ONE_ARGUMENT_INFER_CHECK],
