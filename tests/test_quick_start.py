74b861192c51bce2be7fd790630c7452c885c923
Vasily <vaskonov@users.noreply.github.com>
2022-03-24 18:04:36 +0300

    Remove/zh squad (#1534)
    * Remove zh squad
    
    * Remove tests
    
    * Fix docs
    
    Co-authored-by: Fedor Ignatov <ignatov.fedor@gmail.com>

--- tests/test_quick_start.py
+++ tests/test_quick_start.py
@@ -257,8 +257,6 @@ PARAMS = {
         ("squad/squad.json", "squad_model", ALL_MODES): [TWO_ARGUMENTS_INFER_CHECK],
         ("squad/squad_ru.json", "squad_model_ru", ALL_MODES): [TWO_ARGUMENTS_INFER_CHECK],
         ("squad/multi_squad_noans.json", "multi_squad_noans", ('IP',)): [TWO_ARGUMENTS_INFER_CHECK],
-        ("squad/squad_zh_bert_mult.json", "squad_zh_bert_mult", ALL_MODES): [TWO_ARGUMENTS_INFER_CHECK],
-        ("squad/squad_zh_bert_zh.json", "squad_zh_bert_zh", ALL_MODES): [TWO_ARGUMENTS_INFER_CHECK],
         ("squad/squad_torch_bert.json", "squad_torch_bert", ('IP', 'TI')): [TWO_ARGUMENTS_INFER_CHECK],
         ("squad/squad_torch_bert_infer.json", "squad_torch_bert_infer", ('IP',)): [TWO_ARGUMENTS_INFER_CHECK]
     },
