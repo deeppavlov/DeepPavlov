616eda02d81b8519d9160b065cca94ebc182a51d
Дмитрий Евсеев <dmitrij.euseew@yandex.ru>
2022-04-13 10:29:32 +0300

    fix tests

--- deeppavlov/models/torch_bert/crf.py
+++ deeppavlov/models/torch_bert/crf.py
@@ -14,14 +14,15 @@ class CRF(CRFbase):
         nn.init.zeros_(self.transitions)
         nn.init.zeros_(self.start_transitions)
         nn.init.zeros_(self.end_transitions)
-        self.stats = torch.zeros(num_tags, num_tags)
-        self.zeros = torch.zeros(num_tags, num_tags)
-        self.neg = torch.full((num_tags, num_tags), -1000)
+        self.stats = torch.zeros((num_tags, num_tags), dtype=torch.float)
+        self.zeros = torch.zeros((num_tags, num_tags), dtype=torch.float)
+        self.neg = torch.full((num_tags, num_tags), -1000.0)
 
     def forward(self, tags_batch: torch.LongTensor, y_masks: np.ndarray):
         seq_lengths = np.sum(y_masks, axis=1)
         for seq_len, tags_list in zip(seq_lengths, tags_batch):
             if seq_len > 1:
                 for i in range(seq_len - 1):
-                    self.stats[tags_list[i]][tags_list[i + 1]] += 1
-        self.transitions = torch.where(self.stats > 0, self.zeros, self.neg)
+                    self.stats[int(tags_list[i])][int(tags_list[i + 1])] += 1.0
+        with torch.no_grad():
+            self.transitions.copy_(torch.where(self.stats > 0, self.zeros, self.neg))
