2ba02d8315cc311af3c20e16b62f16f2f4f3e62c
Fedor Ignatov <ignatov.fedor@gmail.com>
2022-04-15 16:48:53 +0300

    feat: squad aliases

--- deeppavlov/core/common/aliases.py
+++ deeppavlov/core/common/aliases.py
@@ -13,16 +13,31 @@
 # limitations under the License.
 
 ALIASES = {
+    'multi_squad_noans': 'qa_squad2_bert',
+    'multi_squad_noans_infer': 'qa_squad2_bert',
+    'multi_squad_retr_noans': 'qa_squad2_bert',
     'ner_collection3_m1': 'ner_collection3_bert',
     'ner_conll2003': 'ner_conll2003_bert',
     'ner_conll2003_torch_bert': 'ner_conll2003_bert',
     'ner_dstc2': 'ner_conll2003_bert',
-    'ner_ontonotes': 'ner_ontonotes_bert',
-    'ner_ontonotes_bert_emb': 'ner_ontonotes_bert',
-    'ner_ontonotes_bert_torch': 'ner_ontonotes_bert',
-    'ner_ontonotes_bert_mult_torch': 'ner_ontonotes_bert_mult',
     'ner_few_shot_ru': 'ner_rus_bert',
     'ner_few_shot_ru_simulate': 'ner_rus_bert',
+    'ner_ontonotes': 'ner_ontonotes_bert',
+    'ner_ontonotes_bert_emb': 'ner_ontonotes_bert',
+    'ner_ontonotes_bert_mult_torch': 'ner_ontonotes_bert_mult',
+    'ner_ontonotes_bert_torch': 'ner_ontonotes_bert',
     'ner_rus': 'ner_rus_bert',
-    'sentseg_dailydialog': 'sentseg_dailydialog_bert'
+    'ru_odqa_infer_wiki_rubert': 'ru_odqa_infer_wiki',
+    'sentseg_dailydialog': 'sentseg_dailydialog_bert',
+    'squad': 'squad_bert',
+    'squad_bert_infer': 'squad_bert',
+    'squad_bert_multilingual_freezed_emb': 'squad_bert',
+    'squad_ru': 'squad_ru_bert',
+    'squad_ru_bert_infer': 'squad_ru_bert',
+    'squad_ru_convers_distilrubert_2L_infer': 'squad_ru_convers_distilrubert_2L',
+    'squad_ru_convers_distilrubert_6L_infer': 'squad_ru_convers_distilrubert_6L',
+    'squad_ru_rubert': 'squad_ru_bert',
+    'squad_ru_rubert_infer': 'squad_ru_bert',
+    'squad_torch_bert': 'squad_bert',
+    'squad_torch_bert_infer': 'squad_bert'
 }
