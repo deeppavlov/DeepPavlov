7ec638824dae9e1d7f6e4e90678c189ad3779a9e
dimakarp1996 <dimakarp1996@github.com>
2022-07-10 13:03:56 +0300

    current glue version

---
+++ train.sh
@@ -0,0 +1,1 @@
+python -m deeppavlov train config_copa.json | tee copa.txt; python -m deeppavlov train config_stsb.json | tee stsb.txt;  python -m deeppavlov train config_conll.json | tee conll.txt;python -m deeppavlov train config_cola.json | tee cola.txt;   python -m deeppavlov train config_rte.json | tee rte.txt
