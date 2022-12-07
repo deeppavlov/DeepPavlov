ea779a9e48c04eb60e4959d8696e28acceae4e15
vasily <vasili.konov@gmail.com>
2022-12-07 13:27:07 +0300

    Update upload script

--- utils/prepare/upload.py
+++ utils/prepare/upload.py
@@ -13,19 +13,25 @@
 # limitations under the License.
 
 import argparse
-import os
 import shutil
 import tarfile
+import pathlib
 from pathlib import Path
 
+from hashes import main
 from deeppavlov.core.commands.utils import parse_config
 from deeppavlov.core.common.file import find_config
-from hashes import main
 
 
-def upload(config_in_file):
+def upload(config_in_file: str, tar_name: str, tar_output_dir: str):
 
-    print(config_in_file)
+    if not Path.exists(Path(tar_output_dir)):
+        raise RuntimeError(f'A folder {tar_output_dir} does not exist')
+
+    if not Path.exists(Path(config_in_file)):
+        raise RuntimeError(f'A config {config_in_file} does not exist')
+
+    print(f'Config: {config_in_file}')
     config_in = parse_config(config_in_file)
     config_in_file = find_config(config_in_file)
 
@@ -33,35 +39,33 @@ def upload(config_in_file):
     models_path = Path(config_in['metadata']['variables']['MODELS_PATH']).expanduser()
     model_name, class_name = config_in_file.stem, config_in_file.parent.name
     
-    if str(model_name) not in str(model_path):
-        raise(f'{model_name} is not the path of the {model_path}')
+    if tar_name is None:
+        tar_name = f'{model_name}'
+        print(f'tar_name set to {tar_name}')
     
-    arcname = str(model_path).split("models/")[1]
-    tar_path = models_path/model_name
-    tmp_folder = f'/tmp/'
-    tmp_tar = tmp_folder + f'{model_name}.tar.gz'
+    full_tar_name = tar_output_dir + "/" + tar_name + '.tar.gz'
+    if Path.exists(Path(full_tar_name)):
+        raise RuntimeError(f'An archive {Path(full_tar_name)} already exists')
 
-    print("model_path", model_path)
-    print("class_name", class_name)
-    print("model_name", model_name)
+    print(f'model_path: {model_path}')
+    print(f'class_name: {class_name}')
+    print(f'model_name: {model_name}')
     
-    print("Start tarring")
-    archive = tarfile.open(tmp_tar, "w|gz")
-    archive.add(model_path, arcname=arcname)
+    print("Start tarring...")
+    archive = tarfile.open(full_tar_name, "w|gz")
+    archive.add(model_path, arcname=pathlib.os.sep)
     archive.close()
     print("Stop tarring")
+    print(f'Tar archive: {Path(full_tar_name)} has been created')
 
     print("Calculating hash")
-    main(tmp_tar)
-
-    print("tmp_tar", tmp_tar)
-    command = f'scp -r {tmp_folder}{model_name}* share.ipavlov.mipt.ru:/home/export/v1/{class_name}'
-    donwload_url = f'http://files.deeppavlov.ai/v1/{class_name}/{model_name}.tar.gz'
-    print(command, donwload_url, sep='\n')
+    main(full_tar_name)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("config_in", help="path to a config", type=str)
+    parser.add_argument("--tar_name", help="name of the tar archive (without tar.gz extension)", default=None, required=False, type=str)
+    parser.add_argument("--tar_output_dir", help="dir to save a tar archive", default="./", required=False, type=str)
     args = parser.parse_args()
-    upload(args.config_in)
+    upload(args.config_in, args.tar_name, args.tar_output_dir)
