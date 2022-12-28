68af6bc45802129cd89851c25948d07d2df7900f
vasily <vasili.konov@gmail.com>
2022-12-29 01:24:41 +0300

    Update upload script

--- utils/prepare/upload.py
+++ utils/prepare/upload.py
@@ -13,25 +13,24 @@
 # limitations under the License.
 
 import argparse
-import shutil
-import tarfile
 import pathlib
+import tarfile
 from pathlib import Path
 
-from hashes import main
 from deeppavlov.core.commands.utils import parse_config
 from deeppavlov.core.common.file import find_config
+from hashes import main
 
 
-def upload(config_in_file: str, tar_name: str, tar_output_dir: str):
+def upload(config_in_file: str, tar_name: str, tar_output_dir: Path):
 
-    if not Path.exists(Path(tar_output_dir)):
+    if not tar_output_dir.exists():
         raise RuntimeError(f'A folder {tar_output_dir} does not exist')
 
-    if not Path.exists(Path(config_in_file)):
+    print(f'Config: {config_in_file}')
+    if not Path(config_in_file).exists():
         raise RuntimeError(f'A config {config_in_file} does not exist')
 
-    print(f'Config: {config_in_file}')
     config_in = parse_config(config_in_file)
     config_in_file = find_config(config_in_file)
 
@@ -43,18 +42,17 @@ def upload(config_in_file: str, tar_name: str, tar_output_dir: str):
         tar_name = f'{model_name}'
         print(f'tar_name set to {tar_name}')
     
-    full_tar_name = tar_output_dir + "/" + tar_name + '.tar.gz'
-    if Path.exists(Path(full_tar_name)):
+    full_tar_name = tar_output_dir / f'{tar_name}.tar.gz'
+    if Path(full_tar_name).exists():
         raise RuntimeError(f'An archive {Path(full_tar_name)} already exists')
 
     print(f'model_path: {model_path}')
     print(f'class_name: {class_name}')
-    print(f'model_name: {model_name}')
+    print(f'model_name: {model_name}') 
+    print(f'Start tarring to {full_tar_name}')
+    with tarfile.open(str(full_tar_name), "w|gz") as archive:
+        archive.add(model_path, arcname=pathlib.os.sep)
     
-    print("Start tarring...")
-    archive = tarfile.open(full_tar_name, "w|gz")
-    archive.add(model_path, arcname=pathlib.os.sep)
-    archive.close()
     print("Stop tarring")
     print(f'Tar archive: {Path(full_tar_name)} has been created')
 
@@ -64,8 +62,10 @@ def upload(config_in_file: str, tar_name: str, tar_output_dir: str):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("config_in", help="path to a config", type=str)
-    parser.add_argument("--tar_name", help="name of the tar archive (without tar.gz extension)", default=None, required=False, type=str)
-    parser.add_argument("--tar_output_dir", help="dir to save a tar archive", default="./", required=False, type=str)
+    parser.add_argument('-c', '--config_in', help='path to a config', type=str)
+    parser.add_argument('-n', '--tar_name', help='name of the tar archive (without tar.gz extension)', 
+        default=None, required=False, type=str)
+    parser.add_argument('-o', '--tar_output_dir', help='dir to save a tar archive', default='./', 
+        required=False, type=Path)
     args = parser.parse_args()
     upload(args.config_in, args.tar_name, args.tar_output_dir)
