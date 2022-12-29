1cf722211a255233e41bbde50559db04f26989f3
Fedor Ignatov <ignatov.fedor@gmail.com>
2022-12-29 17:25:06 +0300

    refactor: formatting

--- utils/prepare/upload.py
+++ utils/prepare/upload.py
@@ -23,7 +23,6 @@ from hashes import main
 
 
 def upload(config_in_file: str, tar_name: str, tar_output_dir: Path):
-
     if not tar_output_dir.exists():
         raise RuntimeError(f'A folder {tar_output_dir} does not exist')
 
@@ -35,24 +34,23 @@ def upload(config_in_file: str, tar_name: str, tar_output_dir: Path):
     config_in_file = find_config(config_in_file)
 
     model_path = Path(config_in['metadata']['variables']['MODEL_PATH']).expanduser()
-    models_path = Path(config_in['metadata']['variables']['MODELS_PATH']).expanduser()
     model_name, class_name = config_in_file.stem, config_in_file.parent.name
-    
+
     if tar_name is None:
         tar_name = f'{model_name}'
         print(f'tar_name set to {tar_name}')
-    
+
     full_tar_name = tar_output_dir / f'{tar_name}.tar.gz'
     if Path(full_tar_name).exists():
         raise RuntimeError(f'An archive {Path(full_tar_name)} already exists')
 
     print(f'model_path: {model_path}')
     print(f'class_name: {class_name}')
-    print(f'model_name: {model_name}') 
+    print(f'model_name: {model_name}')
     print(f'Start tarring to {full_tar_name}')
     with tarfile.open(str(full_tar_name), "w|gz") as archive:
         archive.add(model_path, arcname=pathlib.os.sep)
-    
+
     print("Stop tarring")
     print(f'Tar archive: {Path(full_tar_name)} has been created')
 
@@ -63,9 +61,9 @@ def upload(config_in_file: str, tar_name: str, tar_output_dir: Path):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--config_in', help='path to a config', type=str)
-    parser.add_argument('-n', '--tar_name', help='name of the tar archive (without tar.gz extension)', 
-        default=None, required=False, type=str)
-    parser.add_argument('-o', '--tar_output_dir', help='dir to save a tar archive', default='./', 
-        required=False, type=Path)
+    parser.add_argument('-n', '--tar_name', help='name of the tar archive (without tar.gz extension)',
+                        default=None, required=False, type=str)
+    parser.add_argument('-o', '--tar_output_dir', help='dir to save a tar archive', default='./',
+                        required=False, type=Path)
     args = parser.parse_args()
     upload(args.config_in, args.tar_name, args.tar_output_dir)
