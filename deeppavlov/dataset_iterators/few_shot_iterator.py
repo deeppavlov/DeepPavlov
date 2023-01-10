2401cf8c753e7c05e7fad5070eedc6e8f07958ec
nastyachizhikova <apchizhikova@gmail.com>
2023-01-10 17:43:39 +0300

    update few_shot_iterator

--- deeppavlov/dataset_iterators/few_shot_iterator.py
+++ deeppavlov/dataset_iterators/few_shot_iterator.py
@@ -18,6 +18,7 @@ from logging import getLogger
 from pathlib import Path
 from random import Random
 from typing import Dict, Any, List, Tuple, Optional
+from collections import defaultdict
 
 from deeppavlov.core.common.registry import register
 from deeppavlov.core.data.data_learning_iterator import DataLearningIterator
@@ -25,11 +26,28 @@ from deeppavlov.core.data.data_learning_iterator import DataLearningIterator
 ENTAILMENT = 'entailment'
 NON_ENTAILMENT = 'non_entailment'
 
+SUPPORT_DATASET_PATH="~/.deeppavlov/preprocessed_datasets/support_dataset.json"
+
 log = getLogger(__name__)
 
 
 @register('few_shot_iterator')
 class FewShotIterator(DataLearningIterator):
+    """
+    Class gets data dictionary from DatasetReader instance, samples N examples from each class of the training data
+    and, if necessary, returns data in the NLI format (all possible pairs of training sentences).
+
+    Args:
+        data: dictionary of data with fields "train", "valid" and "test" (or some of them)
+        seed: random seed for iterating
+        shuffle: whether to returned shuffled examples
+        shot: number of examples to sample for each class in training data
+        shot_test: number of examples to sample for each class in validation and test
+        return_nli_format: whether to return the data in NLI format - pairs of sentences separated by a special token
+
+    Attributes:
+        data: dictionary of data with fields "train", "valid" and "test" (or some of them)
+        """
 
     def __init__(self,
                  data: Dict[str, List[Tuple[Any, Any]]],
@@ -37,25 +55,27 @@ class FewShotIterator(DataLearningIterator):
                  shuffle: bool = True,
                  shot: Optional[int] = None,
                  shot_test: Optional[int] = None,
-                 save_path: Optional[str] = None,
-                 return_nli_format: bool = False,
-                 *args, **kwargs) -> None:
+                 return_nli_format: bool = False
+                 ) -> None:
         self.shot = shot
         self.shot_test = shot_test
         self.shuffle = shuffle
         self.random = Random(seed)
 
-        self.train = self.get_shot_examples(data.get('train', []), self.shot)
-        self.valid = self.get_shot_examples(data.get('valid', []), self.shot_test)
-        self.test = self.get_shot_examples(data.get('test', []), self.shot_test)
+        self.train = self.delete_oos(data.get('train', []))
+        self.valid = self.delete_oos(data.get('valid', []))
+        self.test = self.delete_oos(data.get('test', []))
 
-        if save_path is not None:
-            save_path = Path(save_path).expanduser()
-            save_path.parent.mkdir(parents=True, exist_ok=True)
-            with save_path.open("w") as file:
-                json_dict = {"columns": ["text", "category"]}
-                json_dict["data"] = [[text, label] for text, label in self.train]
-                json.dump(json_dict, file, indent=4)
+        self.train = self.get_shot_examples(self.train, self.shot)
+        self.valid = self.get_shot_examples(self.valid, self.shot_test)
+        self.test = self.get_shot_examples(self.test, self.shot_test)
+
+        save_path = Path(SUPPORT_DATASET_PATH).expanduser()
+        save_path.parent.mkdir(parents=True, exist_ok=True)
+        with save_path.open("w") as file:
+            json_dict = {"columns": ["text", "category"]}
+            json_dict["data"] = [[text, label] for text, label in self.train]
+            json.dump(json_dict, file, indent=4)
 
         if return_nli_format:
             self.train = self.convert2nli(self.train)
@@ -67,31 +87,26 @@ class FewShotIterator(DataLearningIterator):
             'valid': self.valid,
             'test': self.test,
             'all': self.train + self.test + self.valid
-        }
+            }
+    
+    def _gather_info(self, data: List[Tuple[Any, Any]]) -> Tuple[Dict, Dict]:
+        unique_labels = set([label for text, label in data])
 
-    def _gather_info(self, data: List[Tuple[Any, Any]]):
-        unique_labels = list(set([label for text, label in data]))
-
-        label2examples = {}
-        for label in unique_labels:
-            label2examples[label] = []
+        label2examples = defaultdict(list)
         for text, label in data:
             label2examples[label].append(text)
 
-        label2negative = {}
-        for i, label in enumerate(unique_labels):
-            label2negative[label] = unique_labels.copy()
-            del label2negative[label][i]
+        label2negative = {label: unique_labels - {label} for label in unique_labels}
 
         return label2examples, label2negative
 
-    def convert2nli(self, data: List[Tuple[Any, Any]]) -> List[Tuple[Any, Any]]:
+    def convert2nli(self, data: List[Tuple[Any, Any]]) -> List[Tuple[Tuple[Any, Any], Any]]:
         if len(data) == 0:
             return data
 
         label2examples, label2negative = self._gather_info(data)
-
         nli_triplets = []
+
         # negative examples
         for text, label in data:
             for negative_label in label2negative[label]:
@@ -103,33 +118,40 @@ class FewShotIterator(DataLearningIterator):
             for positive_example in label2examples[label]:
                 if positive_example != text:
                     nli_triplets.append([[text, positive_example], ENTAILMENT])
-
+        
         if self.shuffle:
             self.random.shuffle(nli_triplets)
 
         return nli_triplets
+        
+    def delete_oos(self, data: List[Tuple[Any, Any]]) -> List[Tuple[Any, Any]]:
+        filtered_data = []
+        for text, label in data:
+            if label != 'oos':
+                filtered_data.append([text, label])
+        return filtered_data
 
-    def get_shot_examples(self, data, shot):
+    def get_shot_examples(self, data: List[Tuple[Any, Any]], shot: int) -> List[Tuple[Any, Any]]:
         if shot is None:
             return data
 
         # shuffle data to select shot-examples
-        if self.shuffle:
-            self.random.shuffle(data)
+        self.random.shuffle(data)
 
-        data_dict = {}
-        for _, label in data:
-            data_dict[label] = []
-
+        data_dict = defaultdict(list)
         for text, label in data:
             if len(data_dict[label]) < shot:
                 data_dict[label].append(text)
-
-        if max(len(x) for x in data_dict.values()) < shot:
-            log.warning(f"Some labels have less than \"shot\"={shot} examples")
+        
+        if min(len(x) for x in data_dict.values()) < shot:
+            log.warning(f"Some labels have less than {shot} examples")
 
         new_data = []
         for label in data_dict.keys():
             for text in data_dict[label]:
                 new_data.append((text, label))
+
+        if self.shuffle:
+            self.random.shuffle(new_data)
+
         return new_data
