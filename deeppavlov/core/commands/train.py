ed92dd63d801dcf4de05f7f44342770e5084eaff
dimakarp1996 <dimakarp1996@yandex.ru>
2023-02-18 02:01:18 +0300

    Update train.py

--- deeppavlov/core/commands/train.py
+++ deeppavlov/core/commands/train.py
@@ -49,14 +49,17 @@ def read_data_by_config(config: dict):
     except KeyError:
         raise ConfigError("No dataset reader is provided in the JSON config.")
 
-    reader = get_model(reader_config.pop('class_name'))()
-    data_path = reader_config.pop('data_path', '')
-    if isinstance(data_path, list):
-        data_path = [expand_path(x) for x in data_path]
+    reader_class_name = reader_config.pop('class_name')
+    reader = get_model(reader_class_name)()
+    if reader_class_name != 'multitask_reader':
+        data_path = reader_config.pop('data_path', '')
+        if isinstance(data_path, list):
+            data_path = [expand_path(x) for x in data_path]
+        else:
+            data_path = expand_path(data_path)
+        return reader.read(data_path, **reader_config)
     else:
-        data_path = expand_path(data_path)
-
-    return reader.read(data_path, **reader_config)
+        return reader.read(**reader_config)
 
 
 def get_iterator_from_config(config: dict, data: dict):
