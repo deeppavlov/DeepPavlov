237bdd5b447727338264c654da17c77156ccdc05
dimakarp1996 <dimakarp1996@yandex.ru>
2023-02-21 19:59:02 +0300

    Update fmeasure.py

--- deeppavlov/metrics/fmeasure.py
+++ deeppavlov/metrics/fmeasure.py
@@ -18,7 +18,7 @@ from itertools import chain
 from logging import getLogger
 
 import numpy as np
-from sklearn.metrics import f1_score, confusion_matrix
+from sklearn.metrics import f1_score, confusion_matrix, multilabel_confusion_matrix
 from sklearn.preprocessing import MultiLabelBinarizer
 
 from deeppavlov.core.common.metrics_registry import register_metric
