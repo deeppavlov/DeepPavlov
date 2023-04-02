0bab3d632404198740a953042d867641b4cb9fd6
Дмитрий Евсеев <dmitrij.euseew@yandex.ru>
2023-04-02 17:50:49 +0300

    odqa files

---
+++ deeppavlov/models/doc_retrieval/bpr.py
@@ -0,0 +1,100 @@
+from typing import Any, List, Optional, Tuple
+import faiss
+import numpy as np
+import torch
+from tqdm import trange
+from transformers import AutoTokenizer, BertModel
+
+from deeppavlov.core.common.registry import register
+from deeppavlov.core.models.component import Component
+from deeppavlov.core.models.serializable import Serializable
+
+
+@register('concat_lists')
+def concat_lists(list_a: List[List[Any]], list_b: List[List[Any]]):
+    list_u = []
+    for element_a, element_b in zip(list_a, list_b):
+        list_u.append(element_a + element_b)
+    return list_u
+
+
+class FaissBinaryIndex:
+    def __init__(self, index: faiss.Index):
+        self.index = index
+
+    def search(self, query_embs: np.ndarray, k: int, binary_k=1000, rerank=True) -> Tuple[np.ndarray, np.ndarray]:
+        faiss.omp_set_num_threads(12)
+        num_queries = query_embs.shape[0]
+        bin_query_embs = np.packbits(np.where(query_embs > 0, 1, 0)).reshape(num_queries, -1)
+
+        raw_index = self.index.index
+        _, ids_arr = raw_index.search(bin_query_embs, binary_k)
+        psg_embs = np.vstack([np.unpackbits(raw_index.reconstruct(int(id_))) for id_ in ids_arr.reshape(-1)])
+        psg_embs = psg_embs.reshape(query_embs.shape[0], binary_k, query_embs.shape[1])
+        psg_embs = psg_embs.astype(np.float32)
+
+        psg_embs = psg_embs * 2 - 1
+        scores_arr = np.einsum("ijk,ik->ij", psg_embs, query_embs)
+        sorted_indices = np.argsort(-scores_arr, axis=1)
+
+        ids_arr = ids_arr[np.arange(num_queries)[:, None], sorted_indices]
+        ids_arr = np.array([self.index.id_map.at(int(id_)) for id_ in ids_arr.reshape(-1)], dtype=np.int)
+        ids_arr = ids_arr.reshape(num_queries, -1)
+        scores_arr = scores_arr[np.arange(num_queries)[:, None], sorted_indices]
+
+        return scores_arr[:, :k], ids_arr[:, :k]
+
+
+@register('bpr')
+class BPR(Component, Serializable):
+    def __init__(self, pretrained_model: str,
+                load_path: str,
+                bpr_index: str,
+                query_encoder_file: str,
+                max_query_length: int = 256,
+                top_n: int = 100,
+                device: str = "gpu",
+                *args, **kwargs
+                ):
+        super().__init__(save_path=None, load_path=load_path)
+        self.device = torch.device("cuda" if torch.cuda.is_available() and device == "gpu" else "cpu")
+        self.bpr_index = bpr_index
+        self.top_n = top_n
+        self.max_query_length = max_query_length
+        self.query_encoder_file = query_encoder_file
+        self.tokenizer = AutoTokenizer.from_pretrained(pretrained_model, use_fast=True)
+        self.q_encoder = BertModel.from_pretrained(pretrained_model).to(self.device)
+        self.load()
+        self.index = FaissBinaryIndex(self.base_index)
+    
+    def load(self):
+        checkpoint = torch.load(str(self.load_path / self.query_encoder_file), map_location=self.device)
+        self.q_encoder.load_state_dict(checkpoint["model_state_dict"], strict=False)
+        self.base_index = faiss.read_index_binary(str(self.load_path / self.bpr_index))
+        
+    def save(self) -> None:
+        pass
+    
+    def encode_queries(self, queries, batch_size: int = 256) -> np.ndarray:
+        embeddings = []
+        with torch.no_grad():
+            for start in trange(0, len(queries), batch_size):
+                model_inputs = self.tokenizer.batch_encode_plus(
+                    queries[start : start + batch_size],
+                    return_tensors="pt",
+                    max_length=self.max_query_length,
+                    padding="max_length",
+                )
+                model_inputs = {k: v.to(self.device) for k, v in model_inputs.items()}
+                sequence_output = self.q_encoder(**model_inputs)[0]
+                emb = sequence_output[:, 0, :].contiguous().cpu().numpy()
+                embeddings.append(emb)
+
+        return np.vstack(embeddings)
+
+    def __call__(self, queries):
+        queries = [query.lower() for query in queries]
+        query_embeddings = self.encode_queries(queries)
+        scores_batch, ids_batch = self.index.search(query_embeddings, self.top_n)
+        ids_batch = ids_batch.tolist()
+        return ids_batch
--- deeppavlov/models/torch_bert/torch_transformers_squad.py
+++ deeppavlov/models/torch_bert/torch_transformers_squad.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 import re
+from collections import namedtuple
 from logging import getLogger
 from pathlib import Path
 from typing import List, Tuple, Optional, Dict
@@ -20,7 +21,7 @@ from typing import List, Tuple, Optional, Dict
 import numpy as np
 import torch
 from overrides import overrides
-from transformers import AutoModelForQuestionAnswering, AutoConfig
+from transformers import AutoModelForQuestionAnswering, AutoConfig, AutoModel
 from transformers.data.processors.utils import InputFeatures
 
 from deeppavlov.core.commands.utils import expand_path
@@ -34,6 +35,24 @@ logger = getLogger(__name__)
 def softmax_mask(val, mask):
     inf = 1e30
     return -inf * (1 - mask.to(torch.float32)) + val
+
+
+class PassageReaderClassifier(torch.nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.encoder = AutoModel.from_config(config=config)
+        self.qa_outputs = torch.nn.Linear(config.hidden_size, 2)
+        self.qa_classifier = torch.nn.Linear(config.hidden_size, 1)
+
+    def forward(self, input_ids, attention_mask, token_type_ids):
+        out = self.encoder(input_ids=input_ids, token_type_ids=token_type_ids, attention_mask=attention_mask)
+        logits = self.qa_outputs(out[0])
+        start_logits, end_logits = logits.split(1, dim=-1)
+        start_logits = start_logits.squeeze(-1)
+        end_logits = end_logits.squeeze(-1)
+        rank_logits = self.qa_classifier(out[0][:, 0, :])
+        outputs = namedtuple("outputs", "start_logits end_logits rank_logits")
+        return outputs(start_logits=start_logits, end_logits=end_logits, rank_logits=rank_logits)
 
 
 @register('torch_transformers_squad')
@@ -76,6 +95,7 @@ class TorchTransformersSquad(TorchModel):
                  load_before_drop: bool = True,
                  clip_norm: Optional[float] = None,
                  min_learning_rate: float = 1e-06,
+                 psg_cls: bool = False,
                  batch_size: int = 10,
                  **kwargs) -> None:
 
@@ -91,6 +111,7 @@ class TorchTransformersSquad(TorchModel):
 
         self.pretrained_bert = pretrained_bert
         self.bert_config_file = bert_config_file
+        self.psg_cls = psg_cls
         self.batch_size = batch_size
 
         super().__init__(optimizer=optimizer,
@@ -219,7 +240,11 @@ class TorchTransformersSquad(TorchModel):
 
                 start_probs = torch.nn.functional.softmax(logits_st, dim=-1)
                 end_probs = torch.nn.functional.softmax(logits_end, dim=-1)
-                scores = torch.tensor(1) - start_probs[:, 0] * end_probs[:, 0]  # ok
+                if self.psg_cls:
+                    logits_rank = outputs.rank_logits
+                    scores = logits_rank.squeeze(1)
+                else:
+                    scores = torch.tensor(1) - start_probs[:, 0] * end_probs[:, 0]
 
                 outer = torch.matmul(start_probs.view(*start_probs.size(), 1),
                                      end_probs.view(end_probs.size()[0], 1, end_probs.size()[1]))
@@ -274,8 +299,10 @@ class TorchTransformersSquad(TorchModel):
             config = AutoConfig.from_pretrained(self.pretrained_bert,
                                                 output_attentions=False,
                                                 output_hidden_states=False)
-
-            self.model = AutoModelForQuestionAnswering.from_pretrained(self.pretrained_bert, config=config)
+            if self.psg_cls:
+                self.model = PassageReaderClassifier(config=config)
+            else:
+                self.model = AutoModelForQuestionAnswering.from_pretrained(self.pretrained_bert, config=config)
 
         elif self.bert_config_file and Path(self.bert_config_file).is_file():
             self.bert_config = AutoConfig.from_json_file(str(expand_path(self.bert_config_file)))
@@ -284,7 +311,10 @@ class TorchTransformersSquad(TorchModel):
                 self.bert_config.attention_probs_dropout_prob = 1.0 - self.attention_probs_keep_prob
             if self.hidden_keep_prob is not None:
                 self.bert_config.hidden_dropout_prob = 1.0 - self.hidden_keep_prob
-            self.model = AutoModelForQuestionAnswering(config=self.bert_config)
+            if self.psg_cls:
+                self.model = PassageReaderClassifier(config=self.bert_config)
+            else:
+                self.model = AutoModelForQuestionAnswering(config=self.bert_config)
         else:
             raise ConfigError("No pre-trained BERT model is given.")
 
