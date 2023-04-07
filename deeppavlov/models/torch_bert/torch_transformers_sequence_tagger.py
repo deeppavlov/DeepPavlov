e9d0b7a50f77344296092f3d61178581cb618622
dmitrijeuseew <dmitrij.euseew@yandex.ru>
2023-04-07 10:29:08 +0300

    fix: CRF save in torch_transformers_sequence_tagger (#1637)
    Co-authored-by: Fedor Ignatov <ignatov.fedor@gmail.com>

--- deeppavlov/models/torch_bert/torch_transformers_sequence_tagger.py
+++ deeppavlov/models/torch_bert/torch_transformers_sequence_tagger.py
@@ -310,8 +310,10 @@ class TorchTransformersSequenceTagger(TorchModel):
                     log.warning(f"Init from scratch. Load path {weights_path_crf} does not exist.")
 
     def save(self, fname: Optional[str] = None, *args, **kwargs) -> None:
-        super().save()
+        super().save(fname)
         if self.use_crf:
+            if fname is None:
+                fname = self.save_path
             weights_path_crf = Path(f"{fname}_crf").resolve()
             weights_path_crf = weights_path_crf.with_suffix(".pth.tar")
             torch.save({"model_state_dict": self.crf.cpu().state_dict()}, weights_path_crf)
