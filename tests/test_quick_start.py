71ef040225af7642c04d8d078af7ec8f9d8efd94
Fedor Ignatov <ignatov.fedor@gmail.com>
2023-07-03 10:27:27 +0300

    fix: test_quick_start syntax

--- tests/test_quick_start.py
+++ tests/test_quick_start.py
@@ -120,7 +120,6 @@ PARAMS = {
         ("classifiers/superglue/superglue_boolq_roberta_mnli.json", "classifiers", ('TI',)): [TWO_ARGUMENTS_INFER_CHECK],
         ("classifiers/superglue/superglue_record_roberta.json", "classifiers", ('TI',)): [RECORD_ARGUMENTS_INFER_CHECK],
         ("classifiers/superglue/superglue_wic_bert.json", "classifiers", ('TI',)): [TWO_ARGUMENTS_INFER_CHECK],
-        ("classifiers/topics_distilbert_base_uncased.json", "classifiers", ('TI',)): [ONE_ARGUMENT_INFER_CHECK]
         ("classifiers/topics_distilbert_base_uncased.json", "classifiers", ('TI',)): [ONE_ARGUMENT_INFER_CHECK],
         ("classifiers/few_shot_roberta.json", "classifiers", ('IP',)): [
             ('Dummy text', ['Dummy text Dummy text', 'Dummy class'], ('Dummy class',))
