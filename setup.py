914e12d2c74b3100f425176b0bc42605e9c82a7e
optk2k <optk2k@yandex.ru>
2024-02-16 10:09:49 +0300

    sphinx for python > 9 3

--- setup.py
+++ setup.py
@@ -69,7 +69,6 @@ if __name__ == '__main__':
             ],
             'docs': [
 
-                'sphinx==3.5.4;python_version<"3.9"',
                 'sphinx==5.0.*;python_version<="3.10"',
                 'sphinx==7.2.*;python_version=="3.11.*"',
                 'sphinx_rtd_theme==0.5.2;python_version<="3.10"',
