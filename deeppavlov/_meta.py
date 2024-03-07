c35a429e986c32a6037db37be34cd882e57ca05f
Fedor Ignatov <ignatov.fedor@gmail.com>
2024-03-07 16:52:54 +0300

    refactor: fixes in documentation and small improvements

--- deeppavlov/_meta.py
+++ deeppavlov/_meta.py
@@ -1,4 +1,4 @@
-__version__ = '1.5.0'
+__version__ = '1.6.0'
 __author__ = 'Neural Networks and Deep Learning lab, MIPT'
 __description__ = 'An open source library for building end-to-end dialog systems and training chatbots.'
 __keywords__ = ['NLP', 'NER', 'SQUAD', 'Intents', 'Chatbot']
